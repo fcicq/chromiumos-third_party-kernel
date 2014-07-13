fdc9d775b223070edae893ec8d50efc2cc616e8f
Richard Weinberger <richard@nod.at>
2014-07-13 17:09:40 +0200

    BACKPORT: arm64: Remove signal translation and exec_domain
    As execution domain support is gone we can remove
    signal translation from the signal code and remove
    exec_domain from thread_info.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit 9699a517e0029c4dc34159787a26a746dfab858b)
    
    Conflicts:
       arch/arm64/include/asm/thread_info.h [context]
    
    BUG=b:80506651
    TEST=ARC++
    
    Change-Id: I18ad3e420feefc50eb07c12499b1c03ef0f9c50d
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1739322
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Victor Hsieh <victorhsieh@chromium.org>

--- arch/arm64/kernel/asm-offsets.c
+++ arch/arm64/kernel/asm-offsets.c
@@ -39,7 +39,6 @@ int main(void)
   DEFINE(TI_PREEMPT,		offsetof(struct thread_info, preempt_count));
   DEFINE(TI_ADDR_LIMIT,		offsetof(struct thread_info, addr_limit));
   DEFINE(TI_TASK,		offsetof(struct thread_info, task));
-  DEFINE(TI_EXEC_DOMAIN,	offsetof(struct thread_info, exec_domain));
   DEFINE(TI_CPU,		offsetof(struct thread_info, cpu));
 #ifdef CONFIG_ALT_SYSCALL
   DEFINE(TI_NR_SYSCALLS,	offsetof(struct thread_info, nr_syscalls));
