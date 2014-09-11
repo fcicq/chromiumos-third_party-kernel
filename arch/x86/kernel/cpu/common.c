c1e0af29178ab77b2c130e0e4a06fbfa5e51455a
Dave Hansen <dave.hansen@linux.intel.com>
2014-09-11 14:15:24 -0700

    x86: Add more disabled features
    The original motivation for these patches was for an Intel CPU
    feature called MPX.  The patch to add a disabled feature for it
    will go in with the other parts of the support.
    
    But, in the meantime, there are a few other features than MPX
    that we can make assumptions about at compile-time based on
    compile options.  Add them to disabled-features.h and check them
    with cpu_feature_enabled().
    
    Note that this gets rid of the last things that needed an #ifdef
    CONFIG_X86_64 in cpufeature.h.  Yay!
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: http://lkml.kernel.org/r/20140911211524.C0EC332A@viggo.jf.intel.com
    Acked-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    (cherry picked from commit 9298b815efe500b272e4084ed05eeae7a92b5340)
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1441,7 +1441,7 @@ void cpu_init(void)
 
 	pr_info("Initializing CPU#%d\n", cpu);
 
-	if (cpu_has_vme || cpu_has_tsc || cpu_has_de)
+	if (cpu_feature_enabled(X86_FEATURE_VME) || cpu_has_tsc || cpu_has_de)
 		cr4_clear_bits(X86_CR4_VME|X86_CR4_PVI|X86_CR4_TSD|X86_CR4_DE);
 
 	load_current_idt();
