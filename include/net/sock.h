869e4cc9ce8a8db2dee844f83a6698cba7c3f3f1
Craig Gallek <kraig@google.com>
2015-06-15 11:26:18 -0400

    UPSTREAM: sock_diag: define destruction multicast groups
    These groups will contain socket-destruction events for
    AF_INET/AF_INET6, IPPROTO_TCP/IPPROTO_UDP.
    
    Near the end of socket destruction, a check for listeners is
    performed.  In the presence of a listener, rather than completely
    cleanup the socket, a unit of work will be added to a private
    work queue which will first broadcast information about the socket
    and then finish the cleanup operation.
    
    Signed-off-by: Craig Gallek <kraig@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (cherry picked from commit eb4cb008529ca08e0d8c0fa54e8f739520197a65)
    
    BUG=b:73424026
    TEST=build, flashed caroline-arcnext, booted, checked ARC container
    worked fined, exercised netd sock_diag code, ran some tests with ss.
    Signed-off-by: Hugo Benichi <hugobenichi@google.com>
    
    Change-Id: I6163e6c0a8e1bc4e5cb5b29189d278afc5efed20
    Reviewed-on: https://chromium-review.googlesource.com/1131159
    Commit-Ready: Hugo Benichi <hugobenichi@google.com>
    Tested-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1591,6 +1591,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 		      struct proto *prot);
 void sk_free(struct sock *sk);
 void sk_release_kernel(struct sock *sk);
+void sk_destruct(struct sock *sk);
 struct sock *sk_clone_lock(const struct sock *sk, const gfp_t priority);
 
 struct sk_buff *sock_wmalloc(struct sock *sk, unsigned long size, int force,
