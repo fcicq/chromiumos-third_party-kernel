0995675c0c9501fdab978b50d3152977b99853e3
Jani Nikula <jani.nikula@intel.com>
2015-09-14 14:03:48 +0300

    BACKPORT: drm/i915: make backlight hooks connector specific
    Previously we've relied on having basically one backlight and one
    backlight type per platform. This is already a bit quirky with PMIC PWM
    support on VLV/CHV platforms with MIPI DSI. In the foreseeable future
    we'll have at least DPCD based backlight control on eDP and DCS command
    based backlight control on MIPI DSI. Backlight is becoming more and more
    connector specific, so reflect this fact by making the backlight control
    hooks connector specific.
    
    This enables further work to reuse generic backlight code in
    intel_panel.c while adding more specific backlight code accessed via the
    hooks.
    
    Cc: Deepak M <m.deepak@intel.com>
    Cc: Yetunde Adebisi <yetundex.adebisi@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Deepak M <m.deepak@intel.com>
    Reviewed-by: Yetunde Adebisi <yetundex.adebisi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 5507faeb03f87f952373314b21741548421a6944)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    Conflicts:
    Minor context change in	drivers/gpu/drm/i915/i915_drv.h
    Fix compilation error and skip
    mipi related initialization for vlv drivers/gpu/drm/i915/intel_panel.c
    
    BUG=chrome-os-partner:53839
    TEST=External monitors connected via MST hub or daisy-chain over DP should come up.
    
    Change-Id: I973637b547d41bb1841b310c0bf169e8f7d7d41d
    Reviewed-on: https://chromium-review.googlesource.com/421717
    Commit-Ready: Brian J Lovin <brian.j.lovin@intel.com>
    Tested-by: Chenglei Ren <chenglei.ren@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -672,15 +672,6 @@ struct drm_i915_display_funcs {
 	/* display clock increase/decrease */
 	/* pll clock increase/decrease */
 
-	int (*setup_backlight)(struct intel_connector *connector, enum pipe pipe);
-	uint32_t (*get_backlight)(struct intel_connector *connector);
-	void (*set_backlight)(struct intel_connector *connector,
-			      uint32_t level);
-	void (*disable_backlight)(struct intel_connector *connector);
-	void (*enable_backlight)(struct intel_connector *connector);
-	uint32_t (*backlight_hz_to_pwm)(struct intel_connector *connector,
-					uint32_t hz);
-
 	void (*load_csc_matrix)(struct drm_crtc *crtc);
 	void (*load_luts)(struct drm_crtc *crtc);
 };
