0cbe515987d96dd9cbc54a6678ac8ca82a16ffa1
Sugar Zhang <sugar.zhang@rock-chips.com>
2015-10-08 20:40:07 +0800

    UPSTREAM: ASoC: rockchip: i2s: add 8 channels capture support
    support max 8 channels capture, please add property
    'rockchip,capture-channels' in dts to enable this,
    if not, support 2 channels capture default.
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit 4c9c018b2ac72e6ffaeae472723023dc4fd99a88)
    
    [3.14: conflicts with commit
     db3d18e89e5f9 ("CHROMIUM: ASoC: rockchip_i2s: Set I2S_DMACR_TDL to 24")
     because of different context]
    Conflicts:
        sound/soc/rockchip/rockchip_i2s.c
    
    BUG=b:122017492
    TEST=for ch in 2 4 6 8; do
      echo alsa_conformance_test -P hw:1,0 -c $ch
      alsa_conformance_test -P hw:1,0 -c $ch | grep 'Regression'
    done
    Change-Id: I560cdb5b32cff6ce8f8110d2e772e8b8d5f9af63
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1394465
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- sound/soc/rockchip/rockchip_i2s.c
+++ sound/soc/rockchip/rockchip_i2s.c
@@ -280,8 +280,33 @@ static int rockchip_i2s_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
-	regmap_update_bits(i2s->regmap, I2S_TXCR, I2S_TXCR_VDW_MASK, val);
-	regmap_update_bits(i2s->regmap, I2S_RXCR, I2S_RXCR_VDW_MASK, val);
+	switch (params_channels(params)) {
+	case 8:
+		val |= I2S_CHN_8;
+		break;
+	case 6:
+		val |= I2S_CHN_6;
+		break;
+	case 4:
+		val |= I2S_CHN_4;
+		break;
+	case 2:
+		val |= I2S_CHN_2;
+		break;
+	default:
+		dev_err(i2s->dev, "invalid channel: %d\n",
+			params_channels(params));
+		return -EINVAL;
+	}
+
+	if (substream->stream == SNDRV_PCM_STREAM_CAPTURE)
+		regmap_update_bits(i2s->regmap, I2S_RXCR,
+				   I2S_RXCR_VDW_MASK | I2S_RXCR_CSR_MASK,
+				   val);
+	else
+		regmap_update_bits(i2s->regmap, I2S_TXCR,
+				   I2S_TXCR_VDW_MASK | I2S_TXCR_CSR_MASK,
+				   val);
 
         /* DMA w/ 24 bytes to go to support DDRFreq */
 	regmap_update_bits(i2s->regmap, I2S_DMACR, I2S_DMACR_TDL_MASK,
@@ -452,10 +477,12 @@ static const struct regmap_config rockchip_i2s_regmap_config = {
 
 static int rockchip_i2s_probe(struct platform_device *pdev)
 {
+	struct device_node *node = pdev->dev.of_node;
 	struct rk_i2s_dev *i2s;
 	struct resource *res;
 	void __iomem *regs;
 	int ret;
+	int val;
 
 	i2s = devm_kzalloc(&pdev->dev, sizeof(*i2s), GFP_KERNEL);
 	if (!i2s) {
@@ -516,6 +543,14 @@ static int rockchip_i2s_probe(struct platform_device *pdev)
 		ret = i2s_runtime_resume(&pdev->dev);
 		if (ret)
 			goto err_pm_disable;
+	}
+
+	/* refine capture channels */
+	if (!of_property_read_u32(node, "rockchip,capture-channels", &val)) {
+		if (val >= 2 && val <= 8)
+			rockchip_i2s_dai.capture.channels_max = val;
+		else
+			rockchip_i2s_dai.capture.channels_max = 2;
 	}
 
 	ret = devm_snd_soc_register_component(&pdev->dev,
