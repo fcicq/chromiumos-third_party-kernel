eda9c9d938509499ada3ece914bcc0a576ce4e4f
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-11-10 15:11:20 +0100

    BACKPORT: ARM/arm64: KVM: test properly for a PTE's uncachedness
    The open coded tests for checking whether a PTE maps a page as
    uncached use a flawed '(pte_val(xxx) & CONST) != CONST' pattern,
    which is not guaranteed to work since the type of a mapping is
    not a set of mutually exclusive bits
    
    For HYP mappings, the type is an index into the MAIR table (i.e, the
    index itself does not contain any information whatsoever about the
    type of the mapping), and for stage-2 mappings it is a bit field where
    normal memory and device types are defined as follows:
    
        #define MT_S2_NORMAL            0xf
        #define MT_S2_DEVICE_nGnRE      0x1
    
    I.e., masking *and* comparing with the latter matches on the former,
    and we have been getting lucky merely because the S2 device mappings
    also have the PTE_UXN bit set, or we would misidentify memory mappings
    as device mappings.
    
    Since the unmap_range() code path (which contains one instance of the
    flawed test) is used both for HYP mappings and stage-2 mappings, and
    considering the difference between the two, it is non-trivial to fix
    this by rewriting the tests in place, as it would involve passing
    down the type of mapping through all the functions.
    
    However, since HYP mappings and stage-2 mappings both deal with host
    physical addresses, we can simply check whether the mapping is backed
    by memory that is managed by the host kernel, and only perform the
    D-cache maintenance if this is the case.
    
    BUG=chromium:846515
    TEST=build/boot on hana with USE=kvm_host
    
    Change-Id: Icf584d6f75d04e9fe1e13e080773d91315e5e470
    Cc: stable@vger.kernel.org
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Tested-by: Pavel Fedin <p.fedin@samsung.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>
    (cherry picked from commit e6fab54423450d699a09ec2b899473a541f61971)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    [SR: context from not having 15a49a44fc3620]
     Conflicts:
    	arch/arm/kvm/mmu.c
    Reviewed-on: https://chromium-review.googlesource.com/1088107
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- arch/arm/kvm/mmu.c
+++ arch/arm/kvm/mmu.c
@@ -116,6 +116,11 @@ static void kvm_flush_dcache_pud(pud_t pud)
 	__kvm_flush_dcache_pud(pud);
 }
 
+static bool kvm_is_device_pfn(unsigned long pfn)
+{
+	return !pfn_valid(pfn);
+}
+
 static int mmu_topup_memory_cache(struct kvm_mmu_memory_cache *cache,
 				  int min, int max)
 {
@@ -212,7 +217,7 @@ static void unmap_ptes(struct kvm *kvm, pmd_t *pmd,
 			kvm_tlb_flush_vmid_ipa(kvm, addr);
 
 			/* No need to invalidate the cache for device mappings */
-			if ((pte_val(old_pte) & PAGE_S2_DEVICE) != PAGE_S2_DEVICE)
+			if (!kvm_is_device_pfn(__phys_to_pfn(addr)))
 				kvm_flush_dcache_pte(old_pte);
 
 			put_page(virt_to_page(pte));
@@ -304,8 +309,7 @@ static void stage2_flush_ptes(struct kvm *kvm, pmd_t *pmd,
 
 	pte = pte_offset_kernel(pmd, addr);
 	do {
-		if (!pte_none(*pte) &&
-		    (pte_val(*pte) & PAGE_S2_DEVICE) != PAGE_S2_DEVICE)
+		if (!pte_none(*pte) && !kvm_is_device_pfn(__phys_to_pfn(addr)))
 			kvm_flush_dcache_pte(*pte);
 	} while (pte++, addr += PAGE_SIZE, addr != end);
 }
@@ -1029,11 +1033,6 @@ static bool kvm_is_write_fault(struct kvm_vcpu *vcpu)
 		return false;
 
 	return kvm_vcpu_dabt_iswrite(vcpu);
-}
-
-static bool kvm_is_device_pfn(unsigned long pfn)
-{
-	return !pfn_valid(pfn);
 }
 
 /**
