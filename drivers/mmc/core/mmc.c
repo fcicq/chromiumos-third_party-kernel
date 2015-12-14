e673db2f66069a2f2cd4d9b8c1c1c2804de399e1
Adrian Hunter <adrian.hunter@intel.com>
2015-12-14 15:51:27 +0200

    BACKPORT: mmc: It is not an error for the card to be removed while suspended
    A card can be removed while it is runtime suspended.
    Do not print an error message.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 520322d92eab66b6fee562557fdd201b01cd6240)
    Signed-off-by: Azhar Shaikh <azhar.shaikh@intel.com>
    
    Conflicts:
    	drivers/mmc/core/mmc.c
    	drivers/mmc/core/sd.c
    	Minor conflicts due to change in the text of error message.
    
    BUG=b:36790283
    TEST=with sd card inserted and no activity being done on the sd card
         SD host controller driver goes to suspend after ~3-4secs of
         inactivity. Tested this on a cyan.
    
    Change-Id: If6a53cd7935fc0e75a1a5b538e3d05aabce285c9
    Reviewed-on: https://chromium-review.googlesource.com/463989
    Commit-Ready: Azhar Shaikh <azhar.shaikh@intel.com>
    Tested-by: Hannah Williams <hannah.williams@intel.com>
    Reviewed-by: Marc Herbert <marc.herbert@intel.com>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1970,7 +1970,7 @@ static int mmc_runtime_resume(struct mmc_host *host)
 		return 0;
 
 	err = _mmc_resume(host);
-	if (err)
+	if (err && err != -ENOMEDIUM)
 		pr_err("%s: error %d doing aggressive resume\n",
 			mmc_hostname(host), err);
 
