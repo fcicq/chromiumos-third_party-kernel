f9b0ebb37457eef0783532836b65a0c2709d9de5
Douglas Anderson <dianders@chromium.org>
2016-02-03 08:39:17 -0800

    CHROMIUM: clk: rockchip: Bump up Fvco for 216 MHz for 72 MHz HDMI
    We were using:
     ('0.000%', 'i=3', 72000000, 1, 72, 8, (1728000000, 216000000), 'exact')
    
    Try:
     ('0.000%', 'i=3', 72000000, 1, 144, 16, (3456000000, 216000000), 'exact')
    
    We'll use the standard high Fvco methods we've been using recently.
    
    BUG=chrome-os-partner:49507
    TEST=72 MHz still works OK and uses new PLL setting.
    
    Change-Id: Ic91c42d0883ceb4482e89e96c687d62315300ba9
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/327713
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/clk/rockchip/clk-rk3288.c
+++ drivers/clk/rockchip/clk-rk3288.c
@@ -111,7 +111,7 @@ struct rockchip_pll_rate_table rk3288_pll_rates[] = {
 	RK3066_PLL_RATE_NB( 250500000, 1, 167, 16, 1), /* Fvco = 4.008 GHz */
 	RK3066_PLL_RATE_NB( 243428571, 1, 142, 14, 1), /* Fvco = 3.408 GHz */
 	RK3066_PLL_RATE( 238000000, 1, 119, 12),       /* Fvco = 2.856 GHz */
-	RK3066_PLL_RATE( 216000000, 1, 72, 8),
+	RK3066_PLL_RATE_NB( 216000000, 1, 144, 16, 1), /* Fvco = 3.456 GHz */
 	RK3066_PLL_RATE_NB( 213000000, 1, 142, 16, 1), /* Fvco = 3.408 GHz */
 	RK3066_PLL_RATE( 195428571, 1, 114, 14),       /* Fvco = 2.736 GHz */
 	RK3066_PLL_RATE( 160000000, 1, 80, 12),
