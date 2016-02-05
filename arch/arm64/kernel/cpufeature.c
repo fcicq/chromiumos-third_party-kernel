a6bf131b28df067a89768beb4bb9d27132ee7979
James Morse <james.morse@arm.com>
2016-02-05 14:58:49 +0000

    UPSTREAM: arm64: cpufeature: Test 'matches' pointer to find the end of the list
    CPU feature code uses the desc field as a test to find the end of the list,
    this means every entry must have a description. This generates noise for
    entries in the list that aren't really features, but combinations of them.
    e.g.
    > CPU features: detected feature: Privileged Access Never
    > CPU features: detected feature: PAN and not UAO
    
    These combination features are needed for corner cases with alternatives,
    where cpu features interact.
    
    Change all walkers of the arm64_features[] and arm64_hwcaps[] lists to test
    'matches' not 'desc', and only print 'desc' if it is non-NULL.
    
    BUG=chromium:888146
    TEST=build/boot on kevin - ensure SMC workaround is being called
    
    Change-Id: I3d74d4de8e74b7375bb8bc9f5cd90bdef97a6e47
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by : Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 644c2ae198412c956700e55a2acf80b2541f6aa5)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1239271
    (cherry picked from commit 3e21dff198ede99b4e1dec06e0e766986c1273e8)
    Reviewed-on: https://chromium-review.googlesource.com/1252647

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -763,7 +763,7 @@ static void __init setup_cpu_hwcaps(void)
 	int i;
 	const struct arm64_cpu_capabilities *hwcaps = arm64_hwcaps;
 
-	for (i = 0; hwcaps[i].desc; i++)
+	for (i = 0; hwcaps[i].matches; i++)
 		if (hwcaps[i].matches(&hwcaps[i]))
 			cap_set_hwcap(&hwcaps[i]);
 }
@@ -773,11 +773,11 @@ void update_cpu_capabilities(const struct arm64_cpu_capabilities *caps,
 {
 	int i;
 
-	for (i = 0; caps[i].desc; i++) {
+	for (i = 0; caps[i].matches; i++) {
 		if (!caps[i].matches(&caps[i]))
 			continue;
 
-		if (!cpus_have_cap(caps[i].capability))
+		if (!cpus_have_cap(caps[i].capability) && caps[i].desc)
 			pr_info("%s %s\n", info, caps[i].desc);
 		cpus_set_cap(caps[i].capability);
 	}
@@ -792,7 +792,7 @@ enable_cpu_capabilities(const struct arm64_cpu_capabilities *caps)
 {
 	int i;
 
-	for (i = 0; caps[i].desc; i++)
+	for (i = 0; caps[i].matches; i++)
 		if (caps[i].enable && cpus_have_cap(caps[i].capability))
 			/*
 			 * Use stop_machine() as it schedules the work allowing
@@ -905,7 +905,7 @@ void verify_local_cpu_capabilities(void)
 		return;
 
 	caps = arm64_features;
-	for (i = 0; caps[i].desc; i++) {
+	for (i = 0; caps[i].matches; i++) {
 		if (!cpus_have_cap(caps[i].capability) || !caps[i].sys_reg)
 			continue;
 		/*
@@ -918,7 +918,7 @@ void verify_local_cpu_capabilities(void)
 			caps[i].enable(NULL);
 	}
 
-	for (i = 0, caps = arm64_hwcaps; caps[i].desc; i++) {
+	for (i = 0, caps = arm64_hwcaps; caps[i].matches; i++) {
 		if (!cpus_have_hwcap(&caps[i]))
 			continue;
 		if (!feature_matches(__raw_read_system_reg(caps[i].sys_reg), &caps[i]))
