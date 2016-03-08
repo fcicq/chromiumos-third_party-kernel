8539b93d7ab54171ad8d1010e5f37d0dc176e3c4
Douglas Anderson <dianders@chromium.org>
2016-03-08 12:23:52 -0800

    CHROMIUM: ARM: rockchip: Handle ERRATA 821420 for resume
    The normal prov-v7.S code doesn't run on the main CPU during resume from
    S3.  Apply the errata early in the resume at the same time that we
    restore the l2ctlr.
    
    BUG=chrome-os-partner:47192
    TEST=kernel_CheckArmErrata autotest
    
    Change-Id: I7df5242dade936ae2236a882e62ddabd8c5923ba
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/331745
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-rockchip/embedded/rk3288_resume.c
+++ arch/arm/mach-rockchip/embedded/rk3288_resume.c
@@ -47,17 +47,23 @@ struct rk3288_resume_params rk3288_resume_params
  */
 static void __noreturn rk3288_resume_c(void)
 {
+	u32 tmp;
+
 	if (rk3288_resume_params.l2ctlr_f)
 		asm("mcr p15, 1, %0, c9, c0, 2" : :
 			"r" (rk3288_resume_params.l2ctlr));
 
 	/* We will have lost this when the CPU powered off */
 	if (IS_ENABLED(CONFIG_ARM_ERRATA_818325_852422)) {
-		u32 diag_reg;
-
-		asm volatile("mrc p15, 0, %0, c15, c0, 1" : "=r" (diag_reg));
-		diag_reg |= (1 << 12);
-		asm volatile("mcr p15, 0, %0, c15, c0, 1" : : "r" (diag_reg));
+		asm volatile("mrc p15, 0, %0, c15, c0, 1" : "=r" (tmp));
+		tmp |= (1 << 12);
+		asm volatile("mcr p15, 0, %0, c15, c0, 1" : : "r" (tmp));
+		asm volatile("isb");
+	}
+	if (IS_ENABLED(CONFIG_ARM_ERRATA_821420)) {
+		asm volatile("mrc p15, 0, %0, c15, c0, 2" : "=r" (tmp));
+		tmp |= (1 << 1);
+		asm volatile("mcr p15, 0, %0, c15, c0, 2" : : "r" (tmp));
 		asm volatile("isb");
 	}
 
