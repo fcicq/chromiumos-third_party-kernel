16bb75e6403fdb51ce69cbda6c42bf7a1883acc2
Imre Deak <imre.deak@intel.com>
2016-03-16 13:38:59 +0200

    UPSTREAM: drm/i915: Move load time audio component registration earlier
    We should register all the interfaces before we enable runtime PM.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1458128348-15730-11-git-send-email-imre.deak@intel.com
    (cherry picked from commit 3487b66ba13c1a7ba1185028ad0dae95e97bd4d2)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    BUG=chrome-os-partner:53839
    TEST=External monitors connected via MST hub or daisy-chain over DP should come up.
    
    Change-Id: I2b9af1628fb654193f992f9ba286ec4c92905865
    Reviewed-on: https://chromium-review.googlesource.com/421881
    Commit-Ready: Brian J Lovin <brian.j.lovin@intel.com>
    Tested-by: Chenglei Ren <chenglei.ren@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1084,9 +1084,9 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	if (IS_GEN5(dev))
 		intel_gpu_ips_init(dev_priv);
 
-	intel_runtime_pm_enable(dev_priv);
-
 	i915_audio_component_init(dev_priv);
+
+	intel_runtime_pm_enable(dev_priv);
 
 	intel_runtime_pm_put(dev_priv);
 
@@ -1126,8 +1126,6 @@ int i915_driver_unload(struct drm_device *dev)
 
 	intel_fbdev_fini(dev);
 
-	i915_audio_component_cleanup(dev_priv);
-
 	ret = i915_gem_suspend(dev);
 	if (ret) {
 		DRM_ERROR("failed to idle hardware: %d\n", ret);
@@ -1135,6 +1133,8 @@ int i915_driver_unload(struct drm_device *dev)
 	}
 
 	intel_power_domains_fini(dev_priv);
+
+	i915_audio_component_cleanup(dev_priv);
 
 	intel_gpu_ips_teardown();
 
