e4337449621e733736187137f5c53929e405f00c
Jesse Gross <jesse@kernel.org>
2016-03-19 09:32:01 -0700

    BACKPORT: tunnels: Don't apply GRO to multiple layers of encapsulation.
    When drivers express support for TSO of encapsulated packets, they
    only mean that they can do it for one layer of encapsulation.
    Supporting additional levels would mean updating, at a minimum,
    more IP length fields and they are unaware of this.
    
    No encapsulation device expresses support for handling offloaded
    encapsulated packets, so we won't generate these types of frames
    in the transmit path. However, GRO doesn't have a check for
    multiple levels of encapsulation and will attempt to build them.
    
    UDP tunnel GRO actually does prevent this situation but it only
    handles multiple UDP tunnels stacked on top of each other. This
    generalizes that solution to prevent any kind of tunnel stacking
    that would cause problems.
    
    Fixes: bf5a755f ("net-gre-gro: Add GRE support to the GRO stack")
    Signed-off-by: Jesse Gross <jesse@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (cherry picked from commit fac8e0f579695a3ecbc4d3cac369139d7f819971)
    
    Notes on backport:
    ------------------------------------------------------------------------
    The upstream patch doesn't apply cleanly on top of ChromeOS 3.14 kernel.
    
    - In 3.14, `struct napi_gro_cb' defined in include/linux/netdevice.h
      doesn't store its `udp_mark' field as bit field, so we simply rename
      `udp_mark' to `encap_mark' like the upstream patch does.
    
    - net/ipv4/af_net.c is left unchanged as `ipip_offload' defined there
      doesn't override .gro_receive field.
    
    - net/ipv6/ip6_offload.c is left unchanged as `sit_offload' defined
      there doesn't override .gro_receive field.
    ------------------------------------------------------------------------
    
    BUG=b:32515955, chromium:672642
    TEST=Trybot runs on paladin and release builders.
    
    Change-Id: I33a0cd7d9fadd1b85086ded6eff0544846fe3180
    Signed-off-by: Ben Chan <benchan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/419806
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Kevin Cernekee <cernekee@chromium.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1701,8 +1701,8 @@ struct napi_gro_cb {
 	/* Used in ipv6_gro_receive() */
 	u16	proto;
 
-	/* Used in udp_gro_receive */
-	u16	udp_mark;
+	/* Used in tunnel GRO receive */
+	u8	encap_mark;
 
 	/* used to support CHECKSUM_COMPLETE for tunneling protocols */
 	__wsum	csum;
