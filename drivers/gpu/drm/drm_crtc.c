22c14d8e7b77b77b48c488896cb937ea7c8372d3
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-03-30 11:51:16 +0200

    UPSTREAM: drm: Initialize a linear gamma table by default
    Code stolen from gma500.
    
    This is just a minor bit of safety code that I spotted and figured it
    might be useful if we put it into the core. This is to make the
    get_gamma ioctl reflect likely reality even before the first set_gamma
    ioctl call.
    
    v2 on irc: Extend commit message per Maarten's suggestions.
    
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1459331485-28376-2-git-send-email-daniel.vetter@ffwll.ch
    (cherry picked from commit cf48e2921ee95011a164dc31e8725022bd008666)
    from topic/drm-misc-2016-06-01
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
    Change-Id: I25f7e9f12caaed514b3a5710714430e8c2f8159b
    Reviewed-on: https://chromium-review.googlesource.com/536203
    Commit-Ready: Sean Paul <seanpaul@chromium.org>
    Tested-by: Casey G Bowman <casey.g.bowman@intel.com>
    Tested-by: Sean Paul <seanpaul@google.com>
    Reviewed-by: Sean Paul <seanpaul@google.com>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -971,6 +971,9 @@ int drm_mode_crtc_set_obj_prop(struct drm_mode_object *obj,
 int drm_mode_crtc_set_gamma_size(struct drm_crtc *crtc,
 				 int gamma_size)
 {
+	uint16_t *r_base, *g_base, *b_base;
+	int i;
+
 	crtc->gamma_size = gamma_size;
 
 	crtc->gamma_store = kcalloc(gamma_size, sizeof(uint16_t) * 3,
@@ -979,6 +982,16 @@ int drm_mode_crtc_set_gamma_size(struct drm_crtc *crtc,
 		crtc->gamma_size = 0;
 		return -ENOMEM;
 	}
+
+	r_base = crtc->gamma_store;
+	g_base = r_base + gamma_size;
+	b_base = g_base + gamma_size;
+	for (i = 0; i < gamma_size; i++) {
+		r_base[i] = i << 8;
+		g_base[i] = i << 8;
+		b_base[i] = i << 8;
+	}
+
 
 	return 0;
 }
