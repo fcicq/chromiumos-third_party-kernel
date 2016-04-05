500e98e573558d8df058a6b67754c35c6d811662
Yetunde Adebisi <yetundex.adebisi@intel.com>
2016-04-05 15:10:50 +0100

    UPSTREAM: drm/dp: Add definition for Display Control DPCD Registers capability size
    This is used when reading Display Control capability Registers on the sink
    device.
    
    cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Yetunde Adebisi <yetundex.adebisi@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1459865452-9138-2-git-send-email-yetundex.adebisi@intel.com
    (cherry picked from commit 4e382db36cf50969f6bafb1ac99ae6c3f5bf5568)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    BUG=chrome-os-partner:53839
    TEST=External monitors connected via MST hub or daisy-chain over DP should come up.
    
    Change-Id: I0d4b8530751a500e986430a7cc8faf6c9a868c46
    Reviewed-on: https://chromium-review.googlesource.com/421921
    Commit-Ready: Brian J Lovin <brian.j.lovin@intel.com>
    Tested-by: Chenglei Ren <chenglei.ren@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -623,6 +623,7 @@ u8 drm_dp_get_adjust_request_post_cursor(const u8 link_status[DP_LINK_STATUS_SIZ
 #define DP_BRANCH_OUI_HEADER_SIZE	0xc
 #define DP_RECEIVER_CAP_SIZE		0xf
 #define EDP_PSR_RECEIVER_CAP_SIZE	2
+#define EDP_DISPLAY_CTL_CAP_SIZE	3
 
 void drm_dp_link_train_clock_recovery_delay(const u8 dpcd[DP_RECEIVER_CAP_SIZE]);
 void drm_dp_link_train_channel_eq_delay(const u8 dpcd[DP_RECEIVER_CAP_SIZE]);
