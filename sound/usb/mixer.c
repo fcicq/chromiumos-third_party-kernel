238a5ba447baca225ef48ac2d27fb547791f96d8
Daniel Mack <daniel@zonque.org>
2016-04-08 19:52:02 +0200

    UPSTREAM: ALSA: usb-audio: allow clock source validity interrupts
    miniDSP USBStreamer UAC2 devices send clock validity changes with the
    control field set to zero. The current interrupt handler ignores all
    packets if the control field does not match the mixer element's, but
    it really should only do that in case that field is needed to
    distinguish multiple elements with the same ID.
    
    This patch implements a logic that lets notifications packets pass
    if the element ID is unique for a given device.
    
    Signed-off-by: Daniel Mack <daniel@zonque.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 191227d99a281333b50aaf82ab4152fbfa249c19)
    Signed-off-by: Ben Zhang <benzh@chromium.org>
    
    BUG=b:74149899
    TEST=Test dongle with UAC2 jack detect with various headsets on Soraka
    
    Change-Id: I4a1622fbeb6900d690dc9141806a353e68c6df38
    Reviewed-on: https://chromium-review.googlesource.com/1072880
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Ben Zhang <benzh@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -2413,6 +2413,7 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 	__u8 unitid = (index >> 8) & 0xff;
 	__u8 control = (value >> 8) & 0xff;
 	__u8 channel = value & 0xff;
+	unsigned int count = 0;
 
 	if (channel >= MAX_CHANNELS) {
 		usb_audio_dbg(mixer->chip,
@@ -2421,6 +2422,12 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 		return;
 	}
 
+	for (list = mixer->id_elems[unitid]; list; list = list->next_id_elem)
+		count++;
+
+	if (count == 0)
+		return;
+
 	for (list = mixer->id_elems[unitid]; list; list = list->next_id_elem) {
 		struct usb_mixer_elem_info *info;
 
@@ -2428,7 +2435,7 @@ static void snd_usb_mixer_interrupt_v2(struct usb_mixer_interface *mixer,
 			continue;
 
 		info = (struct usb_mixer_elem_info *)list;
-		if (info->control != control)
+		if (count > 1 && info->control != control)
 			continue;
 
 		switch (attribute) {
