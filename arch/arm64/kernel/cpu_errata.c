43cfd45d6a8a3c85a70ed5f2f03af71dc33bdf92
Suzuki K Poulose <suzuki.poulose@arm.com>
2016-04-22 12:25:31 +0100

    BACKPORT: arm64: cpufeature: Add scope for capability check
    Add scope parameter to the arm64_cpu_capabilities::matches(), so that
    this can be reused for checking the capability on a given CPU vs the
    system wide. The system uses the default scope associated with the
    capability for initialising the CPU_HWCAPs and ELF_HWCAPs.
    
    BUG=chromium:889539
    TEST=build/boot on hana with USE=kvm_host
     - ensure SMC workaround is being called
    
    Change-Id: I5e8a2c1b326cfbfa5234a9348faf57b005ce284e
    Cc: James Morse <james.morse@arm.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Andre Przywara <andre.przywara@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from commit 92406f0cc9e3d5cc77bf3de6d68c9c2373dcd701)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    [SR: 4.4: conflicts from not having PAN or UAO support
     and from having stable patch
     da643dc17f20e arm64: cpufeature: Schedule enable() calls instead of calling them via IPI
     and fixup to previous backport for
     arm64: cpu_errata: Allow an erratum to be match for all revisions of a core]
    [SR: 3.18: conflicts from not having
     963fcd40958711  arm64: cpufeatures: Check ICC_EL1_SRE.SRE before enabling ARM64_HAS_SYSREG_GIC_CPUIF]
     Conflicts:
    	arch/arm64/include/asm/cpufeature.h
    	arch/arm64/kernel/cpufeature.c
    Reviewed-on: https://chromium-review.googlesource.com/1245296
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -22,8 +22,9 @@
 #include <asm/cpufeature.h>
 
 static bool __maybe_unused
-is_affected_midr_range(const struct arm64_cpu_capabilities *entry)
+is_affected_midr_range(const struct arm64_cpu_capabilities *entry, int scope)
 {
+	WARN_ON(scope != SCOPE_LOCAL_CPU || preemptible());
 	return MIDR_IS_CPU_MODEL_RANGE(read_cpuid_id(), entry->midr_model,
 				       entry->midr_range_min,
 				       entry->midr_range_max);
@@ -165,12 +166,14 @@ static int enable_smccc_arch_workaround_1(void *data)
 #endif	/* CONFIG_HARDEN_BRANCH_PREDICTOR */
 
 #define MIDR_RANGE(model, min, max) \
+	.def_scope = SCOPE_LOCAL_CPU, \
 	.matches = is_affected_midr_range, \
 	.midr_model = model, \
 	.midr_range_min = min, \
 	.midr_range_max = max
 
 #define MIDR_ALL_VERSIONS(model) \
+	.def_scope = SCOPE_LOCAL_CPU, \
 	.matches = is_affected_midr_range, \
 	.midr_model = model, \
 	.midr_range_min = 0, \
