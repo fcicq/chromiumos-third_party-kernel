8749bcfc4e662a66accf8bd99e2cabd79902b84a
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-04-22 22:10:28 +0200

    UPSTREAM: drm: Fix fb leaks and WARN spew in get/set_prop ioctls
    Dave Airlie had at least the refcount leak fixed in a later patch (but
    that patch does other things which need a bit more work). But we still
    have the trouble that silly userspace could hit the WARN_ON in
    drm_mode_object_find.
    
    Fix this all up to make sure we don't leak objects, and don't spew
    into demsg.
    
    Fixes: d0f37cf62979 ("drm/mode: move framebuffer reference into object.")
    Testcase: igt/kms_addfb_basic/invalid-*-prop*
    Cc: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 1649c33ba34299f66d4f5b5a86964897a2923851)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    BUG=chrome-os-partner:53839
    TEST=External monitors connected via MST hub or daisy-chain over DP should come up.
    
    Change-Id: I9bd7e29934d62b193406e6ef113c0753bed15060
    Reviewed-on: https://chromium-review.googlesource.com/421925
    Commit-Ready: Brian J Lovin <brian.j.lovin@intel.com>
    Tested-by: Chenglei Ren <chenglei.ren@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -396,9 +396,7 @@ struct drm_mode_object *drm_mode_object_find(struct drm_device *dev,
 {
 	struct drm_mode_object *obj = NULL;
 
-	/* Framebuffers are reference counted and need their own lookup
-	 * function.*/
-	WARN_ON(type == DRM_MODE_OBJECT_FB || type == DRM_MODE_OBJECT_BLOB);
+	WARN_ON(type == DRM_MODE_OBJECT_BLOB);
 	obj = _object_find(dev, id, type);
 	return obj;
 }
@@ -4927,7 +4925,7 @@ int drm_mode_obj_get_properties_ioctl(struct drm_device *dev, void *data,
 	}
 	if (!obj->properties) {
 		ret = -EINVAL;
-		goto out;
+		goto out_unref;
 	}
 
 	ret = get_properties(obj, file_priv->atomic,
@@ -4935,6 +4933,8 @@ int drm_mode_obj_get_properties_ioctl(struct drm_device *dev, void *data,
 			(uint64_t __user *)(unsigned long)(arg->prop_values_ptr),
 			&arg->count_props);
 
+out_unref:
+	drm_mode_object_unreference(obj);
 out:
 	drm_modeset_unlock_all(dev);
 	return ret;
@@ -4977,20 +4977,20 @@ int drm_mode_obj_set_property_ioctl(struct drm_device *dev, void *data,
 		goto out;
 	}
 	if (!arg_obj->properties)
-		goto out;
+		goto out_unref;
 
 	for (i = 0; i < arg_obj->properties->count; i++)
 		if (arg_obj->properties->properties[i]->base.id == arg->prop_id)
 			break;
 
 	if (i == arg_obj->properties->count)
-		goto out;
+		goto out_unref;
 
 	prop_obj = drm_mode_object_find(dev, arg->prop_id,
 					DRM_MODE_OBJECT_PROPERTY);
 	if (!prop_obj) {
 		ret = -ENOENT;
-		goto out;
+		goto out_unref;
 	}
 	property = obj_to_property(prop_obj);
 
@@ -5013,6 +5013,8 @@ int drm_mode_obj_set_property_ioctl(struct drm_device *dev, void *data,
 
 	drm_property_change_valid_put(property, ref);
 
+out_unref:
+	drm_mode_object_unreference(arg_obj);
 out:
 	drm_modeset_unlock_all(dev);
 	return ret;
