0dcf1e1ef2569b7ecd282599b0b0d6af67bdb63b
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-04-28 10:46:57 -0300

    UPSTREAM: dma-buf/sync_file: de-stage sync_file headers
    Move sync_file headers file to include/ dir.
    
    BUG=chromium:911825
    TEST=boot caroline, chell, run login_LoginSuccess, graphics_SanAngeles, cheets_StartAndroid
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 460bfc41fd52959311ed0328163f785e023857af)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I5d4afeefe4327991f798dce6e6fbbb033643285e

--- drivers/staging/android/sync_debug.c
+++ drivers/staging/android/sync_debug.c
@@ -27,7 +27,7 @@
 #include <linux/anon_inodes.h>
 #include <linux/ratelimit.h>
 #include <linux/time64.h>
-#include "sync_file.h"
+#include <linux/sync_file.h>
 #include "sw_sync.h"
 
 #ifdef CONFIG_DEBUG_FS
