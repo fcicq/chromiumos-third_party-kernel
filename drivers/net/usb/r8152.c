c5f5b41f7ce932a4f5c897dc262780ab00f480a4
hayeswang <hayeswang@realtek.com>
2016-06-13 17:49:37 +0800

    UPSTREAM: r8152: move the setting for the default speed
    Move calling set_speed() from open() to rtl_hw_phy_work_func_t().
    Then, we would set the default speed only for first initialization
    or after resuming.
    
    Besides, the set_speed() could handle the flag of PHY_RESET which
    would be set in rtl_ops.hw_phy_cfg().
    
    Signed-off-by: Hayes Wang <hayeswang@realtek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 9d21c0d83ea3def89b9cd33222b1c44c1af44b4d)
    
    BUG=chromium:768542
    TEST=netperf, repeated reboot, and network_EthernetStressPlug on coral
    
    Change-Id: I92aba0f4c7e612e24b5f4dda496c578c592ce716
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/864777
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -3057,6 +3057,10 @@ static void rtl_hw_phy_work_func_t(struct work_struct *work)
 
 	tp->rtl_ops.hw_phy_cfg(tp);
 
+	rtl8152_set_speed(tp, AUTONEG_ENABLE,
+			  tp->mii.supports_gmii ? SPEED_1000 : SPEED_100,
+			  DUPLEX_FULL);
+
 	mutex_unlock(&tp->control);
 
 	usb_autopm_put_interface(tp->intf);
@@ -3108,9 +3112,6 @@ static int rtl8152_open(struct net_device *netdev)
 
 	tp->rtl_ops.up(tp);
 
-	rtl8152_set_speed(tp, AUTONEG_ENABLE,
-			  tp->mii.supports_gmii ? SPEED_1000 : SPEED_100,
-			  DUPLEX_FULL);
 	netif_carrier_off(netdev);
 	netif_start_queue(netdev);
 	set_bit(WORK_ENABLE, &tp->flags);
@@ -3620,10 +3621,6 @@ static int rtl8152_resume(struct usb_interface *intf)
 			napi_enable(&tp->napi);
 		} else {
 			tp->rtl_ops.up(tp);
-			rtl8152_set_speed(tp, AUTONEG_ENABLE,
-					  tp->mii.supports_gmii ?
-					  SPEED_1000 : SPEED_100,
-					  DUPLEX_FULL);
 			netif_carrier_off(tp->netdev);
 			set_bit(WORK_ENABLE, &tp->flags);
 		}
