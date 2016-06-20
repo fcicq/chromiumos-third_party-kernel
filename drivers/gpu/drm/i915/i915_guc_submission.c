ec2bb43357e2fc665a2507bd0da1a090f5e995a4
Dave Gordon <david.s.gordon@intel.com>
2016-06-20 15:18:07 +0100

    UPSTREAM: drm/i915/guc: index host arrays by i915 engine ID, not guc_id
    The ONLY places that guc_id (aka hw_id) should be used are those where
    the value or address is determined by and shared with the GuC firmware;
    specifically, when filling in the GuC-context-descriptor or the GuC
    addon data, or putting an entry in the GuC's work queue.
    
    It need not (and therefore should not) be used to index GuC statistics
    or similar host-managed tracking data. In particular, i915_guc_submit()
    produces (and debugfs decodes) GuC submission statistics which should be
    indexed by driver-engine-id rather then guc-engine-id.
    
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1466432287-5799-1-git-send-email-david.s.gordon@intel.com
    (cherry picked from commit 0b63bb1412bf9b2d72d60e38d07bd9dfe657a382)
    from drm-intel-next-2016-07-11
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
    Change-Id: I1d4373e5481f60f4807ef4ddfb2aa45d5c227306
    Reviewed-on: https://chromium-review.googlesource.com/460210
    Commit-Ready: Wayne Boyer <wayne.boyer@intel.com>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/i915/i915_guc_submission.c
+++ drivers/gpu/drm/i915/i915_guc_submission.c
@@ -582,7 +582,7 @@ static void guc_add_workqueue_item(struct i915_guc_client *gc,
  */
 int i915_guc_submit(struct drm_i915_gem_request *rq)
 {
-	unsigned int engine_id = rq->engine->guc_id;
+	unsigned int engine_id = rq->engine->id;
 	struct intel_guc *guc = &rq->i915->guc;
 	struct i915_guc_client *client = guc->execbuf_client;
 	int b_ret;
