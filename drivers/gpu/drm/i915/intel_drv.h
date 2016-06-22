5e2b36bab645ff39b8a49b96c19a15742f2f761f
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-06-22 21:57:01 +0300

    BACKPORT: drm/i915: Add output_types bitmask into the crtc state
    Rather than looping through encoders to see which encoder types
    are being driven by the pipe, add an output_types bitmask into
    the crtc state and populate it prior to compute_config and during
    state readout.
    
    v2: Determine output_types before .compute_config() hooks are called
    
    Signed-off-by: Ville Syrjl <ville.syrjala@linux.intel.com>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1466621833-5054-4-git-send-email-ville.syrjala@linux.intel.com
    (cherry picked from commit 253c84c82aaeb9c68e07a9ef0df0a3172b3d37fc)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    Conflicts:
    Resolve conflicts due to has_dsi_encoder
    drivers/gpu/drm/i915/intel_display.c
    drivers/gpu/drm/i915/intel_drv.h
    
    BUG=chrome-os-partner:53839
    TEST=External monitors connected via MST hub or daisy-chain over DP should come up.
    
    Change-Id: I33ddee57121f732a77f0de516baffc246bc80252
    Reviewed-on: https://chromium-review.googlesource.com/422006
    Commit-Ready: Brian J Lovin <brian.j.lovin@intel.com>
    Tested-by: Chenglei Ren <chenglei.ren@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -458,6 +458,11 @@ struct intel_crtc_state {
 	 * accordingly. */
 	bool has_dp_encoder;
 
+	/* Bitmask of encoder types (enum intel_output_type)
+	 * driven by the pipe.
+	 */
+	unsigned int output_types;
+
 	/* Whether we should send NULL infoframes. Required for audio. */
 	bool has_hdmi_sink;
 
