625a3a8d00e92a066bfe2e867227dcaeb9fc4805
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2016-06-29 16:09:30 +0100

    UPSTREAM: drm/i915: Compact gen8_ring_sync
    Store the semaphore offset in a temporary variable to avoid
    having to get the VMA offset twice.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    (cherry picked from commit c38c651b39a642d5900e9c8122cfc9bff4343538)
    from drm-intel-next-2016-07-11
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
    Change-Id: I4e9220b7f7ade2e960753ec62c404a811531f9f7

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1536,6 +1536,7 @@ gen8_ring_sync(struct drm_i915_gem_request *waiter_req,
 {
 	struct intel_engine_cs *waiter = waiter_req->engine;
 	struct drm_i915_private *dev_priv = waiter_req->i915;
+	u64 offset = GEN8_WAIT_OFFSET(waiter, signaller->id);
 	struct i915_hw_ppgtt *ppgtt;
 	int ret;
 
@@ -1547,10 +1548,8 @@ gen8_ring_sync(struct drm_i915_gem_request *waiter_req,
 				MI_SEMAPHORE_GLOBAL_GTT |
 				MI_SEMAPHORE_SAD_GTE_SDD);
 	intel_ring_emit(waiter, seqno);
-	intel_ring_emit(waiter,
-			lower_32_bits(GEN8_WAIT_OFFSET(waiter, signaller->id)));
-	intel_ring_emit(waiter,
-			upper_32_bits(GEN8_WAIT_OFFSET(waiter, signaller->id)));
+	intel_ring_emit(waiter, lower_32_bits(offset));
+	intel_ring_emit(waiter, upper_32_bits(offset));
 	intel_ring_advance(waiter);
 
 	/* When the !RCS engines idle waiting upon a semaphore, they lose their
