a64dfea98bc674026ba0800eb44caf528b0c7635
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2016-08-09 17:04:13 +0200

    UPSTREAM: drm/i915: Convert intel_dp to use atomic state
    Slightly less straightforward. Some of the drrs calls are done from
    workers or from intel_ddi.c, pass along crtc_state when we can,
    or crtc->config when we can't.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1470755054-32699-15-git-send-email-maarten.lankhorst@linux.intel.com
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 85cb48a1651ee7cbc49468776d3078418f7827c5)
    from drm-intel-next-2016-09-02
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
    Change-Id: Ib57a990676a8c7ded9819cb6c1ffb72421b8f1cb

--- drivers/gpu/drm/i915/intel_ddi.c
+++ drivers/gpu/drm/i915/intel_ddi.c
@@ -1815,7 +1815,7 @@ static void intel_enable_ddi(struct intel_encoder *intel_encoder,
 
 		intel_edp_backlight_on(intel_dp);
 		intel_psr_enable(intel_dp);
-		intel_edp_drrs_enable(intel_dp);
+		intel_edp_drrs_enable(intel_dp, pipe_config);
 	}
 
 	if (intel_crtc->config->has_audio) {
@@ -1843,7 +1843,7 @@ static void intel_disable_ddi(struct intel_encoder *intel_encoder,
 	if (type == INTEL_OUTPUT_EDP) {
 		struct intel_dp *intel_dp = enc_to_intel_dp(encoder);
 
-		intel_edp_drrs_disable(intel_dp);
+		intel_edp_drrs_disable(intel_dp, old_crtc_state);
 		intel_psr_disable(intel_dp);
 		intel_edp_backlight_off(intel_dp);
 	}
