adaf8fb432ee98c12732a8113da7761412b7f7fb
Chris Wilson <chris@chris-wilson.co.uk>
2016-10-28 13:58:46 +0100

    BACKPORT: drm/i915: Combine seqno + tracking into a global timeline struct
    Our timelines are more than just a seqno. They also provide an ordered
    list of requests to be executed. Due to the restriction of handling
    individual address spaces, we are limited to a timeline per address
    space but we use a fence context per engine within.
    
    Our first step to introducing independent timelines per context (i.e. to
    allow each context to have a queue of requests to execute that have a
    defined set of dependencies on other requests) is to provide a timeline
    abstraction for the global execution queue.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161028125858.23563-23-chris@chris-wilson.co.uk
    (cherry picked from commit 73cb97010d4fdd2a29f00cac14d206c7641c23d2)
    from drm-intel-next-2016-11-08
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    
     Conflicts: [Tarun]
    
    	["0ad35fe drm/i915: gvt: Introduce the basic architecture of
    	GVT-g" Not needed. Retain HEAD]
    	drivers/gpu/drm/i915/i915_drv.c
    	[Minor ctx delta. Accept the change]
    	drivers/gpu/drm/i915/intel_ringbuffer.h
    
    Change-Id: I2137aac6abb6191a8a7d2def6b4550c76129eb58

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -62,6 +62,7 @@
 #include "i915_gem_gtt.h"
 #include "i915_gem_render_state.h"
 #include "i915_gem_request.h"
+#include "i915_gem_timeline.h"
 
 /* General customization:
  */
@@ -1808,7 +1809,6 @@ struct drm_i915_private {
 	struct i915_gem_context *kernel_context;
 	struct intel_engine_cs *engine[I915_NUM_ENGINES];
 	struct i915_vma *semaphore;
-	u32 next_seqno;
 
 	struct drm_dma_handle *status_page_dmah;
 	struct resource mch_res;
@@ -2065,6 +2065,9 @@ struct drm_i915_private {
 	struct {
 		void (*resume)(struct drm_i915_private *);
 		void (*cleanup_engine)(struct intel_engine_cs *engine);
+
+		struct list_head timelines;
+		struct i915_gem_timeline global_timeline;
 
 		/**
 		 * Is the GPU currently considered idle, or busy executing
@@ -3126,7 +3129,7 @@ int i915_gem_get_aperture_ioctl(struct drm_device *dev, void *data,
 				struct drm_file *file_priv);
 int i915_gem_wait_ioctl(struct drm_device *dev, void *data,
 			struct drm_file *file_priv);
-void i915_gem_load_init(struct drm_device *dev);
+int i915_gem_load_init(struct drm_device *dev);
 void i915_gem_load_cleanup(struct drm_device *dev);
 void i915_gem_load_init_fences(struct drm_i915_private *dev_priv);
 void *i915_gem_object_alloc(struct drm_device *dev);
@@ -3295,7 +3298,7 @@ void i915_gem_track_fb(struct drm_i915_gem_object *old,
 		       struct drm_i915_gem_object *new,
 		       unsigned frontbuffer_bits);
 
-int __must_check i915_gem_set_seqno(struct drm_device *dev, u32 seqno);
+int __must_check i915_gem_set_global_seqno(struct drm_device *dev, u32 seqno);
 
 struct drm_i915_gem_request *
 i915_gem_find_active_request(struct intel_engine_cs *engine);
