8bb0513b552ae44a724dd323e537ac8102a9dc6d
Mark Rutland <mark.rutland@arm.com>
2016-11-03 20:23:03 +0000

    UPSTREAM: arm64: thread_info remove stale items
    We have a comment claiming __switch_to() cares about where cpu_context
    is located relative to cpu_domain in thread_info. However arm64 has
    never had a thread_info::cpu_domain field, and neither __switch_to nor
    cpu_switch_to care where the cpu_context field is relative to others.
    
    Additionally, the init_thread_info alias is never used anywhere in the
    kernel, and will shortly become problematic when thread_info is moved
    into task_struct.
    
    This patch removes both.
    
    BUG=chromium:836330
    TEST=Build and run
    
    Change-Id: I21d0a78ab29042cd49719a9aa4c7103325f22cf1
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit dcbe02855f048fdf1e13ebc697e83c8d297f9f5a)
    Signed-off-by: Zubin Mithra <zsm@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1037672
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -43,7 +43,6 @@ typedef unsigned long mm_segment_t;
 
 /*
  * low level task data that entry.S needs immediate access to.
- * __switch_to() assumes cpu_context follows immediately after cpu_domain.
  */
 struct thread_info {
 	unsigned long		flags;		/* low level flags */
@@ -87,7 +86,6 @@ extern void * const compat_sys_call_table[];
 	INIT_THREAD_INFO_SYSCALL					\
 }
 
-#define init_thread_info	(init_thread_union.thread_info)
 #define init_stack		(init_thread_union.stack)
 
 /*
