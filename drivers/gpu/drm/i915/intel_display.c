4a02f0eccefd681ec96ad4e28d91a834665df61d
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-11-18 21:52:53 +0200

    UPSTREAM: drm/i915: Set fb->dev early on for inherited fbs
    We want the fbs inherited from the BIOS to be more or less fully working
    prior to actually registering them. This will allow us to just pass the
    fb to various helper function instead of having to pass all the
    different parameters separately.
    
    Cc: intel-gfx@lists.freedesktop.org
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1479498793-31021-18-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit d2e9f5fcc646d3b2e41cfef79b3c8e720d28e230)
    drm-intel-next-2017-01-09
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    
    Change-Id: Id7567d74994eed1f5fbeab1151e1f16393ebedbf

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -8689,6 +8689,8 @@ i9xx_get_initial_plane_config(struct intel_crtc *crtc,
 
 	fb = &intel_fb->base;
 
+	fb->dev = dev;
+
 	if (INTEL_GEN(dev_priv) >= 4) {
 		if (val & DISPPLANE_TILED) {
 			plane_config->tiling = I915_TILING_X;
@@ -9741,6 +9743,8 @@ skylake_get_initial_plane_config(struct intel_crtc *crtc,
 
 	fb = &intel_fb->base;
 
+	fb->dev = dev;
+
 	val = I915_READ(PLANE_CTL(pipe, 0));
 	if (!(val & PLANE_CTL_ENABLE))
 		goto error;
@@ -9852,6 +9856,8 @@ ironlake_get_initial_plane_config(struct intel_crtc *crtc,
 	}
 
 	fb = &intel_fb->base;
+
+	fb->dev = dev;
 
 	if (INTEL_GEN(dev_priv) >= 4) {
 		if (val & DISPPLANE_TILED) {
