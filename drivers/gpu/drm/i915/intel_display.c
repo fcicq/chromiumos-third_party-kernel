ef65d57ab022bf1fc590cbd5a722c39689e44f5a
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-11-22 18:01:57 +0200

    UPSTREAM: drm/i915: Add crtc->plane_ids_mask
    Add a mask of which planes are available for each pipe. This doesn't
    quite work for old platforms with dynamic plane<->pipe assignment, but
    as we don't support that sort of stuff (yet) we can get away with it.
    
    The main use I have for this is the for_each_plane_id_on_crtc() macro
    for iterating over all possible planes on the crtc. I suppose we could
    not add the mask, and instead iterate by comparing intel_plane->pipe
    but then we'd need a local intel_plane variable which is just
    unnecessary clutter in some cases. But I'm not hung up on this, so if
    people prefer the other option I could be convinced to use it.
    
    v2: Use BIT() in the iterator macro too (Paulo)
    
    Signed-off-by: Ville Syrjl <ville.syrjala@linux.intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1479830524-7882-3-git-send-email-ville.syrjala@linux.intel.com
    (cherry picked from commit d97d7b48b644378349f2566dd83739e0be3ba410)
    drm-intel-next-2016-12-05
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
    Change-Id: Ia48efe73eb24305ae4faa1e60c5939311a752c2c
    Reviewed-on: https://chromium-review.googlesource.com/560352
    Commit-Ready: Sean Paul <seanpaul@chromium.org>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -15384,6 +15384,7 @@ static int intel_crtc_init(struct drm_i915_private *dev_priv, enum pipe pipe)
 		ret = PTR_ERR(primary);
 		goto fail;
 	}
+	intel_crtc->plane_ids_mask |= BIT(primary->id);
 
 	for_each_sprite(dev_priv, pipe, sprite) {
 		struct intel_plane *plane;
@@ -15393,6 +15394,7 @@ static int intel_crtc_init(struct drm_i915_private *dev_priv, enum pipe pipe)
 			ret = PTR_ERR(plane);
 			goto fail;
 		}
+		intel_crtc->plane_ids_mask |= BIT(plane->id);
 	}
 
 	cursor = intel_cursor_plane_create(dev_priv, pipe);
@@ -15400,6 +15402,7 @@ static int intel_crtc_init(struct drm_i915_private *dev_priv, enum pipe pipe)
 		ret = PTR_ERR(cursor);
 		goto fail;
 	}
+	intel_crtc->plane_ids_mask |= BIT(cursor->id);
 
 	ret = drm_crtc_init_with_planes(&dev_priv->drm, &intel_crtc->base,
 					&primary->base, &cursor->base,
