5a8ecef90077ef8b7fa893b469a63014757946e1
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-14 00:28:26 +0000

    UPSTREAM: drm/i915: Eliminate superfluous i915_ggtt_view_rotated
    It is only being used to clear a struct and set the type, after which it
    is overwritten. Since we no longer check the unset bits of the union,
    skipping the clear is permissible.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170114002827.31315-6-chris@chris-wilson.co.uk
    (cherry picked from commit 7b92c047bae2210874d64ae8bbb56fbd18ab6731)
    drm-intel-next-2017-01-23
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    
    Change-Id: I30b9067c23fb8e99d335b645017559570ce0565d

--- drivers/gpu/drm/i915/i915_gem_gtt.c
+++ drivers/gpu/drm/i915/i915_gem_gtt.c
@@ -102,9 +102,6 @@ i915_get_ggtt_vma_pages(struct i915_vma *vma);
 const struct i915_ggtt_view i915_ggtt_view_normal = {
 	.type = I915_GGTT_VIEW_NORMAL,
 };
-const struct i915_ggtt_view i915_ggtt_view_rotated = {
-	.type = I915_GGTT_VIEW_ROTATED,
-};
 
 int intel_sanitize_enable_ppgtt(struct drm_i915_private *dev_priv,
 			       	int enable_ppgtt)
