5624429940b96753bd00fe9493f68049e6966944
Kevin Cernekee <cernekee@chromium.org>
2017-01-27 16:15:55 -0800

    CHROMIUM: Implement Android permission overrides for PPTP VPN tunnels
    Android kernels override the capable() checks for CAP_NET_RAW and
    CAP_NET_ADMIN globally[0], allowing processes with GIDs AID_NET_RAW
    or AID_NET_ADMIN to perform privileged operations.  Rather than
    changing the global permission checks (which makes it harder to
    reason about the extent of the risk), Chrome OS supports a limited,
    well-defined subset of this functionality through the per-netns
    android_paranoid sysctl.
    
    Extend these permission overrides to cover the following cases, which
    are needed to support Android's builtin PPTP VPN client:
    
     - SIOCSIF* MTU and legacy IP configuration
     - SO_BINDTODEVICE
     - Opening /dev/ppp
     - Creating RAW sockets (for GRE)
    
    [0] https://android.googlesource.com/kernel/common/+/9a5e2c00592e2dcb648a1ae2ad7906b56b93448d%5E%21/
    
    BUG=chromium:207904
    TEST=none
    
    Change-Id: I0e46d6fd91ecd3aa041363e29f7aad56261e6fb8
    Signed-off-by: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/802775
    Reviewed-by: Abhishek Bhardwaj <abhishekbh@google.com>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -65,6 +65,7 @@
 #include <net/rtnetlink.h>
 #include <net/net_namespace.h>
 #include <net/addrconf.h>
+#include <net/sock.h>
 
 #include "fib_lookup.h"
 
@@ -928,7 +929,7 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 
 	case SIOCSIFFLAGS:
 		ret = -EPERM;
-		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
+		if (!android_ns_capable(net, CAP_NET_ADMIN))
 			goto out;
 		break;
 	case SIOCSIFADDR:	/* Set interface address (and family) */
@@ -937,7 +938,7 @@ int devinet_ioctl(struct net *net, unsigned int cmd, void __user *arg)
 	case SIOCSIFNETMASK: 	/* Set the netmask for the interface */
 	case SIOCKILLADDR:	/* Nuke all sockets on this address */
 		ret = -EPERM;
-		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
+		if (!android_ns_capable(net, CAP_NET_ADMIN))
 			goto out;
 		ret = -EINVAL;
 		if (sin->sin_family != AF_INET)
