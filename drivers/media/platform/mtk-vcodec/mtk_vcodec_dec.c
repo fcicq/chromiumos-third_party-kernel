c65690b35ccf87f951fd2e2b1782354c62444b61
Wu-Cheng Li <wuchengli@google.com>
2017-03-03 16:22:45 +0800

    FROMLIST: mtk-vcodec: check the vp9 decoder buffer index from VPU.
    VPU firmware has a bug and may return invalid buffer index for
    some vp9 videos. Check the buffer indexes before accessing the
    buffer.
    
    (cherry-picked from https://patchwork.linuxtv.org/patch/39859/)
    Signed-off-by: Wu-Cheng Li <wuchengli@chromium.org>
    
    BUG=chromium:696904
    BUG=b/35936915
    TEST=Play https://cdn.lyft.com/brochure/videos/hero-video-opt.webm
         on elm. Play a VP9 youtube video. Run VDA unittest.
    
    Change-Id: I398db363b876ae7906bcba9a6515af6f5cc5ea3f
    Reviewed-on: https://chromium-review.googlesource.com/449494
    Commit-Ready: Wu-cheng Li <wuchengli@chromium.org>
    Tested-by: Wu-cheng Li <wuchengli@chromium.org>
    Reviewed-by: Wu-cheng Li <wuchengli@chromium.org>
    (cherry picked from commit 192de100e380700d07d4c4a76042f1892b7dc253)
    Reviewed-on: https://chromium-review.googlesource.com/453869

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.c
@@ -416,6 +416,11 @@ static void mtk_vdec_worker(struct work_struct *work)
 				dst_buf->index,
 				ret, res_chg);
 			src_buf = v4l2_m2m_src_buf_remove(ctx->m2m_ctx);
+			if (ret == -EIO) {
+				mutex_lock(&ctx->lock);
+				src_buf_info->error = true;
+				mutex_unlock(&ctx->lock);
+			}
 			v4l2_m2m_buf_done(&src_buf_info->vb,
 					VB2_BUF_STATE_ERROR);
 		} else if (res_chg == false) {
@@ -1048,8 +1053,16 @@ static void vb2ops_vdec_buf_queue(struct vb2_buffer *vb)
 				 */
 				int log_level = ret ? 0 : 1;
 				src_buf = v4l2_m2m_src_buf_remove(ctx->m2m_ctx);
-				v4l2_m2m_buf_done(to_vb2_v4l2_buffer(src_buf),
-						VB2_BUF_STATE_DONE);
+				if (ret == -EIO) {
+					mtk_v4l2_err("[%d] Unrecoverable error in vdec_if_decode.",
+							ctx->id);
+					ctx->state = MTK_STATE_ABORT;
+					v4l2_m2m_buf_done(to_vb2_v4l2_buffer(src_buf),
+							VB2_BUF_STATE_ERROR);
+				} else {
+					v4l2_m2m_buf_done(to_vb2_v4l2_buffer(src_buf),
+							VB2_BUF_STATE_DONE);
+				}
 				mtk_v4l2_debug(log_level,
 						"[%d] vdec_if_decode() src_buf=%d, size=%zu, fail=%d, res_chg=%d",
 						ctx->id, src_buf->index,
@@ -1119,18 +1132,24 @@ static void vb2ops_vdec_buf_queue(struct vb2_buffer *vb)
 static void vb2ops_vdec_buf_finish(struct vb2_buffer *vb)
 {
 	struct mtk_vcodec_ctx *ctx = vb2_get_drv_priv(vb->vb2_queue);
+	struct vb2_v4l2_buffer *vb2_v4l2;
+	struct mtk_video_dec_buf *buf;
+	bool buf_error;
 
-	if (vb->vb2_queue->type ==
-		V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE) {
-		struct vb2_v4l2_buffer *vb2_v4l2;
-		struct mtk_video_dec_buf *buf;
+	vb2_v4l2 = container_of(vb, struct vb2_v4l2_buffer, vb2_buf);
+	buf = container_of(vb2_v4l2, struct mtk_video_dec_buf, vb);
 
-		vb2_v4l2 = container_of(vb, struct vb2_v4l2_buffer, vb2_buf);
-		buf = container_of(vb2_v4l2, struct mtk_video_dec_buf, vb);
-		mutex_lock(&ctx->lock);
+	mutex_lock(&ctx->lock);
+	if (vb->vb2_queue->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE) {
 		buf->queued_in_v4l2 = false;
 		buf->queued_in_vb2 = false;
-		mutex_unlock(&ctx->lock);
+	}
+	buf_error = buf->error;
+	mutex_unlock(&ctx->lock);
+
+	if (buf_error) {
+		mtk_v4l2_err("Unrecoverable error on buffer.");
+		ctx->state = MTK_STATE_ABORT;
 	}
 }
 
