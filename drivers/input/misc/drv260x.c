2961c26bc528985ab07912a8183301453bcdc751
Jingkui Wang <jkwang@google.com>
2017-03-08 16:38:05 -0800

    FROMLIST: drivers: Remove OF dependency in drv260x driver
    As the driver is using generic device properties, it should work
    properly when CONFIG_OF is turned off. This patch removes the
    ifdef CONFIGOF and make sure the driver always have of_match_table.
    
    Signed-off-by: Jingkui Wang <jkwang@google.com>
    (am from https://patchwork.kernel.org/patch/9612249/)
    
    BUG=b:36071081
    TEST=update kernel on pbody and run touchkb_haptic_test
    Change-Id: Ifb9b1cecf645e4185f8c70002c15ea8fa5cecee4
    Reviewed-on: https://chromium-review.googlesource.com/451899
    Commit-Ready: Jingkui Wang <jkwang@google.com>
    Tested-by: Jingkui Wang <jkwang@google.com>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/input/misc/drv260x.c
+++ drivers/input/misc/drv260x.c
@@ -654,7 +654,6 @@ static const struct i2c_device_id drv260x_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, drv260x_id);
 
-#ifdef CONFIG_OF
 static const struct of_device_id drv260x_of_match[] = {
 	{ .compatible = "ti,drv2604", },
 	{ .compatible = "ti,drv2604l", },
@@ -663,14 +662,13 @@ static const struct of_device_id drv260x_of_match[] = {
 	{ }
 };
 MODULE_DEVICE_TABLE(of, drv260x_of_match);
-#endif
 
 static struct i2c_driver drv260x_driver = {
 	.probe		= drv260x_probe,
 	.driver		= {
 		.name	= "drv260x-haptics",
 		.owner	= THIS_MODULE,
-		.of_match_table = of_match_ptr(drv260x_of_match),
+		.of_match_table = drv260x_of_match,
 		.pm	= &drv260x_pm_ops,
 	},
 	.id_table = drv260x_id,
