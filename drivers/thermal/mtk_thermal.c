fcicq/chromiumos-third_party-kernel

commit ecda7adc095eec9049feb54b1109346275cc7294

CHROMIUM: thermal: mediatek: skip svs late_init by default
Previously, we would always do mtk_svs_late_init, even if mtk_thermal
itself didn't probe (ie, there was no "mediatek,mt8173-thermal" .dts node).
In such a case, mtk_svs_late_init() would check the global skip_svs_init,
which was false by default, and therefore proceed.  Eventually the system
would crash because mtk thermal / SVS had not been probed.

This patch changes the global to allow_svs_late_init so we can still keep
it in .bss (eg default false), but have it protect us from this potential
situation.

Fixes: 5be71f4c13d3 ("BACKPORT: FROMLIST: PM / AVS: thermal: MT8173:
 Introduce support for SVS engine")

Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>

BUG=chrome-os-partner:60805
TEST=Remove thermal node from mt8173.dtsi, build and boot to UI.
 => No system crash at boot

Change-Id: I7409be8d5cb2a2d66ebc16df568a271bbb9a1204
Reviewed-on: https://chromium-review.googlesource.com/448776
Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
Tested-by: Yidi Lin <yidi.lin@mediatek.com>
Reviewed-by: Yidi Lin <yidi.lin@mediatek.com>
Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

drivers/thermal/mtk_thermal.c
@@ -1236,7 +1236,7 @@ static int cpufreq_svs_notifier(struct notifier_block *nb, unsigned long event,
 	return NOTIFY_OK;
 }
 
-static bool skip_svs_init;
+static bool allow_svs_late_init;
 
 static struct notifier_block svs_cpufreq_notifier_block = {
 	.notifier_call = cpufreq_svs_notifier,
@@ -1252,7 +1252,7 @@ static int mtk_svs_late_init(void)
 {
 	int ret, i;
 
-	if (skip_svs_init)
+	if (allow_svs_late_init)
 		return -EINVAL;
 
 	ret = cpufreq_register_notifier(&svs_cpufreq_notifier_block,
@@ -1292,7 +1292,7 @@ static int mtk_svs_probe(struct platform_device *pdev)
 			return PTR_ERR(mt->svs_pll);
 
 		pr_err("Failed to get SVS PLL clock\n");
-		goto out_skip_svs_init;
+		return ret;
 	}
 
 	mt->svs_mux = devm_clk_get(&pdev->dev, "svs_mux");
@@ -1301,7 +1301,7 @@ static int mtk_svs_probe(struct platform_device *pdev)
 			return PTR_ERR(mt->svs_mux);
 
 		pr_err("Failed to get SVS MUX clock\n");
-		goto out_skip_svs_init;
+		return ret;
 	}
 
 	for (i = 0; i < MT8173_NUM_SVS_BANKS; i++) {
@@ -1314,7 +1314,7 @@ static int mtk_svs_probe(struct platform_device *pdev)
 				return PTR_ERR(reg);
 
 			pr_err("Failed to get %s regulator\n", supply);
-			goto out_skip_svs_init;
+			return ret;
 		}
 
 		svs_banks[i].reg = reg;
@@ -1323,11 +1323,9 @@ static int mtk_svs_probe(struct platform_device *pdev)
 
 	ret = mtk_svs_get_calibration_data(mt->dev, mt);
 	if (ret) {
-		if (ret == -EPROBE_DEFER)
-			return ret;
-
-		pr_err("Failed to get SVS calibration data\n");
-		goto out_skip_svs_init;
+		if (ret != -EPROBE_DEFER)
+			pr_err("Failed to get SVS calibration data\n");
+		return ret;
 	}
 
 	mt->svs_irq = platform_get_irq(pdev, 1);
@@ -1337,14 +1335,13 @@ static int mtk_svs_probe(struct platform_device *pdev)
 					"mtk-svs", mt);
 	if (ret) {
 		pr_err("Failed to get SVS IRQ\n");
-		goto out_skip_svs_init;
+		return ret;
 	}
 
-	return 0;
+	/* SVS has successfully probed, allow SVS late init */
+	allow_svs_late_init = true;
 
-out_skip_svs_init:
-	skip_svs_init = true;
-	return ret;
+	return 0;
 }
 
 static int mtk_thermal_probe(struct platform_device *pdev)

commit 43b3026c123e6a6bdce6d77ea8681aa76def694d

FIXUP: CHROMIUM: thermal: mediatek: skip svs late_init by default
Whoops, we should abort mtk_svs_late_init if we do NOT allow svs_late_init.

Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
Fixes: ecda7adc095e CHROMIUM: thermal: mediatek: skip svs late_init by default

BUG=b:36271185
TEST=boot elm to UI
TEST=run multi_v7_defconfig images with various targets on qemu
  => No crash in mtk_svs_hw_init

Change-Id: I093f91632b23cf10338acb880188b2c7bd1c3cb3
Reviewed-on: https://chromium-review.googlesource.com/456540
Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
Tested-by: Yidi Lin <yidi.lin@mediatek.com>
Reviewed-by: Guenter Roeck <groeck@chromium.org>

drivers/thermal/mtk_thermal.c
@@ -1253,7 +1253,7 @@ static int mtk_svs_late_init(void)
 {
 	int ret, i;
 
-	if (allow_svs_late_init)
+	if (!allow_svs_late_init)
 		return -EINVAL;
 
 	ret = cpufreq_register_notifier(&svs_cpufreq_notifier_block,