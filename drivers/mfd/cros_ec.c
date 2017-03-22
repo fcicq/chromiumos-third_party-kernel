BlissRoms-x86/kernel_common

CHROMIUM: cros_ec: Notify EC of suspend state
Notify EC when going to or returning from suspend so that proper actions
related to wake events can be taken.

BUG=chrome-os-partner:55875
BRANCH=None
TEST=Manual on kevin, verify EC_CMD_HOST_SLEEP_EVENT w/ 0 param is sent
on boot, S3_SUSPEND param on suspend, and S3_RESUME param on resume.

Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
Change-Id: I150c05c576a8e66f09f7f82e13dab92892f5d862
Reviewed-on: https://chromium-review.googlesource.com/370303
Commit-Ready: Shawn N <shawnn@chromium.org>
Tested-by: Shawn N <shawnn@chromium.org>
Reviewed-by: Douglas Anderson <dianders@chromium.org>

drivers/mfd/cros_ec.c
@@ -17,12 +17,13 @@
  * battery charging and regulator control, firmware update.
  */
 
-#include <linux/of_platform.h>
 #include <linux/interrupt.h>
-#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/cros_ec.h>
+#include <linux/of_platform.h>
+#include <linux/slab.h>
+#include <linux/suspend.h>
 
 #include <asm/unaligned.h>
 
@@ -126,6 +127,24 @@ static irqreturn_t ec_irq_thread(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+static int cros_ec_sleep_event(struct cros_ec_device *ec_dev, u8 sleep_event)
+{
+	struct {
+		struct cros_ec_command msg;
+		struct ec_params_host_sleep_event data;
+	} __packed buf;
+	struct cros_ec_command *msg = &buf.msg;
+	struct ec_params_host_sleep_event *req = &buf.data;
+
+	memset(&buf, 0, sizeof(buf));
+	req->sleep_event = sleep_event;
+	msg->command = EC_CMD_HOST_SLEEP_EVENT;
+	msg->version = 0;
+	msg->outsize = sizeof(req);
+
+	return cros_ec_cmd_xfer(ec_dev, msg);
+}
+
 static struct cros_ec_dev_platform pd_p = {
 	.ec_name = CROS_EC_DEV_PD_NAME,
 	.cmd_offset = EC_CMD_PASSTHRU_OFFSET(CROS_EC_DEV_PD_INDEX),
@@ -214,6 +233,15 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 		}
 	}
 
+	/*
+	 * Clear sleep event - this will fail harmlessly on platforms that
+	 * don't implement the sleep event host command.
+	 */
+	err = cros_ec_sleep_event(ec_dev, 0);
+	if (err < 0)
+		dev_dbg(ec_dev->dev, "Error %d clearing sleep event to ec",
+			err);
+
 	dev_info(dev, "Chrome EC device registered\n");
 
 	return 0;
@@ -237,6 +265,12 @@ EXPORT_SYMBOL(cros_ec_remove);
 int cros_ec_suspend(struct cros_ec_device *ec_dev)
 {
 	struct device *dev = ec_dev->dev;
+	int ret;
+
+	ret = cros_ec_sleep_event(ec_dev, HOST_SLEEP_EVENT_S3_SUSPEND);
+	if (ret < 0)
+		dev_dbg(ec_dev->dev, "Error %d sending suspend event to ec",
+			ret);
 
 	if (device_may_wakeup(dev))
 		ec_dev->wake_enabled = !enable_irq_wake(ec_dev->irq);
@@ -258,8 +292,15 @@ static void cros_ec_drain_events(struct cros_ec_device *ec_dev)
 
 int cros_ec_resume(struct cros_ec_device *ec_dev)
 {
+	int ret;
+
 	ec_dev->suspended = false;
 	enable_irq(ec_dev->irq);
+
+	ret = cros_ec_sleep_event(ec_dev, HOST_SLEEP_EVENT_S3_RESUME);
+	if (ret < 0)
+		dev_dbg(ec_dev->dev, "Error %d sending resume event to ec",
+			ret);
 
 	/*
 	 * In some case, we need to distinguish events that occur during

fcicq/chromiumos-third_party-kernel

Cherry picked from
commit 6a3c9585ddd5 ("CHROMIUM: cros_ec: Notify EC of suspend state")
with addition of the EC command interface.
Modified to match how 3.14 fills EC command.
BUG=chrome-os-partner:55875,chrome-os-partner:60882
TEST=Verified on Samus.
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/440428

drivers/mfd/cros_ec.c
@@ -17,15 +17,16 @@
  * battery charging and regulator control, firmware update.
  */
 
+#include <linux/delay.h>
 #include <linux/interrupt.h>
-#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/cros_ec.h>
 #include <linux/mfd/cros_ec_commands.h>
 #include <linux/module.h>
 #include <linux/of_platform.h>
-#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/suspend.h>
 
 #include "cros_ec_dev.h"
 
@@ -277,6 +278,20 @@ static int cros_ec_probe_all(struct cros_ec_device *ec_dev)
 	return 0;
 }
 
+static int cros_ec_sleep_event(struct cros_ec_device *ec_dev, u8 sleep_event)
+{
+	struct cros_ec_command msg;
+	struct ec_params_host_sleep_event data;
+
+	memset(&msg, 0, sizeof(msg));
+	data.sleep_event = sleep_event;
+	msg.command = EC_CMD_HOST_SLEEP_EVENT;
+	msg.outdata = (u8 *)&data;
+	msg.outsize = sizeof(data);
+
+	return cros_ec_cmd_xfer(ec_dev, &msg);
+}
+
 int cros_ec_prepare_tx(struct cros_ec_device *ec_dev,
 		       struct cros_ec_command *msg)
 {
@@ -504,6 +519,14 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 	if (err && err != -ENODEV)
 		dev_err(dev, "failed to add cros-ec-accel mfd devices\n");
 
+	/*
+	 * Clear sleep event - this will fail harmlessly on platforms that
+	 * don't implement the sleep event host command.
+	 */
+	err = cros_ec_sleep_event(ec_dev, 0);
+	if (err < 0)
+		dev_dbg(dev, "Error %d clearing sleep event to ec", err);
+
 	dev_info(dev, "Chrome EC device registered\n");
 
 	return 0;
@@ -521,6 +544,12 @@ EXPORT_SYMBOL(cros_ec_remove);
 int cros_ec_suspend(struct cros_ec_device *ec_dev)
 {
 	struct device *dev = ec_dev->dev;
+	int ret;
+
+	ret = cros_ec_sleep_event(ec_dev, HOST_SLEEP_EVENT_S3_SUSPEND);
+	if (ret < 0)
+		dev_dbg(ec_dev->dev, "Error %d sending suspend event to ec",
+			ret);
 
 	if (device_may_wakeup(dev))
 		ec_dev->wake_enabled = !enable_irq_wake(ec_dev->irq);
@@ -535,8 +564,16 @@ EXPORT_SYMBOL(cros_ec_suspend);
 
 int cros_ec_resume(struct cros_ec_device *ec_dev)
 {
+	int ret;
+
 	ec_dev->suspended = false;
+
 	enable_irq(ec_dev->irq);
+
+	ret = cros_ec_sleep_event(ec_dev, HOST_SLEEP_EVENT_S3_RESUME);
+	if (ret < 0)
+		dev_dbg(ec_dev->dev, "Error %d sending resume event to ec",
+			ret);
 
 	if (ec_dev->wake_enabled) {
 		disable_irq_wake(ec_dev->irq);