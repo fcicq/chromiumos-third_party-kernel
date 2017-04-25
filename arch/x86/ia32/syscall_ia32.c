783cad80d9584683560c0207d8774918fc6d25e3
Matthias Kaehlcke <mka@chromium.org>
2017-04-25 15:53:47 -0700

    CHROMIUM: Remove duplicate declarations of sys_call_ptr_t
    clang generates errors because sys_call_ptr_t is declared in different
    locations. Remove the duplicates and include the header with the
    declaration instead. Add the asmlinkage tag ("tells the compiler that
    the function should not expect to find any of its arguments in registers
    (a common optimization), but only on the CPU's stack") to the
    declaration as upstream does and some of the dupes already did.
    
    This is part of what 034042cc1e28 ("x86/entry/syscalls: Move
    syscall table declarations into asm/syscalls.h") does. We don't do a
    full backport since the change has further dependencies in which we
    are not interested.
    
    BUG=chromium:702746
    TEST=build with clang for edgar and hana
      build with gcc for gale
    
    Change-Id: Id690a6a0f29847766cefb64f2e5d699e210c6954
    Reviewed-on: https://chromium-review.googlesource.com/492367
    Commit-Ready: Matthias Kaehlcke <mka@chromium.org>
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- arch/x86/ia32/syscall_ia32.c
+++ arch/x86/ia32/syscall_ia32.c
@@ -5,14 +5,13 @@
 #include <linux/cache.h>
 #include <linux/thread_info.h> /* for ti_sys_call_ptr_t */
 #include <asm/asm-offsets.h>
+#include <asm/syscall.h>
 
 #define __SYSCALL_I386(nr, sym, compat) extern asmlinkage void compat(void) ;
 #include <asm/syscalls_32.h>
 #undef __SYSCALL_I386
 
 #define __SYSCALL_I386(nr, sym, compat) [nr] = compat,
-
-typedef void (*sys_call_ptr_t)(void);
 
 extern void compat_ni_syscall(void);
 
--- arch/x86/kernel/syscall_32.c
+++ arch/x86/kernel/syscall_32.c
@@ -4,14 +4,13 @@
 #include <linux/sys.h>
 #include <linux/cache.h>
 #include <asm/asm-offsets.h>
+#include <asm/syscall.h>
 
 #define __SYSCALL_I386(nr, sym, compat) extern asmlinkage void sym(void) ;
 #include <asm/syscalls_32.h>
 #undef __SYSCALL_I386
 
 #define __SYSCALL_I386(nr, sym, compat) [nr] = sym,
-
-typedef asmlinkage void (*sys_call_ptr_t)(void);
 
 extern asmlinkage void sys_ni_syscall(void);
 
