b2f6be973c783c5f653447d44d31dfe9fbba684b
Matthias Kaehlcke <mka@chromium.org>
2017-05-01 14:38:10 -0700

    CHROMIUM: x86: alt-syscall: Use same data type for declarations of ia32_sys_call_table
    clang raises an error because ia32_sys_call_table is declared with
    different types in ia32/syscall_ia32.c and include/asm/thread_info.h.
    
    Ideally both would declare the table as const sys_call_ptr_t[], however
    thread_info.h can't include <asm/syscall.h> with the definition of
    sys_call_ptr_t. Instead thread_info.h defines and uses ti_sys_call_ptr_t.
    which is equivalent to sys_call_ptr_t. Change the type of
    ia32_sys_call_table in ia32/syscall_ia32.c to ti_sys_call_ptr_t[]. Also
    add the const attribute to the extern declaration in thread_info.h to
    match the actual type.
    
    BUG=chromium:702746
    TEST=build with clang for edgar
    
    Change-Id: I70c8eaeff5095f05a2fd7c24df351e6a50bc87a3
    Reviewed-on: https://chromium-review.googlesource.com/492366
    Commit-Ready: Matthias Kaehlcke <mka@chromium.org>
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- arch/x86/ia32/syscall_ia32.c
+++ arch/x86/ia32/syscall_ia32.c
@@ -3,6 +3,7 @@
 #include <linux/linkage.h>
 #include <linux/sys.h>
 #include <linux/cache.h>
+#include <linux/thread_info.h> /* for ti_sys_call_ptr_t */
 #include <asm/asm-offsets.h>
 
 #define __SYSCALL_I386(nr, sym, compat) extern asmlinkage void compat(void) ;
@@ -15,7 +16,7 @@ typedef void (*sys_call_ptr_t)(void);
 
 extern void compat_ni_syscall(void);
 
-const sys_call_ptr_t ia32_sys_call_table[__NR_ia32_syscall_max+1] = {
+const ti_sys_call_ptr_t ia32_sys_call_table[__NR_ia32_syscall_max+1] = {
 	/*
 	 * Smells like a compiler bug -- it doesn't work
 	 * when the & below is removed.
