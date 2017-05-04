83fe4838be683ea27a59bc6562eff666a852c310
Shawn Lin <shawn.lin@rock-chips.com>
2017-05-04 10:24:48 +0800

    UPSTREAM: PCI: rockchip: Rename rockchip_cfg_atu() to rockchip_pcie_cfg_atu()
    Rename rockchip_cfg_atu() to keep the name consistent with other functions
    in pcie-rockchip.c.
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    (cherry picked from commit 7a1d3b8cb22cc9c4b5d4b5edf03095d466299700)
    
    BUG=none
    TEST=kevin WiFi
    
    Change-Id: I0fa140ce8fc558634e7225c6c9ad011e74f60ed9
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/717425
    Reviewed-by: Alexandru M Stan <amstan@chromium.org>

--- drivers/pci/host/pcie-rockchip.c
+++ drivers/pci/host/pcie-rockchip.c
@@ -1182,7 +1182,7 @@ static int rockchip_pcie_prog_ib_atu(struct rockchip_pcie *rockchip,
 	return 0;
 }
 
-static int rockchip_cfg_atu(struct rockchip_pcie *rockchip)
+static int rockchip_pcie_cfg_atu(struct rockchip_pcie *rockchip)
 {
 	struct device *dev = rockchip->dev;
 	int offset;
@@ -1305,7 +1305,7 @@ static int __maybe_unused rockchip_pcie_resume_noirq(struct device *dev)
 	if (err)
 		return err;
 
-	err = rockchip_cfg_atu(rockchip);
+	err = rockchip_pcie_cfg_atu(rockchip);
 	if (err)
 		return err;
 
@@ -1422,7 +1422,7 @@ static int rockchip_pcie_probe(struct platform_device *pdev)
 		}
 	}
 
-	err = rockchip_cfg_atu(rockchip);
+	err = rockchip_pcie_cfg_atu(rockchip);
 	if (err)
 		goto err_free_res;
 
