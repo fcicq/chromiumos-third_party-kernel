d8c9ed296a9d6062a07185331d499a47d556b984
Nathan Ciobanu <nathan.d.ciobanu@intel.com>
2017-06-02 11:23:10 -0700

    Revert "CHROMIUM: ACPI / property: have acpi_get_next_subnode take fwnode_handle"
    This reverts commit 2456571dc270bf24d190562193cec3b702748d99.
    Code upstream already and replaced by 34055190b19d
    
    Conflicts:
    Minor context delta
    	drivers/base/property.c
    
    BUG=b:62359918
    TEST=No regression in camera functionality
    TEST=Kernel builds and boots
    
    Change-Id: I3b9faa1cc09a3d7e33cec9fba4b334833d0fbc81
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/528606
    Commit-Ready: Nathan D Ciobanu <nathan.d.ciobanu@intel.com>
    Tested-by: Yong Zhi <yong.zhi@intel.com>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -825,7 +825,7 @@ int acpi_node_prop_read(struct fwnode_handle *fwnode, const char *propname,
 int acpi_dev_prop_read(struct acpi_device *adev, const char *propname,
 		       enum dev_prop_type proptype, void *val, size_t nval);
 
-struct fwnode_handle *acpi_get_next_subnode(struct fwnode_handle *node,
+struct fwnode_handle *acpi_get_next_subnode(struct device *dev,
 					    struct fwnode_handle *subnode);
 struct fwnode_handle *acpi_node_get_parent(struct fwnode_handle *fwnode);
 
@@ -944,8 +944,7 @@ static inline int acpi_dev_prop_read(struct acpi_device *adev,
 	return -ENXIO;
 }
 
-static inline struct fwnode_handle *acpi_get_next_subnode(
-						struct fwnode_handle *node,
+static inline struct fwnode_handle *acpi_get_next_subnode(struct device *dev,
 						struct fwnode_handle *subnode)
 {
 	return NULL;
