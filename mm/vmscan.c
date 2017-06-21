97c372af61e7a23e4d388fd1cd3376a797a5b140
Ben Cheng <bccheng@chromium.org>
2017-06-21 17:10:02 +0000

    Revert "CHROMIUM: spread min_filelist_kbytes across zones."
    This reverts commit 6ebd0ae516ef96aaf09eee52e7eb13c8bbe129d7.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > CHROMIUM: spread min_filelist_kbytes across zones.
    > 
    > min_filelist_kbytes represents the number of file pages below
    > which we stop file list reclaim.  It is understood to be a
    > global amount, but before this change it was applied separately
    > to each zone, resulting typically in a 2x increase on both ARM
    > and x86.
    > 
    > This could be done by adding a zone->min_filelist_pages field
    > and precomputing its value, but then we touch more files because
    > the min_filelist_kbytes sysfs handler would need to update that
    > field.
    > 
    > We have to use do_div() because some older builds don't support
    > 64-bit division (at least for ARM).
    > 
    > BUG=chromium:598857
    > TEST=check file pages in /proc/meminfo under memory pressure
    > CQ-DEPEND=CL:487914
    > Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    > 
    > (cherry picked from commit b096b0c4fbbb ("CHROMIUM: spread
    > min_filelist_kbytes across zones.")
    > 
    > Change-Id: I4466cd1b3e52f8e4669bc99e25e45979a9f45f3b
    > Reviewed-on: https://chromium-review.googlesource.com/488841
    > Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    > Tested-by: Luigi Semenzato <semenzato@chromium.org>
    > Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    > (cherry picked from commit 28aa49a8750e585bb04ab71953dddc63c111df5b)
    > Reviewed-on: https://chromium-review.googlesource.com/541963
    > Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    
    Bug: chromium:598857
    Change-Id: I83c15f402c63d15c03b8be42edf58b2a272ffc89
    Reviewed-on: https://chromium-review.googlesource.com/543665
    Reviewed-by: Ben Cheng <bccheng@chromium.org>
    Commit-Queue: Ben Cheng <bccheng@chromium.org>
    Tested-by: Ben Cheng <bccheng@chromium.org>
    Trybot-Ready: Ben Cheng <bccheng@chromium.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -155,14 +155,13 @@ static bool global_reclaim(struct scan_control *sc)
 
 static unsigned long zone_reclaimable_pages(struct zone *zone)
 {
-	u64 pages_min = min_filelist_kbytes >> (PAGE_SHIFT - 10);
+	unsigned long pages_min;
 	int nr;
 
 	nr = zone_page_state(zone, NR_ACTIVE_FILE) +
 	     zone_page_state(zone, NR_INACTIVE_FILE);
 
-	pages_min *= zone->managed_pages;
-	do_div(pages_min, totalram_pages);
+	pages_min = min_filelist_kbytes >> (PAGE_SHIFT - 10);
 	if (nr < pages_min)
 		nr = 0;
 
@@ -1863,16 +1862,13 @@ static int inactive_list_is_low(struct lruvec *lruvec, enum lru_list lru)
  */
 static int file_is_low(struct lruvec *lruvec)
 {
-	unsigned long active, inactive;
+	unsigned long pages_min, active, inactive;
 	struct zone *zone = lruvec_zone(lruvec);
-	u64 pages_min = min_filelist_kbytes >> (PAGE_SHIFT - 10);
 
 	if (!mem_cgroup_disabled())
 		return false;
 
-	pages_min *= zone->managed_pages;
-	do_div(pages_min, totalram_pages);
-
+	pages_min = min_filelist_kbytes >> (PAGE_SHIFT - 10);
 	active = zone_page_state(zone, NR_ACTIVE_FILE);
 	inactive = zone_page_state(zone, NR_INACTIVE_FILE);
 
