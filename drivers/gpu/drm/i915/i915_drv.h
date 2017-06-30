7a18e1b1b63e7a6bb517d902035668f02ce0dd18
Manasi Navare <manasi.d.navare@intel.com>
2017-06-30 09:33:48 -0700

    UPSTREAM: drm/i915/edp: Add a T12 panel delay quirk to fix DP AUX CH timeouts
    This patch fixes the DP AUX CH timeouts observed during CI IGT
    tests thus fixing the CI failures. This is done by adding a
    quirk for a particular PCI device that requires the panel power
    cycle delay (T12) to be set to 800ms which is 300msecs more than
    the minimum value specified in the eDP spec. So a quirk is
    implemented for that specific PCI device.
    
    v4:
    * Add Bugzilla links for FDO bugs in the commit message (Ville, Jani)
    v3:
    * Change some comments, specify the delay as 800 * 10 (Ville)
    v2:
    * Change the function and variable names to from PPS_T12_
    to _T12 since it is a T12 delay (Clint)
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101144
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101154
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101167
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101515
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Clinton Taylor <clinton.a.taylor@intel.com>
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Reviewed-by: Clinton Taylor <clinton.a.taylor@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1498840428-23176-1-git-send-email-manasi.d.navare@intel.com
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    (cherry picked from commit c99a259b4b5192ba30d37fe25f09632d4c52f5d7)
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    
    BUG=None
    TEST=CNL boots to graphics
    
    Change-Id: Iac5f4a1bb1e4b8ed9316d4eba3299ba5d1c87dd2

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1283,6 +1283,7 @@ enum intel_sbi_destination {
 #define QUIRK_BACKLIGHT_PRESENT (1<<3)
 #define QUIRK_PIPEB_FORCE (1<<4)
 #define QUIRK_PIN_SWIZZLED_PAGES (1<<5)
+#define QUIRK_INCREASE_T12_DELAY (1<<6)
 
 struct intel_fbdev;
 struct intel_fbc_work;
