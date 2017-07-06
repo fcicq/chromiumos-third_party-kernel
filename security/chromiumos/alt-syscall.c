ee23b8f3ed9c7848c44dd29f14008c4b6d8f549e
Chad Versace <chadversary@chromium.org>
2017-07-06 11:40:43 -0700

    CHROMIUM: alt-syscall: Whitelist memfd_create
    Intel's Vulkan driver requires memfd_create.  Whitelisting it should pose
    little security risk, as the syscall does little more than create an
    anonymous in-memory file with an initial size of 0.
    
    BUG=b:37665613
    TEST=Android boots for x86 and arm.
    
    Signed-off-by: Chad Versace <chadversary@chromium.org>
    Change-Id: I383b8ab0952ef8a4274b8353cb0984f3d11e1a87
    Reviewed-on: https://chromium-review.googlesource.com/642806
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- security/chromiumos/alt-syscall.c
+++ security/chromiumos/alt-syscall.c
@@ -258,6 +258,7 @@ static asmlinkage long alt_sys_prctl(int option, unsigned long arg2,
 #define __NR_compat_lsetxattr	__NR_ia32_lsetxattr
 #define __NR_compat_lstat	__NR_ia32_lstat
 #define __NR_compat_madvise	__NR_ia32_madvise
+#define __NR_compat_memfd_create	__NR_ia32_memfd_create
 #define __NR_compat_mincore	__NR_ia32_mincore
 #define __NR_compat_mkdir	__NR_ia32_mkdir
 #define __NR_compat_mkdirat	__NR_ia32_mkdirat
@@ -723,6 +724,7 @@ static struct syscall_whitelist_entry android_whitelist[] = {
 	SYSCALL_ENTRY(lseek),
 	SYSCALL_ENTRY(lsetxattr),
 	SYSCALL_ENTRY(madvise),
+	SYSCALL_ENTRY(memfd_create),
 	SYSCALL_ENTRY(mincore),
 	SYSCALL_ENTRY(mkdirat),
 	SYSCALL_ENTRY(mknodat),
@@ -1024,6 +1026,7 @@ static struct syscall_whitelist_entry third_party_whitelist[] = {
 	SYSCALL_ENTRY(ioctl),
 	SYSCALL_ENTRY(lseek),
 	SYSCALL_ENTRY(madvise),
+	SYSCALL_ENTRY(memfd_create),
 	SYSCALL_ENTRY(mprotect),
 	SYSCALL_ENTRY(munmap),
 	SYSCALL_ENTRY(nanosleep),
@@ -1241,6 +1244,7 @@ static struct syscall_whitelist_entry android_compat_whitelist[] = {
 	COMPAT_SYSCALL_ENTRY(lsetxattr),
 	COMPAT_SYSCALL_ENTRY(lstat),
 	COMPAT_SYSCALL_ENTRY(madvise),
+	COMPAT_SYSCALL_ENTRY(memfd_create),
 	COMPAT_SYSCALL_ENTRY(mincore),
 	COMPAT_SYSCALL_ENTRY(mkdir),
 	COMPAT_SYSCALL_ENTRY(mkdirat),
@@ -1495,6 +1499,7 @@ static struct syscall_whitelist_entry third_party_compat_whitelist[] = {
 	COMPAT_SYSCALL_ENTRY(lstat),
 	COMPAT_SYSCALL_ENTRY(lstat64),
 	COMPAT_SYSCALL_ENTRY(madvise),
+	COMPAT_SYSCALL_ENTRY(memfd_create),
 	COMPAT_SYSCALL_ENTRY(mkdir),
 	COMPAT_SYSCALL_ENTRY(mmap2),
 	COMPAT_SYSCALL_ENTRY(mprotect),
