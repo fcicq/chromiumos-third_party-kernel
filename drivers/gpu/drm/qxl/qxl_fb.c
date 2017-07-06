2ef3dfc1ec2ecd321809eb9c165ab0480b99b817
Daniel Vetter <daniel.vetter@ffwll.ch>
2017-07-06 14:57:35 +0200

    UPSTREAM: drm/<drivers>: Drop fbdev info flags
    - FBINFO_CAN_FORCE_OUTPUT has been a lie ever since we nerfed&removed
      the entire panic handling code in our fbdev emulation. We might
      restore kms panic output, but not through the bazillion of legacy
      code layers called fbdev/fbcon, there's just no way to make that
      work safely.
    
    - With the module check change FBINFO_DEFAULT is always 0, so can be
      removed too.
    
    That removes another change to cargo-cult stuff in kms drivers, yay!
    
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170706125735.28299-5-daniel.vetter@ffwll.ch
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit e1923ab3d01ef600dbde84ee29822980e816ee79)
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    
    BUG=None
    TEST=CNL boots to graphics
    
    Change-Id: I81a758bb78e44b1eaf09afce7a9c31c7622723c3

--- drivers/gpu/drm/qxl/qxl_fb.c
+++ drivers/gpu/drm/qxl/qxl_fb.c
@@ -274,7 +274,6 @@ static int qxlfb_create(struct qxl_fbdev *qfbdev,
 
 	drm_fb_helper_fill_fix(info, fb->pitches[0], fb->format->depth);
 
-	info->flags = FBINFO_DEFAULT;
 	info->fbops = &qxlfb_ops;
 
 	/*
