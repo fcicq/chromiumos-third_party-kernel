48006432fb41dec46d6879c6d7d024aa6e3eb3f9
Jiri Olsa <jolsa@kernel.org>
2017-07-14 18:35:51 +0200

    BACKPORT: perf/x86/intel: Fix debug_store reset field for freq events
    There's a bug in PEBs event enabling code, that prevents PEBS
    freq events to work properly after non freq PEBS event was run.
    
    freq events - perf_event_attr::freq set
                  -F <freq> option of perf record
    
    PEBS events - perf_event_attr::precise_ip > 0
                  default for perf record
    
    Like in following example with CPU 0 busy, we expect ~10000 samples
    for following perf tool run:
    
      # perf record -F 10000 -C 0 sleep 1
      [ perf record: Woken up 2 times to write data ]
      [ perf record: Captured and wrote 0.640 MB perf.data (10031 samples) ]
    
    Everything's fine, but once we run non freq PEBS event like:
    
      # perf record -c 10000 -C 0 sleep 1
      [ perf record: Woken up 4 times to write data ]
      [ perf record: Captured and wrote 1.053 MB perf.data (20061 samples) ]
    
    the freq events start to fail like this:
    
      # perf record -F 10000 -C 0 sleep 1
      [ perf record: Woken up 1 times to write data ]
      [ perf record: Captured and wrote 0.185 MB perf.data (40 samples) ]
    
    The issue is in non freq PEBs event initialization of debug_store reset
    field, which value is used to auto-reload the counter value after PEBS
    event drain. This value is not being used for PEBS freq events, but once
    we run non freq event it stays in debug_store data and screws the
    sample_freq counting for PEBS freq events.
    
    Setting the reset field to 0 for freq events.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Kan Liang <kan.liang@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20170714163551.19459-1-jolsa@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit dc853e26f73e903e0c87e24f2695b5dcf33b3bc1)
    
    Backport: Apply the hunk to the moved-from file.
    
    BUG=chromium:983515
    TEST=Build and test on DUT
    
    Change-Id: Ic5de702f00cb36b24f16354daa24cc0fa24f5d75
    Signed-off-by: Chinglin Yu <chinglinyu@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1704048
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- arch/x86/kernel/cpu/perf_event_intel_ds.c
+++ arch/x86/kernel/cpu/perf_event_intel_ds.c
@@ -859,6 +859,8 @@ void intel_pmu_pebs_enable(struct perf_event *event)
 	if (hwc->flags & PERF_X86_EVENT_AUTO_RELOAD) {
 		ds->pebs_event_reset[hwc->idx] =
 			(u64)(-hwc->sample_period) & x86_pmu.cntval_mask;
+	} else {
+		ds->pebs_event_reset[hwc->idx] = 0;
 	}
 
 	if (first_pebs || ds->pebs_interrupt_threshold > threshold)
