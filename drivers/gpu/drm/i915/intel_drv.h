d4438f89e9d1fbd6ba508950cebd033b7ec4f5de
Matthias Kaehlcke <mka@chromium.org>
2017-07-17 11:14:03 -0700

    FROMGIT: drm/i915: Consistently use enum pipe for PCH transcoders
    The current code uses in some instances enum transcoder for PCH
    transcoders and enum pipe in others. This is error prone and clang
    raises warnings like this:
    
    drivers/gpu/drm/i915/intel_dp.c:3546:51: warning: implicit conversion
      from enumeration type 'enum pipe' to different enumeration type
      'enum transcoder' [-Wenum-conversion]
        intel_set_pch_fifo_underrun_reporting(dev_priv, PIPE_A, false);
    
    Consistently use the type enum pipe for PCH transcoders.
    
    BUG=chromium:702741
    TEST=None
    
    (cherry picked from anongit.freedesktop.org drm-intel/drm-intel-next-queued
    commit a21960339c8c107eae99d68c85e6355189b22192)
    
    Conflicts:
    	drivers/gpu/drm/i915/intel_drv.h
    
    Change-Id: I421526e2f99bae62f36887dc9ece953d45a21cd1
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170717181403.57324-1-mka@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/575783
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1084,12 +1084,12 @@ hdmi_to_dig_port(struct intel_hdmi *intel_hdmi)
 bool intel_set_cpu_fifo_underrun_reporting(struct drm_i915_private *dev_priv,
 					   enum pipe pipe, bool enable);
 bool intel_set_pch_fifo_underrun_reporting(struct drm_i915_private *dev_priv,
-					   enum transcoder pch_transcoder,
+					   enum pipe pch_transcoder,
 					   bool enable);
 void intel_cpu_fifo_underrun_irq_handler(struct drm_i915_private *dev_priv,
 					 enum pipe pipe);
 void intel_pch_fifo_underrun_irq_handler(struct drm_i915_private *dev_priv,
-					 enum transcoder pch_transcoder);
+					 enum pipe pch_transcoder);
 void intel_check_cpu_fifo_underruns(struct drm_i915_private *dev_priv);
 void intel_check_pch_fifo_underruns(struct drm_i915_private *dev_priv);
 
@@ -1170,7 +1170,7 @@ void i915_audio_component_init(struct drm_i915_private *dev_priv);
 void i915_audio_component_cleanup(struct drm_i915_private *dev_priv);
 
 /* intel_display.c */
-enum transcoder intel_crtc_pch_transcoder(struct intel_crtc *crtc);
+enum pipe intel_crtc_pch_transcoder(struct intel_crtc *crtc);
 void skl_set_preferred_cdclk_vco(struct drm_i915_private *dev_priv, int vco);
 void intel_update_rawclk(struct drm_i915_private *dev_priv);
 int vlv_get_cck_clock(struct drm_i915_private *dev_priv,
