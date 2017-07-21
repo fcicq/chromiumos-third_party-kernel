64a210b1256575e5c49280231c805ae0dabd60cc
Chris Wilson <chris@chris-wilson.co.uk>
2017-07-21 13:32:21 +0100

    UPSTREAM: drm/i915: Serialize per-engine resets against new requests
    We rely on disabling the execlists (by stopping the tasklet) to prevent
    new requests from submitting to the engine ELSP before we are ready.
    However, we re-enable the engine before we call init_hw which gives
    userspace the opportunity to subit a new request which is then
    overwritten by init_hw -- but not before the HW may have started
    executing. The subsequent out-of-order CSB is detected by our sanity
    checks in intel_lrc_irq_handler().
    
    Fixes: a1ef70e14453 ("drm/i915: Add support for per engine reset recovery")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Michel Thierry <michel.thierry@intel.com>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Reviewed-by: Michel Thierry <michel.thierry@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170721123238.16428-3-chris@chris-wilson.co.uk
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 0364cd19d61caad7292cc4e87f5001595b6c7642)
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    
    BUG=None
    TEST=CNL boots to graphics
    
    Change-Id: Ie0e4239a40a7f2d6d1be856a38735d6205961904

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1932,6 +1932,12 @@ int i915_reset_engine(struct intel_engine_cs *engine)
 	}
 
 	ret = intel_gpu_reset(engine->i915, intel_engine_flag(engine));
+	if (ret) {
+		/* If we fail here, we expect to fallback to a global reset */
+		DRM_DEBUG_DRIVER("Failed to reset %s, ret=%d\n",
+				 engine->name, ret);
+		goto out;
+	}
 
 	/*
 	 * The request that caused the hang is stuck on elsp, we know the
@@ -1939,15 +1945,6 @@ int i915_reset_engine(struct intel_engine_cs *engine)
 	 * request to resume executing remaining requests in the queue.
 	 */
 	i915_gem_reset_engine(engine, active_request);
-
-	i915_gem_reset_finish_engine(engine);
-
-	if (ret) {
-		/* If we fail here, we expect to fallback to a global reset */
-		DRM_DEBUG_DRIVER("Failed to reset %s, ret=%d\n",
-				 engine->name, ret);
-		goto out;
-	}
 
 	/*
 	 * The engine and its registers (and workarounds in case of render)
@@ -1957,6 +1954,7 @@ int i915_reset_engine(struct intel_engine_cs *engine)
 	ret = engine->init_hw(engine);
 
 out:
+	i915_gem_reset_finish_engine(engine);
 	return ret;
 }
 
