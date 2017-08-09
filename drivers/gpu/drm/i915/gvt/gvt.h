1f71da96a6a43874bf1e2da13db36f2374c4c921
Kechen Lu <kechen.lu@intel.com>
2017-08-10 07:41:36 +0800

    UPSTREAM: drm/i915/gvt: Add shadow context descriptor updating
    The current context logic only updates the descriptor of context when
    it's being pinned to graphics memory space. But this cannot satisfy the
    requirement of shadow context. The addressing mode of the pinned shadow
    context descriptor may be changed according to the guest addressing mode.
    And this won't be updated, as the already pinned shadow context has no
    chance to update its descriptor. And this will lead to GPU hang issue,
    as shadow context is used with wrong descriptor. This patch fixes this
    issue by letting the pinned shadow context descriptor update its
    addressing mode on demand.
    
    This patch fixes GPU HANG issue which happends after changing the
    grub parameter i915.enable_ppgtt form 0x01 to 0x03 or vice versa and
    then rebooting the guest.
    
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>
    Signed-off-by: Kechen Lu <kechen.lu@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    (cherry picked from commit 9dfb8e5b9112483530429c96d463e6d45e0106ed)
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    
    BUG=None
    TEST=CNL boots to graphics
    
    Change-Id: I69f1ec59afb8a2df9f53e19b68dc0abc1b3de5e8

--- drivers/gpu/drm/i915/gvt/gvt.h
+++ drivers/gpu/drm/i915/gvt/gvt.h
@@ -168,6 +168,7 @@ struct intel_vgpu {
 	ktime_t last_ctx_submit_time;
 	DECLARE_BITMAP(tlb_handle_pending, I915_NUM_ENGINES);
 	struct i915_gem_context *shadow_ctx;
+	DECLARE_BITMAP(shadow_ctx_desc_updated, I915_NUM_ENGINES);
 
 #if IS_ENABLED(CONFIG_DRM_I915_GVT_KVMGT)
 	struct {
