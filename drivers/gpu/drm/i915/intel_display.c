98924ed38cb51567307821298f4121b5b0c1e494
Robert Tarasov <tutankhamen77@gmail.com>
2017-08-18 18:41:10 -0700

    CHROMIUM: drm/i915: Fixed "wait_for_vblank" timeout and "wait_for_pending_flips" timeout
    Fixed issue when wait_for_vblank could be invoked during crtc
    deinitialization when planes are already inactive. Also, fixed
    an issue with possible intel_crtc_wait_for_pending_flips()
    timeout during crtc deinitialization by adding one frame delay.
    
    BUG=chromium:692768,chromium:755461
    TEST=cat /var/log/messages | grep "vblank wait timed out"
    
    Change-Id: If65aff20945175dba64fea1b59565f840d8bbf27
    Signed-off-by: Robert Tarasov <tutankhamen77@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/626940
    Commit-Ready: Robert Tarasov <tutankhamen@chromium.org>
    Tested-by: Robert Tarasov <tutankhamen@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -24,6 +24,7 @@
  *	Eric Anholt <eric@anholt.net>
  */
 
+#include <linux/delay.h>
 #include <linux/dmi.h>
 #include <linux/module.h>
 #include <linux/input.h>
@@ -768,6 +769,24 @@ static void g4x_wait_for_vblank(struct drm_device *dev, int pipe)
 }
 
 /**
+ * check_crtc_planes_active - checks is there any active planes available
+ * for given crtc.
+ */
+static bool check_crtc_planes_active(struct drm_crtc *crtc)
+{
+	struct drm_plane *plane;
+	struct intel_plane *intel_plane;
+
+	drm_for_each_legacy_plane(plane, &crtc->dev->mode_config.plane_list) {
+		intel_plane = to_intel_plane(plane);
+		if (intel_plane->base.crtc != NULL &&
+			intel_plane->base.fb != NULL)
+			return true;
+	}
+	return false;
+}
+
+/**
  * intel_wait_for_vblank - wait for vblank on a given pipe
  * @dev: drm device
  * @pipe: pipe to wait for
@@ -782,6 +801,13 @@ void intel_wait_for_vblank(struct drm_device *dev, int pipe)
 	struct drm_crtc *crtc = dev_priv->pipe_to_crtc_mapping[pipe];
 	int timeout = crtc->hwmode.vrefresh ?
 		DIV_ROUND_UP(1000, crtc->hwmode.vrefresh) : 50;
+
+	/* return if all crtc planes are inactive */
+	if (!check_crtc_planes_active(crtc)) {
+		/* wait one frame */
+		msleep_interruptible(timeout);
+		return;
+	}
 
 	if (IS_G4X(dev) || INTEL_INFO(dev)->gen >= 5) {
 		g4x_wait_for_vblank(dev, pipe);
