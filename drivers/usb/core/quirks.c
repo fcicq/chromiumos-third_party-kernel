8ed608fa2acb8f2730e5ea316859cc8c45747896
Stephen Barber <smbarber@chromium.org>
2017-08-28 16:55:42 -0700

    CHROMIUM: usb: quirks: add NO_LPM quirk for plankton
    LPM on plankton appears to cause xHCI host controllers to claim
    that there isn't enough bandwidth to support additional devices.
    
    BUG=b:65034294,chromium:758293
    TEST=while using plankton capture, plug in USB speaker and make sure
    there are no bandwidth errors in dmesg
    
    Change-Id: Ibfcf68720b17c6b6cda107692fcf1bba36516121
    Signed-off-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/639694
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -157,6 +157,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* SKYMEDI USB_DRIVE */
 	{ USB_DEVICE(0x1516, 0x8628), .driver_info = USB_QUIRK_RESET_RESUME },
 
+	/* Google - Plankton */
+	{ USB_DEVICE(0x18d1, 0x501e), .driver_info = USB_QUIRK_NO_LPM },
+
 	/* BUILDWIN Photo Frame */
 	{ USB_DEVICE(0x1908, 0x1315), .driver_info =
 			USB_QUIRK_HONOR_BNUMINTERFACES },
