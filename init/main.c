dd4a9f2a53337e3d70c8cabcfc8f705d5a800331
Hugh Dickins <hughd@google.com>
2017-09-03 18:57:03 -0700

    CHROMIUM: kaiser: stack map PAGE_SIZE at THREAD_SIZE-PAGE_SIZE
    Kaiser only needs to map one page of the stack; and
    kernel/fork.c did not build on powerpc (no __PAGE_KERNEL).
    It's all cleaner if linux/kaiser.h provides kaiser_map_thread_stack()
    and kaiser_unmap_thread_stack() wrappers around asm/kaiser.h's
    kaiser_add_mapping() and kaiser_remove_mapping().  And use
    linux/kaiser.h in init/main.c to avoid the #ifdefs there.
    
    BUG=chromium:781913
    TEST=Build, deploy on caroline.
    
    Originally-from: Dave Hansen <dave.hansen@intel.com>
    
    Conflicts:
    init/main.c
    
    Change-Id: I78174dfd06ecae6d9447e3038bc1d84275aedf39
    Signed-off-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/757196
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/770010

--- init/main.c
+++ init/main.c
@@ -78,15 +78,13 @@
 #include <linux/context_tracking.h>
 #include <linux/random.h>
 #include <linux/list.h>
+#include <linux/kaiser.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
 #include <asm/setup.h>
 #include <asm/sections.h>
 #include <asm/cacheflush.h>
-#ifdef CONFIG_KAISER
-#include <asm/kaiser.h>
-#endif
 
 #ifdef CONFIG_X86_LOCAL_APIC
 #include <asm/smp.h>
@@ -498,9 +496,7 @@ static void __init mm_init(void)
 	percpu_init_late();
 	pgtable_init();
 	vmalloc_init();
-#ifdef CONFIG_KAISER
 	kaiser_init();
-#endif
 }
 
 asmlinkage __visible void __init start_kernel(void)
