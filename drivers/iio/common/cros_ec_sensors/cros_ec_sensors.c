b0fe96772284c9c3dca65684fe4862c5c1727580
Gwendal Grignou <gwendal@chromium.org>
2017-09-20 09:42:23 -0700

    CHROMIUM: iio: cros_ec: Workaround for invalid gyro min_freq
    Until CL:674003, EC indicates the minimal frequency of BMI160 gyro to be
    25mHz (AKA 0.025Hz) instead of 25Hz.
    To prevent CTS testGyroscope_5hz and testGyroscope_10hz to expect gyro
    to work at frequencies less than 25Hz, force the gyro minimal frequency
    at 25Hz when absurdly low.
    
    BUG=b:65000611
    TEST=On Eve, check gyro min_frequency is 25Hz instead of 25mHz.
    Check other frequencies are correct.
    
    Change-Id: I18df3fe6d5c659a59af6d13f484c92e1649eeb26
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/676140
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Nick Vaccaro <nvaccaro@chromium.org>

--- drivers/iio/common/cros_ec_sensors/cros_ec_sensors.c
+++ drivers/iio/common/cros_ec_sensors/cros_ec_sensors.c
@@ -241,6 +241,13 @@ static int cros_ec_sensors_probe(struct platform_device *pdev)
 			break;
 		case MOTIONSENSE_TYPE_GYRO:
 			channel->type = IIO_ANGL_VEL;
+			/*
+			 * Workaround for b:65000611:
+			 * The BMI160 gyro EC code may report minimal frequency
+			 * at 25mHz instead of 25Hz.
+			 */
+			if (state->core.min_freq < 1000)
+				state->core.min_freq = 25000;
 			break;
 		case MOTIONSENSE_TYPE_MAG:
 			channel->type = IIO_MAGN;
