12717a1689c04be5a955475a9818111843ccf2a1
Johannes Berg <johannes.berg@intel.com>
2017-10-06 11:53:33 +0200

    UPSTREAM: mac80211: only remove AP VLAN frames from TXQ
    When removing an AP VLAN interface, mac80211 currently purges
    the entire TXQ for the AP interface. Fix this by using the FQ
    API introduced in the previous patch to filter frames.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Acked-by: Toke Hiland-Jrgensen <toke@toke.dk>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit 2a9e25796b289f71c0802eca46005c750c57af95)
    
    Fix conflicts:
    net/mac80211/ieee80211_i.h
    
    BUG=b:131197056
    TEST=Compile v4.14-gw kernel
    
    Change-Id: If9659b4b167930295507846d449f91c8b65eada5
    Signed-off-by: Kan Yan <kyan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1703102
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Kan Yan <kyan@chromium.org>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -2023,6 +2023,8 @@ void ieee80211_txq_init(struct ieee80211_sub_if_data *sdata,
 void ieee80211_txq_purge(struct ieee80211_local *local,
 			 struct txq_info *txqi);
 void ieee80211_wake_txqs(unsigned long data);
+void ieee80211_txq_remove_vlan(struct ieee80211_local *local,
+			       struct ieee80211_sub_if_data *sdata);
 void ieee80211_send_auth(struct ieee80211_sub_if_data *sdata,
 			 u16 transaction, u16 auth_alg, u16 status,
 			 const u8 *extra, size_t extra_len, const u8 *bssid,
