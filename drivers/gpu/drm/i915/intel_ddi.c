e77c430f039d1bf87a16d98952a4099da7f1c169
Ville Syrjälä <ville.syrjala@linux.intel.com>
2017-10-10 15:12:01 +0300

    BACKPORT: drm/i915: Extract intel_disable_ddi_buf()
    Extract the code to disable the DDI_BUF_CTL into small helper. This
    will allows us to detangle the encoder type mess in
    intel_ddi_post_disable().
    
    v2: Keep using intel_ddi_get_encoder_port() for now
    
    BUG=chromium:911825
    TEST=boot Nocturne, Kevin, Eve, run graphics_SanAngeles and login_LoginSuccess
    
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Ville Syrjl <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171010121207.570-4-ville.syrjala@linux.intel.com
    (cherry picked from commit e725f6456f6fd251e8e02685dfe22543a8c73f5a)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I911c450638926f5a2806baf07aa28e7bbdb77eb7
    Reviewed-on: https://chromium-review.googlesource.com/1493580
    Commit-Ready: Dominik Behr <dbehr@chromium.org>
    Tested-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_ddi.c
+++ drivers/gpu/drm/i915/intel_ddi.c
@@ -1777,6 +1777,29 @@ static void intel_ddi_pre_enable(struct intel_encoder *intel_encoder,
 	}
 }
 
+static void intel_disable_ddi_buf(struct intel_encoder *encoder)
+{
+	struct drm_i915_private *dev_priv = to_i915(encoder->base.dev);
+	enum port port = intel_ddi_get_encoder_port(encoder);
+	bool wait = false;
+	u32 val;
+
+	val = I915_READ(DDI_BUF_CTL(port));
+	if (val & DDI_BUF_CTL_ENABLE) {
+		val &= ~DDI_BUF_CTL_ENABLE;
+		I915_WRITE(DDI_BUF_CTL(port), val);
+		wait = true;
+	}
+
+	val = I915_READ(DP_TP_CTL(port));
+	val &= ~(DP_TP_CTL_ENABLE | DP_TP_CTL_LINK_TRAIN_MASK);
+	val |= DP_TP_CTL_LINK_TRAIN_PAT1;
+	I915_WRITE(DP_TP_CTL(port), val);
+
+	if (wait)
+		intel_wait_ddi_buf_idle(dev_priv, port);
+}
+
 static void intel_ddi_post_disable(struct intel_encoder *intel_encoder,
 				   struct intel_crtc_state *old_crtc_state,
 				   struct drm_connector_state *old_conn_state)
@@ -1786,8 +1809,6 @@ static void intel_ddi_post_disable(struct intel_encoder *intel_encoder,
 	enum port port = intel_ddi_get_encoder_port(intel_encoder);
 	struct intel_dp *intel_dp = NULL;
 	int type = intel_encoder->type;
-	uint32_t val;
-	bool wait = false;
 
 	if (type == INTEL_OUTPUT_DP || type == INTEL_OUTPUT_EDP) {
 		/*
@@ -1806,20 +1827,7 @@ static void intel_ddi_post_disable(struct intel_encoder *intel_encoder,
 			intel_dp_sink_dpms(intel_dp, DRM_MODE_DPMS_OFF);
 	}
 
-	val = I915_READ(DDI_BUF_CTL(port));
-	if (val & DDI_BUF_CTL_ENABLE) {
-		val &= ~DDI_BUF_CTL_ENABLE;
-		I915_WRITE(DDI_BUF_CTL(port), val);
-		wait = true;
-	}
-
-	val = I915_READ(DP_TP_CTL(port));
-	val &= ~(DP_TP_CTL_ENABLE | DP_TP_CTL_LINK_TRAIN_MASK);
-	val |= DP_TP_CTL_LINK_TRAIN_PAT1;
-	I915_WRITE(DP_TP_CTL(port), val);
-
-	if (wait)
-		intel_wait_ddi_buf_idle(dev_priv, port);
+	intel_disable_ddi_buf(intel_encoder);
 
 	if (intel_dp) {
 		intel_edp_panel_vdd_on(intel_dp);
