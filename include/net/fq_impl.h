2425ad18ed5de1f564cf5a1f6cd0fc824b62a73e
Toke Høiland-Jørgensen <toke@toke.dk>
2017-10-16 17:05:57 +0200

    UPSTREAM: fq_impl: Properly enforce memory limit
    The fq structure would fail to properly enforce the memory limit in the case
    where the packet being enqueued was bigger than the packet being removed to
    bring the memory usage down. So keep dropping packets until the memory usage is
    back below the limit. Also, fix the statistics for memory limit violations.
    
    Signed-off-by: Toke Hiland-Jrgensen <toke@toke.dk>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit 0bfe649fbb1337400065fa47679b381b2ac845f0)
    
    BUG=b:131188415
    TEST=None
    
    Signed-off-by: Kan Yan <kyan@google.com>
    
    Change-Id: I99d064307e78552da69716057190cd5566a8e7f7
    Reviewed-on: https://chromium-review.googlesource.com/1666241
    Tested-by: Kan Yan <kyan@google.com>
    Commit-Ready: Kan Yan <kyan@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Kan Yan <kyan@google.com>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- include/net/fq_impl.h
+++ include/net/fq_impl.h
@@ -167,6 +167,7 @@ static void fq_tin_enqueue(struct fq *fq,
 			   fq_flow_get_default_t get_default_func)
 {
 	struct fq_flow *flow;
+	bool oom;
 
 	lockdep_assert_held(&fq->lock);
 
@@ -188,8 +189,8 @@ static void fq_tin_enqueue(struct fq *fq,
 	}
 
 	__skb_queue_tail(&flow->queue, skb);
-
-	if (fq->backlog > fq->limit || fq->memory_usage > fq->memory_limit) {
+	oom = (fq->memory_usage > fq->memory_limit);
+	while (fq->backlog > fq->limit || oom) {
 		flow = list_first_entry_or_null(&fq->backlogs,
 						struct fq_flow,
 						backlogchain);
@@ -204,8 +205,10 @@ static void fq_tin_enqueue(struct fq *fq,
 
 		flow->tin->overlimit++;
 		fq->overlimit++;
-		if (fq->memory_usage > fq->memory_limit)
+		if (oom) {
 			fq->overmemory++;
+			oom = (fq->memory_usage > fq->memory_limit);
+		}
 	}
 }
 
