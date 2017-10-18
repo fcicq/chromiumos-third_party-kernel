762fa6509dd8c8b5a2e41dcfd51e413e8bd2492a
Dmitry Grinberg <dmitrygr@google.com>
2017-10-17 17:54:10 -0700

    bluetooth: make eve-specific hacks generic
    Basically just remove the "is eve" guards
    
    TEST=works on eve
    Bug=b/1
    
    Change-Id: Ie2a2011ec34feb140944b94e2dbe7f0a370ee618
    Reviewed-on: https://chromium-review.googlesource.com/724661
    Commit-Ready: Dmitry Grinberg <dmitrygr@google.com>
    Tested-by: Dmitry Grinberg <dmitrygr@google.com>
    Reviewed-by: Dmitry Grinberg <dmitrygr@google.com>
    (cherry picked from commit 60882faa1067b5288e88e4dd7cb7e2361c7ed0b3)
    Reviewed-on: https://chromium-review.googlesource.com/757470
    (cherry picked from commit 1e857fe118772a70cfe2af2af00cb14c4fe03dca)
    Reviewed-on: https://chromium-review.googlesource.com/759307

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -4232,7 +4232,6 @@ static void hci_rx_work(struct work_struct *work)
 	}
 }
 
-#ifdef CONFIG_BT_EVE_HACKS
 /* This is a conditional HCI command. The HCI command
  * would be executed only when the run-time condition
  * is met.
@@ -4297,7 +4296,6 @@ out:
 	hci_dev_unlock(hdev);
 	return ret;
 }
-#endif
 
 static void hci_cmd_work(struct work_struct *work)
 {
@@ -4316,11 +4314,9 @@ static void hci_cmd_work(struct work_struct *work)
 		if (hdev->sent_cmd) {
 			atomic_dec(&hdev->cmd_cnt);
 
-#ifdef CONFIG_BT_EVE_HACKS
 			/* Check if the command could be skipped. */
 			if (skip_conditional_cmd(work, skb))
 				return;
-#endif
 
 			hci_send_frame(hdev, skb);
 			if (test_bit(HCI_RESET, &hdev->flags))
