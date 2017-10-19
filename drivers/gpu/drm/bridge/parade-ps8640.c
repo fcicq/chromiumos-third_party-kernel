29fb51cf98f00d97f425de64ea67e5f322e9d54d
Nathan Ciobanu <nathan.d.ciobanu@intel.com>
2017-10-19 15:19:44 -0700

    CHROMIUM: drm/bridge/: nuke drm_atomic_helper_connector_dpms()
    Commit 7d902c05b480 "drm: Nuke drm_atomic_helper_connector_dpms"
    removed drm_atomic_helper_connector_dpms() from drm/bridge but
    since the parade-ps8640 is not yet upstream it causes a compiler
    error.
    
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    
    BUG=None
    TEST=CNL boots to graphics
    
    Change-Id: Ie3efec7fbb26232e20083615f360e09fe31e33e7

--- drivers/gpu/drm/bridge/parade-ps8640.c
+++ drivers/gpu/drm/bridge/parade-ps8640.c
@@ -413,7 +413,6 @@ static enum drm_connector_status ps8640_detect(struct drm_connector *connector,
 }
 
 static const struct drm_connector_funcs ps8640_connector_funcs = {
-	.dpms = drm_atomic_helper_connector_dpms,
 	.fill_modes = drm_helper_probe_single_connector_modes,
 	.detect = ps8640_detect,
 	.destroy = drm_connector_cleanup,
