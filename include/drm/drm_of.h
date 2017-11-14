5214eaaa2ab427bfdfad69c6185ef2351c5cfa59
Nickey Yang <nickey.yang@rock-chips.com>
2017-11-14 15:03:54 +0800

    UPSTREAM: drm/drm_of: add drm_of_panel_bridge_remove function
    This function is the pendant of drm_of_find_panel_or_bridge()
    to remove a previously allocated panel_bridge.
    Given a specific port and endpoint it remove the panel bridge.
    Since drm_panel_bridge_remove() will check that bridge parameter
    is not NULL and is a real drm_panel_bridge and no a simple bridge
    it is safe to call it directly.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Reviewed-by: Philippe Cornu <philippe.cornu@st.com>
    Tested-by: Philippe Cornu <philippe.cornu@st.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1506936888-23844-3-git-send-email-benjamin.gaignard@linaro.org
    
    (cherry picked from commit c70087e8f16f1dfe703d223aadd95ede1cde8e30)
    
    BUG=b:65001779
    TEST=build and boot scarlet rev0, display still works
    
    Change-Id: I041cc014fee609a2276114a807fcb0f6e1d56567
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/768366
    Commit-Ready: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- include/drm/drm_of.h
+++ include/drm/drm_of.h
@@ -24,6 +24,8 @@ int drm_of_find_panel_or_bridge(const struct device_node *np,
 				int port, int endpoint,
 				struct drm_panel **panel,
 				struct drm_bridge **bridge);
+int drm_of_panel_bridge_remove(const struct device_node *np,
+			       int port, int endpoint);
 #else
 static inline uint32_t drm_of_find_possible_crtcs(struct drm_device *dev,
 						  struct device_node *port)
@@ -49,6 +51,12 @@ static inline int drm_of_find_panel_or_bridge(const struct device_node *np,
 					      int port, int endpoint,
 					      struct drm_panel **panel,
 					      struct drm_bridge **bridge)
+{
+	return -EINVAL;
+}
+
+static inline int drm_of_panel_bridge_remove(const struct device_node *np,
+					     int port, int endpoint)
 {
 	return -EINVAL;
 }
