3fe75b8caaadd5648e883e13f4dae1b7983c6bb9
Sarthak Kukreti <sarthakkukreti@chromium.org>
2017-11-27 17:02:52 -0800

    CHROMIUM: pkglist: port v3.18 --> v4.4
    - show() and store() are now part of the configfs_attribute.
    - Generalize function definitions to use config_item.
    - Combine the generation of packages_attr and extension_values_attr
    
    BUG=b:63876697
    TEST=compilation, mount esdfs
    
    Change-Id: Ia07a4eee7608b3f968479745d8b95c0cd5e429c1
    Signed-off-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/792494
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

--- drivers/pkglist/pkglist.c
+++ drivers/pkglist/pkglist.c
@@ -53,9 +53,9 @@ MODULE_PARM_DESC(pkglist_config_location, "Location of pkglist in configfs");
 
 static struct kmem_cache *hashtable_entry_cachep;
 
-static DECLARE_HASHTABLE(package_to_appid, 8);
-static DECLARE_HASHTABLE(package_to_userid, 8);
-static DECLARE_HASHTABLE(ext_to_groupid, 8);
+static DEFINE_HASHTABLE(package_to_appid, 8);
+static DEFINE_HASHTABLE(package_to_userid, 8);
+static DEFINE_HASHTABLE(ext_to_groupid, 8);
 static DEFINE_MUTEX(pkg_list_lock);
 static LIST_HEAD(pkglist_listeners);
 
@@ -463,6 +463,32 @@ static void packagelist_destroy(void)
 	pr_info("pkglist: destroyed pkglist\n");
 }
 
+#define PACKAGE_DETAILS_ATTR(_pfx, _name)			\
+static struct configfs_attribute _pfx##attr_##_name = {	\
+	.ca_name	= __stringify(_name),		\
+	.ca_mode	= S_IRUGO | S_IWUGO,		\
+	.ca_owner	= THIS_MODULE,			\
+	.show		= _pfx##_name##_show,		\
+	.store		= _pfx##_name##_store,		\
+}
+
+#define PACKAGE_DETAILS_ATTR_RO(_pfx, _name)			\
+static struct configfs_attribute _pfx##attr_##_name = {	\
+	.ca_name	= __stringify(_name),		\
+	.ca_mode	= S_IRUGO,			\
+	.ca_owner	= THIS_MODULE,			\
+	.show		= _pfx##_name##_show,		\
+}
+
+#define PACKAGE_DETAILS_ATTR_WO(_pfx, _name)			\
+static struct configfs_attribute _pfx##attr_##_name = {	\
+	.ca_name	= __stringify(_name),		\
+	.ca_mode	= S_IWUGO,			\
+	.ca_owner	= THIS_MODULE,			\
+	.store		= _pfx##_name##_store,		\
+}
+
+
 struct package_details {
 	struct config_item item;
 	struct qstr name;
@@ -474,22 +500,16 @@ static inline struct package_details *to_package_details(
 	return item ? container_of(item, struct package_details, item) : NULL;
 }
 
-CONFIGFS_ATTR_STRUCT(package_details);
-#define PACKAGE_DETAILS_ATTR(_name, _mode, _show, _store)	\
-static struct package_details_attribute package_details_attr_##_name = \
-			__CONFIGFS_ATTR(_name, _mode, _show, _store)
-#define PACKAGE_DETAILS_ATTRIBUTE(name) (&package_details_attr_##name.attr)
+#define PACKAGE_DETAILS_ATTRIBUTE(name) (&package_details_attr_##name)
 
-static ssize_t package_details_appid_show(
-			struct package_details *package_details, char *page)
+static ssize_t package_details_appid_show(struct config_item *item, char *page)
 {
 	return scnprintf(page, PAGE_SIZE, "%u\n", from_kuid(current_user_ns(),
-					__get_appid(&package_details->name)));
+				__get_appid(&to_package_details(item)->name)));
 }
 
-static ssize_t package_details_appid_store(
-				struct package_details *package_details,
-				const char *page, size_t count)
+static ssize_t package_details_appid_store(struct config_item *item,
+					   const char *page, size_t count)
 {
 	unsigned int tmp;
 	int ret;
@@ -501,7 +521,7 @@ static ssize_t package_details_appid_store(
 
 	uid = make_kuid(current_user_ns(), tmp);
 
-	ret = insert_packagelist_entry(&package_details->name, uid);
+	ret = insert_packagelist_entry(&to_package_details(item)->name, uid);
 
 	if (ret)
 		return ret;
@@ -509,9 +529,10 @@ static ssize_t package_details_appid_store(
 	return count;
 }
 
-static ssize_t package_details_excluded_userids_show(
-			struct package_details *package_details, char *page)
+static ssize_t package_details_excluded_userids_show(struct config_item *item,
+						     char *page)
 {
+	struct package_details *package_details = to_package_details(item);
 	struct hashtable_entry *hash_cur;
 	unsigned int hash = package_details->name.hash;
 	int count = 0;
@@ -520,7 +541,7 @@ static ssize_t package_details_excluded_userids_show(
 	hash_for_each_possible_rcu(package_to_userid, hash_cur, hlist, hash) {
 		if (qstr_case_eq(&package_details->name, &hash_cur->key))
 			count += scnprintf(page + count, PAGE_SIZE - count,
-					"%d ", atomic_read(&hash_cur->value));
+					   "%d ", atomic_read(&hash_cur->value));
 	}
 	rcu_read_unlock();
 	if (count)
@@ -529,9 +550,8 @@ static ssize_t package_details_excluded_userids_show(
 	return count;
 }
 
-static ssize_t package_details_excluded_userids_store(
-			struct package_details *package_details,
-			const char *page, size_t count)
+static ssize_t package_details_excluded_userids_store(struct config_item *item,
+						      const char *page, size_t count)
 {
 	unsigned int tmp;
 	int ret;
@@ -540,7 +560,7 @@ static ssize_t package_details_excluded_userids_store(
 	if (ret)
 		return ret;
 
-	ret = insert_userid_exclude_entry(&package_details->name, tmp);
+	ret = insert_userid_exclude_entry(&to_package_details(item)->name, tmp);
 
 	if (ret)
 		return ret;
@@ -548,9 +568,8 @@ static ssize_t package_details_excluded_userids_store(
 	return count;
 }
 
-static ssize_t package_details_clear_userid_store(
-			struct package_details *package_details,
-			const char *page, size_t count)
+static ssize_t package_details_clear_userid_store(struct config_item *item,
+						  const char *page, size_t count)
 {
 	unsigned int tmp;
 	int ret;
@@ -558,7 +577,7 @@ static ssize_t package_details_clear_userid_store(
 	ret = kstrtouint(page, 10, &tmp);
 	if (ret)
 		return ret;
-	remove_userid_exclude_entry(&package_details->name, tmp);
+	remove_userid_exclude_entry(&to_package_details(item)->name, tmp);
 	return count;
 }
 
@@ -566,19 +585,15 @@ static void package_details_release(struct config_item *item)
 {
 	struct package_details *package_details = to_package_details(item);
 
-	pr_debug("pkglist: removing %s\n", package_details->name.name);
+	pr_info("pkglist: removing %s\n", package_details->name.name);
 	remove_packagelist_entry(&package_details->name);
 	kfree(package_details->name.name);
 	kfree(package_details);
 }
 
-PACKAGE_DETAILS_ATTR(appid, S_IRUGO | S_IWUGO, package_details_appid_show,
-			package_details_appid_store);
-PACKAGE_DETAILS_ATTR(excluded_userids, S_IRUGO | S_IWUGO,
-		package_details_excluded_userids_show,
-		package_details_excluded_userids_store);
-PACKAGE_DETAILS_ATTR(clear_userid, S_IWUGO, NULL,
-		package_details_clear_userid_store);
+PACKAGE_DETAILS_ATTR(package_details_, appid);
+PACKAGE_DETAILS_ATTR(package_details_, excluded_userids);
+PACKAGE_DETAILS_ATTR_WO(package_details_, clear_userid);
 
 static struct configfs_attribute *package_details_attrs[] = {
 	PACKAGE_DETAILS_ATTRIBUTE(appid),
@@ -587,12 +602,8 @@ static struct configfs_attribute *package_details_attrs[] = {
 	NULL,
 };
 
-CONFIGFS_ATTR_OPS(package_details);
-
 static struct configfs_item_operations package_details_item_ops = {
 	.release = package_details_release,
-	.show_attribute = package_details_attr_show,
-	.store_attribute = package_details_attr_store,
 };
 
 static struct config_item_type package_appid_type = {
@@ -616,11 +627,7 @@ static inline struct extension_details *to_extension_details(
 			: NULL;
 }
 
-CONFIGFS_ATTR_STRUCT(extensions_value);
-#define EXTENSIONS_VALUE_ATTR(_name, _mode, _show, _store)	\
-static struct extensions_value_attribute extensions_value_attr_##_name = \
-			__CONFIGFS_ATTR(_name, _mode, _show, _store)
-#define EXTENSIONS_VALUE_ATTRIBUTE(name) (&extensions_value_attr_##name.attr)
+#define EXTENSIONS_VALUE_ATTRIBUTE(name) (&extensions_value_attr_##name)
 
 static void extension_details_release(struct config_item *item)
 {
@@ -638,15 +645,15 @@ static struct configfs_item_operations extension_details_item_ops = {
 	.release = extension_details_release,
 };
 
-static ssize_t extensions_value_extgid_show(
-			struct extensions_value *value, char *page)
+static ssize_t extensions_value_ext_gid_show(
+			struct config_item *item, char *page)
 {
 	return scnprintf(page, PAGE_SIZE, "%u\n",
-				from_kgid(current_user_ns(), value->gid));
+				from_kgid(current_user_ns(), to_extensions_value(item)->gid));
 }
 
-static ssize_t extensions_value_extgid_store(
-				struct extensions_value *value,
+static ssize_t extensions_value_ext_gid_store(
+				struct config_item *item,
 				const char *page, size_t count)
 {
 	unsigned int tmp;
@@ -656,13 +663,12 @@ static ssize_t extensions_value_extgid_store(
 	if (ret)
 		return ret;
 
-	value->gid = make_kgid(current_user_ns(), tmp);
+	to_extensions_value(item)->gid = make_kgid(current_user_ns(), tmp);
 
 	return count;
 }
 
-EXTENSIONS_VALUE_ATTR(ext_gid, S_IRUGO | S_IWUGO, extensions_value_extgid_show,
-		extensions_value_extgid_store);
+PACKAGE_DETAILS_ATTR(extensions_value_, ext_gid);
 
 static struct configfs_attribute *extensions_value_attrs[] = {
 	EXTENSIONS_VALUE_ATTRIBUTE(ext_gid),
@@ -711,14 +717,7 @@ static struct configfs_group_operations extensions_value_group_ops = {
 	.make_item = extension_details_make_item,
 };
 
-CONFIGFS_ATTR_OPS(extensions_value)
-static struct configfs_item_operations extensions_value_item_ops = {
-	.show_attribute = extensions_value_attr_show,
-	.store_attribute = extensions_value_attr_store,
-};
-
 static struct config_item_type extensions_name_type = {
-	.ct_item_ops	= &extensions_value_item_ops,
 	.ct_attrs	= extensions_value_attrs,
 	.ct_group_ops	= &extensions_value_group_ops,
 	.ct_owner	= THIS_MODULE,
@@ -782,14 +781,6 @@ static inline struct packages *to_packages(struct config_item *item)
 					struct packages, subsystem) : NULL;
 }
 
-CONFIGFS_ATTR_STRUCT(packages);
-#define PACKAGES_ATTR(_name, _mode, _show, _store)	\
-static struct packages_attribute packages_attr_##_name = \
-			__CONFIGFS_ATTR(_name, _mode, _show, _store)
-#define PACKAGES_ATTR_RO(_name, _show)	\
-static struct packages_attribute packages_attr_##_name = \
-			__CONFIGFS_ATTR_RO(_name, _show)
-
 static struct config_item *packages_make_item(struct config_group *group,
 							const char *name)
 {
@@ -811,8 +802,7 @@ static struct config_item *packages_make_item(struct config_group *group,
 	return &package_details->item;
 }
 
-static ssize_t packages_list_show(struct packages *packages,
-					 char *page)
+static ssize_t packages_list_show(struct config_item *item, char *page)
 {
 	struct hashtable_entry *hash_cur_app;
 	struct hashtable_entry *hash_cur_user;
@@ -824,10 +814,10 @@ static ssize_t packages_list_show(struct packages *packages,
 	rcu_read_lock();
 	hash_for_each_rcu(package_to_appid, i, hash_cur_app, hlist) {
 		written = scnprintf(page + count,
-					PAGE_SIZE - sizeof(errormsg) - count,
-					"%s %d\n",
-					hash_cur_app->key.name,
-					atomic_read(&hash_cur_app->value));
+				    PAGE_SIZE - sizeof(errormsg) - count,
+				    "%s %d\n",
+				    hash_cur_app->key.name,
+				    atomic_read(&hash_cur_app->value));
 		hash = hash_cur_app->key.hash;
 		hash_for_each_possible_rcu(package_to_userid, hash_cur_user, hlist, hash) {
 			if (qstr_case_eq(&hash_cur_app->key, &hash_cur_user->key)) {
@@ -847,8 +837,8 @@ static ssize_t packages_list_show(struct packages *packages,
 	return count;
 }
 
-static ssize_t packages_remove_userid_store(struct packages *packages,
-				       const char *page, size_t count)
+static ssize_t packages_remove_userid_store(struct config_item *item,
+					    const char *page, size_t count)
 {
 	unsigned int tmp;
 	int ret;
@@ -860,20 +850,18 @@ static ssize_t packages_remove_userid_store(struct packages *packages,
 	return count;
 }
 
-static struct packages_attribute packages_attr_packages_gid_list =
-		__CONFIGFS_ATTR_RO(packages_gid.list, packages_list_show);
-PACKAGES_ATTR(remove_userid, S_IWUGO, NULL, packages_remove_userid_store);
+static struct configfs_attribute packages_attr_packages_gid_list = {
+    .ca_name	= "packages_gid.list",
+    .ca_mode	= S_IRUGO,
+    .ca_owner	= THIS_MODULE,
+    .show	= packages_list_show,
+};
+PACKAGE_DETAILS_ATTR_WO(packages_, remove_userid);
 
 static struct configfs_attribute *packages_attrs[] = {
-	&packages_attr_packages_gid_list.attr,
-	&packages_attr_remove_userid.attr,
+	&packages_attr_packages_gid_list,
+	&packages_attr_remove_userid,
 	NULL,
-};
-
-CONFIGFS_ATTR_OPS(packages)
-static struct configfs_item_operations packages_item_ops = {
-	.show_attribute = packages_attr_show,
-	.store_attribute = packages_attr_store,
 };
 
 /*
@@ -885,7 +873,6 @@ static struct configfs_group_operations packages_group_ops = {
 };
 
 static struct config_item_type packages_type = {
-	.ct_item_ops	= &packages_item_ops,
 	.ct_group_ops	= &packages_group_ops,
 	.ct_attrs	= packages_attrs,
 	.ct_owner	= THIS_MODULE,
