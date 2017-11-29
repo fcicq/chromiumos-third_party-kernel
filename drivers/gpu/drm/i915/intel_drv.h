bca184f9d84a25b60cc1a44f71245aa05d4e9914
Sean Paul <seanpaul@chromium.org>
2017-11-28 19:43:51 -0500

    BACKPORT: drm/i915: Implement HDCP for HDMI
    This patch adds HDCP support for HDMI connectors by implementing
    the intel_hdcp_shim.
    
    Nothing too special, just a bunch of DDC reads/writes.
    
    Changes in v2:
    - Rebased on drm-intel-next
    Changes in v3:
    - Initialize new worker
    Changes in v4:
    - Remove SKL_ prefix from most register names (Daniel)
    - Wrap sanity checks in WARN_ON (Daniel)
    - Consolidate the enable/disable functions into one toggle fn
    - Use intel_hdcp_init (Daniel)
    Changes in v5:
    - checkpatch whitespace nits
    Changes in v6:
    - None
    
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Ramalingam C <ramalingam.c@intel.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180108195545.218615-9-seanpaul@chromium.org
    
    (cherry picked from commit 2320175feb74a11d3b287eead09bb5c0953cf27f)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    [downstream changes]
    - power_domain isn't stored in intel_encoder yet
    - add worker cleanup to encoder destroy hook
    
    Change-Id: Ie94e31425b25baa6b786a65a525294c10769803b
    Reviewed-on: https://chromium-review.googlesource.com/849084
    Commit-Ready: Sean Paul <seanpaul@google.com>
    Tested-by: Sean Paul <seanpaul@google.com>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/878770
    Reviewed-by: Sean Paul <seanpaul@google.com>
    Commit-Queue: Sean Paul <seanpaul@google.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1238,6 +1238,8 @@ uint32_t ddi_signal_levels(struct intel_dp *intel_dp);
 struct intel_shared_dpll *intel_ddi_get_link_dpll(struct intel_dp *intel_dp,
 						  int clock);
 u8 intel_ddi_dp_voltage_max(struct intel_encoder *encoder);
+int intel_ddi_toggle_hdcp_signalling(struct intel_encoder *intel_encoder,
+				     bool enable);
 
 unsigned int intel_fb_align_height(const struct drm_framebuffer *fb,
 				   int plane, unsigned int height);
