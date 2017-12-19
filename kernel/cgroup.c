ac665619ad93f8fd20f0fb2bb3589b8a6624c9b8
Kees Cook <keescook@chromium.org>
2017-12-18 21:20:57 -0800

    cgroup: Silence paren warning
    This adds parens to avoid warnings from modern compilers.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1425,10 +1425,12 @@ static int parse_cgroupfs_options(char *data, struct cgroup_sb_opts *opts)
 	 * otherwise if 'none', 'name=' and a subsystem name options were
 	 * not specified, let's default to 'all'
 	 */
-	if (all_ss || (!one_ss && !opts->none && !opts->name))
-		for_each_subsys(ss, i)
+	if (all_ss || (!one_ss && !opts->none && !opts->name)) {
+		for_each_subsys(ss, i) {
 			if (!ss->disabled)
 				opts->subsys_mask |= (1 << i);
+		}
+	}
 
 	/*
 	 * We either have to specify by name or by subsystems. (So all
