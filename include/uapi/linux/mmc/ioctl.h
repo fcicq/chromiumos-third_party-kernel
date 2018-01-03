8b72d7005e737262b1bcfc5878eb65463e5604d5
Gwendal Grignou <gwendal@chromium.org>
2018-01-03 15:53:37 -0800

    CHROMIUM: mmc: Increase ioctl command size
    Sandisk FW upgrade send the firmware image as a single command, and the
    image is greater than 128K. Given it is sent over ioctl, increase the
    maximum ioctl data size from 128K to 512K.
    
    BUG=chromium:768851
    TEST=compile
    
    Change-Id: I4c5be87dba756738a4f345ec60d130db7f64406f
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/849489

--- include/uapi/linux/mmc/ioctl.h
+++ include/uapi/linux/mmc/ioctl.h
@@ -47,11 +47,6 @@ struct mmc_ioc_cmd {
 
 #define MMC_IOC_CMD _IOWR(MMC_BLOCK_MAJOR, 0, struct mmc_ioc_cmd)
 
-/*
- * Since this ioctl is only meant to enhance (and not replace) normal access
- * to the mmc bus device, an upper data transfer limit of MMC_IOC_MAX_BYTES
- * is enforced per ioctl call.  For larger data transfers, use the normal
- * block device operations.
- */
-#define MMC_IOC_MAX_BYTES  (512L * 256)
+/* Incease ioctl data size to allow firwmare download in a single command. */
+#define MMC_IOC_MAX_BYTES  (512L * 1024)
 #endif /* LINUX_MMC_IOCTL_H */
