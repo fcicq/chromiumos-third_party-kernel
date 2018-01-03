a090e4b15f2c0b3369bf84bc999497cf19edfa40
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2018-01-02 17:19:22 -0800

    BACKPORT: drm/i915: Extract vfunc setup from logical ring initializers
    Majority of them was duplicated code and only render ring
    currently overrides some of them. We can save some lines of
    code and also take away the confusion on why bsd2 did not
    do the seqno coherency workaround. (VCS2 ring does not exist
    on platforms where workaround is needed but that was not
    documented in the code.)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1452619956-27014-1-git-send-email-tvrtko.ursulin@linux.intel.com
    (cherry picked from commit c9cacf9349ae4ce516627dac931766bfe98df793)
    Signed-off-by: Tarun Vyas <tarun.vyas@intel.com>
    
    Conflicts:
            This abstraction helps following patches overriding render ring
            function. Avoid the s/ring/engine that happened before this patch
            to minimize functional impact
            drivers/gpu/drm/i915/intel_lrc.c
    
    BUG=b:37163270, b:35574152
    TEST=test use cases described on bug
    
    Change-Id: Icaa91a11c6a22c4321db339502da2cbe06ca537e
    Signed-off-by: Lee, Shawn C <shawn.c.lee@intel.com>
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/860955
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Carlos Santa <carlos.santa@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Carlos Santa <carlos.santa@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2420,6 +2420,14 @@ struct drm_i915_cmd_table {
 #define INTEL_DEVID(p)	(INTEL_INFO(p)->device_id)
 #define INTEL_REVID(p)	(__I915__(p)->dev->pdev->revision)
 
+/*
+ * Return true if revision is in range [since,until] inclusive.
+ *
+ * Use 0 for open-ended since, and REVID_FOREVER for open-ended until.
+ */
+#define IS_REVID(p, since, until) \
+	(INTEL_REVID(p) >= (since) && INTEL_REVID(p) <= (until))
+
 #define IS_I830(dev)		(INTEL_DEVID(dev) == 0x3577)
 #define IS_845G(dev)		(INTEL_DEVID(dev) == 0x2562)
 #define IS_I85X(dev)		(INTEL_INFO(dev)->is_i85x)
@@ -2486,17 +2494,21 @@ struct drm_i915_cmd_table {
 
 #define IS_PRELIMINARY_HW(intel_info) ((intel_info)->is_preliminary)
 
-#define SKL_REVID_A0		(0x0)
-#define SKL_REVID_B0		(0x1)
-#define SKL_REVID_C0		(0x2)
-#define SKL_REVID_D0		(0x3)
-#define SKL_REVID_E0		(0x4)
-#define SKL_REVID_F0		(0x5)
+#define SKL_REVID_A0		0x0
+#define SKL_REVID_B0		0x1
+#define SKL_REVID_C0		0x2
+#define SKL_REVID_D0		0x3
+#define SKL_REVID_E0		0x4
+#define SKL_REVID_F0		0x5
 
-#define BXT_REVID_A0		(0x0)
-#define BXT_REVID_B0		(0x3)
-#define BXT_REVID_C0		(0x6)
+#define IS_SKL_REVID(p, since, until) (IS_SKYLAKE(p) && IS_REVID(p, since, until))
 
+#define BXT_REVID_A0		0x0
+#define BXT_REVID_A1		0x1
+#define BXT_REVID_B0		0x3
+#define BXT_REVID_C0		0x9
+#define IS_BXT_REVID(dev_priv, since, until) \
+	(IS_BROXTON(dev_priv) && IS_REVID(dev_priv, since, until))
 /*
  * The genX designation typically refers to the render engine, so render
  * capability related checks should use IS_GEN, while display and other checks
