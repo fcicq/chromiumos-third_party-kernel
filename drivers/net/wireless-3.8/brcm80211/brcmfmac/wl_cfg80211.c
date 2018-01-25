rockchip-linux/kernel

commit 9e637ccc654305600932510ef92c741cf23b304b

CHROMIUMOS: brcmfmac: Disable TDLS
Although partially operational (ping works in automated tests)
real-world testing indicates that TDLS does not actually work
well for this chipset.  Temporarily disable TDLS until further
work can be done to have it working properly.

Signed-off-by: Paul Stewart <pstew@chromium.org>

BUG=chrome-os-partner:35790
TEST=Cast a tab to Chromecast

Change-Id: Ia9a902c3fc66e9f58be9a08012db72908ecadbce
Reviewed-on: https://chromium-review.googlesource.com/243141
Reviewed-by: Daniel Kim <dekim@broadcom.com>
Reviewed-by: Zeping Qiu <zqiu@chromium.org>
Commit-Queue: Paul Stewart <pstew@chromium.org>
Tested-by: Paul Stewart <pstew@chromium.org>

drivers/net/wireless-3.8/brcm80211/brcmfmac/wl_cfg80211.c
@@ -4343,7 +4343,6 @@ static struct cfg80211_ops wl_cfg80211_ops = {
 	.stop_p2p_device = brcmf_p2p_stop_device,
 	.crit_proto_start = brcmf_cfg80211_crit_proto_start,
 	.crit_proto_stop = brcmf_cfg80211_crit_proto_stop,
-	.tdls_oper = brcmf_cfg80211_tdls_oper,
 #ifdef CONFIG_NL80211_TESTMODE
 	.testmode_cmd = brcmf_cfg80211_testmode,
 #endif
@@ -5498,8 +5497,7 @@ static int brcmf_setup_wiphy(struct wiphy *wiphy, struct brcmf_if *ifp)
 	wiphy->n_cipher_suites = ARRAY_SIZE(__wl_cipher_suites);
 	wiphy->flags |= WIPHY_FLAG_PS_ON_BY_DEFAULT |
 			WIPHY_FLAG_OFFCHAN_TX |
-			WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL |
-			WIPHY_FLAG_SUPPORTS_TDLS;
+			WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL;
 	if (!brcmf_roamoff)
 		wiphy->flags |= WIPHY_FLAG_SUPPORTS_FW_ROAM;
 	wiphy->mgmt_stypes = brcmf_txrx_stypes;

fcicq/chromiumos-third_party-kernel

commit f17c1faf3db742a8f5a6bb1332880ca778e0354a

CHROMIUM: Remove unused functions in brcm80211.
This change removes a couple of unused static functions, needed to
enable Werror. These functions became dead in

commit 9e637ccc6543 ("CHROMIUMOS: brcmfmac: Disable TDLS")

The warning they produce is:

drivers/net/wireless-3.8/brcm80211/brcmfmac/wl_cfg80211.c:4473:12:
warning: 'brcmf_cfg80211_tdls_oper' defined but not used [-Wunused-function]

BUG=chromium:702788
TEST=Build veyron_minnie.

Fixes: 9e637ccc6543 ("CHROMIUMOS: brcmfmac: Disable TDLS")
Change-Id: I54d9649150cd7d9add59626408c48af0aaa01e98
Signed-off-by: Evan Green <evgreen@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/877389
Commit-Ready: Evan Green <evgreen@google.com>
Tested-by: Evan Green <evgreen@google.com>
Reviewed-by: Douglas Anderson <dianders@chromium.org>

drivers/net/wireless-3.8/brcm80211/brcmfmac/wl_cfg80211.c
@@ -4449,53 +4449,6 @@ brcmf_notify_tdls_peer_event(struct brcmf_if *ifp,
 	return 0;
 }
 
-static int brcmf_convert_nl80211_tdls_oper(enum nl80211_tdls_operation oper)
-{
-	int ret;
-
-	switch (oper) {
-	case NL80211_TDLS_DISCOVERY_REQ:
-		ret = BRCMF_TDLS_MANUAL_EP_DISCOVERY;
-		break;
-	case NL80211_TDLS_SETUP:
-		ret = BRCMF_TDLS_MANUAL_EP_CREATE;
-		break;
-	case NL80211_TDLS_TEARDOWN:
-		ret = BRCMF_TDLS_MANUAL_EP_DELETE;
-		break;
-	default:
-		brcmf_err("unsupported operation: %d\n", oper);
-		ret = -EOPNOTSUPP;
-	}
-	return ret;
-}
-
-static int brcmf_cfg80211_tdls_oper(struct wiphy *wiphy,
-				    struct net_device *ndev, u8 *peer,
-				    enum nl80211_tdls_operation oper)
-{
-	struct brcmf_if *ifp;
-	struct brcmf_tdls_iovar_le info;
-	int ret = 0;
-
-	ret = brcmf_convert_nl80211_tdls_oper(oper);
-	if (ret < 0)
-		return ret;
-
-	ifp = netdev_priv(ndev);
-	memset(&info, 0, sizeof(info));
-	info.mode = (u8)ret;
-	if (peer)
-		memcpy(info.ea, peer, ETH_ALEN);
-
-	ret = brcmf_fil_iovar_data_set(ifp, "tdls_endpoint",
-				       &info, sizeof(info));
-	if (ret < 0)
-		brcmf_err("tdls_endpoint iovar failed: ret=%d\n", ret);
-
-	return ret;
-}
-
 static struct cfg80211_ops wl_cfg80211_ops = {
 	.add_virtual_intf = brcmf_cfg80211_add_iface,
 	.del_virtual_intf = brcmf_cfg80211_del_iface,