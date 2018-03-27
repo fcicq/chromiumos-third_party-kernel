d8d183db71787058fc3631ec94f1e1381fe61364
Kieran Bingham <kieran.bingham@ideasonboard.com>
2018-03-27 17:46:01 +0100

    BACKPORT: FROMLIST: media: uvcvideo: queue: Simplify spin-lock usage
    Both uvc_start_streaming(), and uvc_stop_streaming() are called from
    userspace context, with interrupts enabled. As such, they do not need to
    save the IRQ state, and can use spin_lock_irq() and spin_unlock_irq()
    respectively.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    (am from https://patchwork.kernel.org/patch/10311049/)
    
    Conflicts:
    	drivers/media/usb/uvc/uvc_queue.c
    
    BUG=chromium:820961
    TEST=compile
    
    Change-Id: Ie51deed3ac18d1f6d95fd816750848ed40b0f5e4
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1084527
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/usb/uvc/uvc_queue.c
+++ drivers/media/usb/uvc/uvc_queue.c
@@ -159,7 +159,6 @@ static int uvc_start_streaming(struct vb2_queue *vq, unsigned int count)
 {
 	struct uvc_video_queue *queue = vb2_get_drv_priv(vq);
 	struct uvc_streaming *stream = uvc_queue_to_stream(queue);
-	unsigned long flags;
 	int ret;
 
 	queue->buf_used = 0;
@@ -168,9 +167,9 @@ static int uvc_start_streaming(struct vb2_queue *vq, unsigned int count)
 	if (ret == 0)
 		return 0;
 
-	spin_lock_irqsave(&queue->irqlock, flags);
+	spin_lock_irq(&queue->irqlock);
 	uvc_queue_return_buffers(queue, UVC_BUF_STATE_QUEUED);
-	spin_unlock_irqrestore(&queue->irqlock, flags);
+	spin_unlock_irq(&queue->irqlock);
 
 	return ret;
 }
@@ -179,13 +178,12 @@ static void uvc_stop_streaming(struct vb2_queue *vq)
 {
 	struct uvc_video_queue *queue = vb2_get_drv_priv(vq);
 	struct uvc_streaming *stream = uvc_queue_to_stream(queue);
-	unsigned long flags;
 
 	uvc_video_enable(stream, 0);
 
-	spin_lock_irqsave(&queue->irqlock, flags);
+	spin_lock_irq(&queue->irqlock);
 	uvc_queue_return_buffers(queue, UVC_BUF_STATE_ERROR);
-	spin_unlock_irqrestore(&queue->irqlock, flags);
+	spin_unlock_irq(&queue->irqlock);
 }
 
 static struct vb2_ops uvc_queue_qops = {
