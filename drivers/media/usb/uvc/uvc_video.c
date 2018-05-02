639ff97b27fde259d8666192ab368b863ba444d4
Dennis Wu <denniswu@google.com>
2018-05-03 07:56:47 +0800

    CHROMIUM: media: uvcvideo: Short-term fix for huddly camera failure bug
    uvcvideo sends a halt signal to request a camera to stop sending frames
    as bulk inputs. This halt signal puts Huddly camera into a bad state.
    This CL adds a quirk that ensures uvcvideo does not send a HALT signal
    to HuddlyGo cameras. This way, HuddlyGo camera can stop on its own, when
    it realizes that the driver stopped reading frames. This fix was put
    into CrOS kernel 3.14 in cl/717816 and we're doing the same for Teemo
    CfM units with kernel 4.4 . Buganizer:b/65557515
    
    BUG=chromium:839517
    TEST=build kernel package and stress test HuddlyGo camera
    
    Change-Id: I3bd8777d5a31397581c73b0cf769d4b54bfd6196
    Reviewed-on: https://chromium-review.googlesource.com/1041160
    Commit-Ready: Dennis Wu <denniswu@google.com>
    Tested-by: Dennis Wu <denniswu@google.com>
    Reviewed-by: Min Zhuo <mzhuo@chromium.org>
    Reviewed-by: Zhongze Hu <frankhu@google.com>

--- drivers/media/usb/uvc/uvc_video.c
+++ drivers/media/usb/uvc/uvc_video.c
@@ -1887,7 +1887,10 @@ int uvc_video_enable(struct uvc_streaming *stream, int enable)
 			unsigned int pipe;
 
 			pipe = usb_sndbulkpipe(stream->dev->udev, epnum) | dir;
-			usb_clear_halt(stream->dev->udev, pipe);
+
+			if (!(stream->dev->quirks &
+						UVC_QUIRK_NO_HALT_ON_STOP_BULK))
+				usb_clear_halt(stream->dev->udev, pipe);
 		}
 
 		uvc_video_clock_cleanup(stream);
