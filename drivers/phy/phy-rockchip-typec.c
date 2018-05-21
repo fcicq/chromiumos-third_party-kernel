19098dca74bc5b7a5775091d4216a0b750972aa4
Lin Huang <hl@rock-chips.com>
2018-05-21 16:13:54 +0800

    BACKPORT: FROMLIST: soc: rockchip: split rockchip_typec_phy struct to separate header
    we may use rockchip_phy_typec struct in other driver, so split
    it to separate header.
    
    Signed-off-by: Lin Huang <hl@rock-chips.com>
    (am from https://patchwork.kernel.org/patch/10420467/)
    Conflicts:
    	drivers/phy/phy-rockchip-typec.c
    [phy-rockchip-typec.c is different path in upstream code]
    
    BUG=b:72006974
    TEST=DP display on Dru
    
    Change-Id: I709331d1577923be662660eb606f92b743903ba7
    Signed-off-by: Lin Huang <hl@rock-chips.com>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1069958

--- drivers/phy/phy-rockchip-typec.c
+++ drivers/phy/phy-rockchip-typec.c
@@ -63,6 +63,7 @@
 
 #include <linux/mfd/syscon.h>
 #include <linux/phy/phy.h>
+#include <soc/rockchip/rockchip_phy_typec.h>
 
 #define CMN_SSM_BANDGAP			(0x21 << 2)
 #define CMN_SSM_BIAS			(0x22 << 2)
@@ -349,52 +350,6 @@
 #define MODE_UFP_USB			BIT(0)
 #define MODE_DFP_USB			BIT(1)
 #define MODE_DFP_DP			BIT(2)
-
-struct usb3phy_reg {
-	u32 offset;
-	u32 enable_bit;
-	u32 write_enable;
-};
-
-/**
- * struct rockchip_usb3phy_port_cfg: usb3-phy port configuration.
- * @reg: the base address for usb3-phy config.
- * @typec_conn_dir: the register of type-c connector direction.
- * @usb3tousb2_en: the register of type-c force usb2 to usb2 enable.
- * @external_psm: the register of type-c phy external psm clock.
- * @pipe_status: the register of type-c phy pipe status.
- * @usb3_host_disable: the register of type-c usb3 host disable.
- * @usb3_host_port: the register of type-c usb3 host port.
- * @uphy_dp_sel: the register of type-c phy DP select control.
- */
-struct rockchip_usb3phy_port_cfg {
-	unsigned int reg;
-	struct usb3phy_reg typec_conn_dir;
-	struct usb3phy_reg usb3tousb2_en;
-	struct usb3phy_reg external_psm;
-	struct usb3phy_reg pipe_status;
-	struct usb3phy_reg usb3_host_disable;
-	struct usb3phy_reg usb3_host_port;
-	struct usb3phy_reg uphy_dp_sel;
-};
-
-struct rockchip_typec_phy {
-	struct device *dev;
-	void __iomem *base;
-	struct extcon_dev *extcon;
-	struct regmap *grf_regs;
-	struct clk *clk_core;
-	struct clk *clk_ref;
-	struct reset_control *uphy_rst;
-	struct reset_control *pipe_rst;
-	struct reset_control *tcphy_rst;
-	const struct rockchip_usb3phy_port_cfg *port_cfgs;
-	/* mutex to protect access to individual PHYs */
-	struct mutex lock;
-
-	bool flip;
-	u8 mode;
-};
 
 struct phy_reg {
 	u16 value;
