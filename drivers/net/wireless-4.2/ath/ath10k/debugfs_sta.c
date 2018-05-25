1b5e553b992dc31e53074ab0d1a5abe299b7c56c
Sudharshan <s.sudharshan@globaledgesoftware.corp-partner.google.com>
2018-05-25 14:36:58 +0530

    CHROMIUM: ath10k: Modified the buffer size for ampdu_subframe_count in debugfs.
    This patch has increased the buffer size of the array in order to
    accommodate "ampdu_subframe_count" string and it's value in the buffer.
    
    BUG=b:80279028
    TEST=Tested the change by reading the ampdu_subframe_count file
    in the debugfs.
    
    Change-Id: I40d20a27fe076135d306ecd7a1aefe6d2500b526
    Reviewed-on: https://chromium-review.googlesource.com/1072990
    Commit-Ready: NARAYANRADDI MASTI <nmasti@google.com>
    Tested-by: s.sudharshan s.sudharshan <s.sudharshan@globaledgesoftware.corp-partner.google.com>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@google.com>
    Reviewed-by: Dheeraj KM <dheeraj.km@globaledgesoft.com>

--- drivers/net/wireless-4.2/ath/ath10k/debugfs_sta.c
+++ drivers/net/wireless-4.2/ath/ath10k/debugfs_sta.c
@@ -713,7 +713,7 @@ static ssize_t ath10k_dbg_sta_read_ampdu_subframe_count(struct file *file,
 	struct ieee80211_sta *sta = file->private_data;
 	struct ath10k_sta *arsta = (struct ath10k_sta *)sta->drv_priv;
 	struct ath10k *ar = arsta->arvif->ar;
-	char buf[20];
+	char buf[32];
 	int len = 0;
 
 	mutex_lock(&ar->conf_mutex);
