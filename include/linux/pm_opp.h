89d8adfe025f6119e61cbb984026d1044d29e790
Matthias Kaehlcke <mka@chromium.org>
2018-05-31 12:44:29 -0700

    BACKPORT: PM / OPP: Expose _of_get_opp_desc_node as dev_pm_opp API
    Rename _of_get_opp_desc_node to dev_pm_opp_of_get_opp_desc_node and add it
    to include/linux/pm_opp.h to allow other drivers, such as platform OPP
    and cpufreq drivers, to make use of it.
    
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry-picked from commit 0764c604c8128f17fd740ff8b1701d0a1301eb7e)
    
    Conflicts:
      drivers/base/power/opp/of.c
        upstream shuffled things around, in v4.4 _of_get_opp_desc_node()
        is located in drivers/base/power/opp/cpu.c
      include/linux/pm_opp.h
        trivial conflict caused by upstream commit d708b384c06dd ("PM / OPP:
        -ENOSYS is applicable only to syscalls")
    
    BUG=b:74321682, b:73050145
    TEST=build and boot on dru
    
    Change-Id: Ibfc062e4a58fbcc8d1a3de53164dcf87d543473b
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1102132
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -65,6 +65,7 @@ void dev_pm_opp_put_supported_hw(struct device *dev);
 int dev_pm_opp_set_prop_name(struct device *dev, const char *name);
 void dev_pm_opp_put_prop_name(struct device *dev);
 int dev_pm_opp_set_sharing_cpus(struct device *cpu_dev, cpumask_var_t cpumask);
+struct device_node *dev_pm_opp_of_get_opp_desc_node(struct device *dev);
 #else
 static inline unsigned long dev_pm_opp_get_voltage(struct dev_pm_opp *opp)
 {
@@ -198,6 +199,11 @@ static inline void dev_pm_opp_of_cpumask_remove_table(cpumask_var_t cpumask)
 static inline int dev_pm_opp_of_get_sharing_cpus(struct device *cpu_dev, cpumask_var_t cpumask)
 {
 	return -ENOSYS;
+}
+
+static inline struct device_node *dev_pm_opp_of_get_opp_desc_node(struct device *dev)
+{
+	return NULL;
 }
 #endif
 
