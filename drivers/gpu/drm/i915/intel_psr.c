155acb1c96e5b0fd61833c6780e269015a8470cc
Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
2018-06-18 15:02:07 -0700

    UPSTREAM: drm/i915/psr: Fix warning in intel_psr_activate()
    commit 5422b37c907e ("drm/i915/psr: Kill delays when activating psr
    back.") removed the call to cancel a scheduled psr_work from
    psr_disable() and instead added an early return in the work function. But,
    if the scheduled work item is executed after psr_enable(), we end up
    printing warnings as PSR is already enabled and active. So, put the
    cancel_work call back in psr_disable().
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Jos Roberto de Souza <jose.souza@intel.com>
    Fixes: 5422b37c907e ("drm/i915/psr: Kill delays when activating psr back.")
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=106948
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180618220207.2778-1-dhinakaran.pandiyan@intel.com
    (cherry picked from commit 98fa2aecb5096bbeb18f6509ffd181aafe4d267d)
    from v4.17-rc3
    
    BUG=chromium:870499
    TEST=$(watch -c -d -n0.3 cat /run/debugfs_gpu/i915_edp_psr_status)
         and ensure that PSR is supported, enabled and active when there
         is no activity on the screen (eDP)
    
    Signed-off-by: Nathan Ciobanu <nathan.d.ciobanu@intel.com>
    Change-Id: Iee4ff6a366cd148ac342ba9f2cfb51484a875a67
    Reviewed-on: https://chromium-review.googlesource.com/1187482
    Tested-by: Casey G Bowman <casey.g.bowman@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_psr.c
+++ drivers/gpu/drm/i915/intel_psr.c
@@ -748,6 +748,7 @@ void intel_psr_disable(struct intel_dp *intel_dp,
 
 	dev_priv->psr.enabled = NULL;
 	mutex_unlock(&dev_priv->psr.lock);
+	cancel_work_sync(&dev_priv->psr.work);
 }
 
 int intel_psr_wait_for_idle(const struct intel_crtc_state *new_crtc_state)
