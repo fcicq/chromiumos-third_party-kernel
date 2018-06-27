2721b9099a5b926599d8ce31a27b1eaf7c760a39
Tarun Vyas <tarun.vyas@intel.com>
2018-06-27 13:02:49 -0700

    BACKPORT: drm/i915/psr: Lockless version of psr_wait_for_idle
    This is a lockless version of the exisiting psr_wait_for_idle().
    We want to wait for PSR to idle out inside intel_pipe_update_start.
    At the time of a pipe update, we should never race with any psr
    enable or disable code, which is a part of crtc enable/disable.
    The follow up patch will use this lockless wait inside pipe_update_
    start to wait for PSR to idle out before checking for vblank evasion.
    We need to keep the wait in pipe_update_start to as less as it can be.
    So,we can live and flourish w/o taking any psr locks at all.
    
    Even if psr is never enabled, psr2_enabled will be false and this
    function will wait for PSR1 to idle out, which should just return
    immediately, so a very short (~1-2 usec) wait for cases where PSR
    is disabled.
    
    v2: Add comment to explain the 25msec timeout (DK)
    
    v3: Rename psr_wait_for_idle to __psr_wait_for_idle_locked to avoid
        naming conflicts and propagate err (if any) to the caller (Chris)
    
    v5: Form a series with the next patch
    
    v7: Better explain the need for lockless wait and increase the max
        timeout to handle refresh rates < 60 Hz (Daniel Vetter)
    
    v8: Rebase
    
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Signed-off-by: Tarun Vyas <tarun.vyas@intel.com>
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180627200250.1515-1-tarun.vyas@intel.com
    (cherry picked from commit c43dbcbbcc8c515d4ececc7a996d5fc7286c28c3)
    Signed-off-by: Tarun Vyas <tarun.vyas@intel.com>
    
    BUG=b:70920211, b:109823283
    TEST=No *Potential atomic update error* in dmesg
    with external display connected and browser opened
    and cursor blinking in address bar for PSR entry-exit
    TEST=No other regressions on KBL/APL/BYT based devices
    
     Conflicts: [Tarun: Ctx detla due to]
    	drivers/gpu/drm/i915/intel_drv.h
    	["54fd3149 drm/i915/psr: Control PSR interrupts via debugfs".
    	While this will be useful sometime in the future, it is too
    	messy to backport right now.]
    
    	["cc3054ff6 drm/i915/psr: Begin to handle PSR/PSR2 errors set by
    	sink". Same as above, useful but too messy rn.]
    
    Change-Id: Ie316fb54e962450eb97a3725ed6955c9c5df4f30
    Reviewed-on: https://chromium-review.googlesource.com/1135428
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Casey G Bowman <casey.g.bowman@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1722,6 +1722,7 @@ void intel_psr_single_frame_update(struct drm_i915_private *dev_priv,
 				   unsigned frontbuffer_bits);
 void intel_psr_compute_config(struct intel_dp *intel_dp,
 			      struct intel_crtc_state *crtc_state);
+int intel_psr_wait_for_idle(struct drm_i915_private *dev_priv);
 
 /* intel_runtime_pm.c */
 int intel_power_domains_init(struct drm_i915_private *);
