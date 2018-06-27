178e191ada48f7a63dcd608c19ed6a10d9c0b99c
Kalle Valo <kvalo@qca.qualcomm.com>
2018-06-27 17:37:26 +0800

    BACKPORT: ath10k: refactor firmware crashdump code to coredump.c
    In preparation to add RAM dump support. No functional changes, only moving code
    and renaming function names.
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>
    (cherry picked from commit f25b9f285a0eff7ae5d987acfb1d2407769b67af)
    
    BUG=b:110227638
    TEST=Do basic test
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Change-Id: I35fcf90b489cec31b1ecdada43a09acd2062db82
    Reviewed-on: https://chromium-review.googlesource.com/1116528
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Wen Gong <wgong@qti-qualcomm.corp-partner.google.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/net/wireless/ar10k/ath/ath10k/debug.h
+++ drivers/net/wireless/ar10k/ath/ath10k/debug.h
@@ -80,12 +80,7 @@ void ath10k_debug_unregister(struct ath10k *ar);
 void ath10k_debug_fw_stats_process(struct ath10k *ar, struct sk_buff *skb);
 void ath10k_debug_tpc_stats_process(struct ath10k *ar,
 				    struct ath10k_tpc_stats *tpc_stats);
-struct ath10k_fw_crash_data *
-ath10k_debug_get_new_fw_crash_data(struct ath10k *ar);
-
 void ath10k_debug_dbglog_add(struct ath10k *ar, u8 *buffer, int len);
-
-int ath10k_debug_fw_devcoredump(struct ath10k *ar);
 
 #define ATH10K_DFS_STAT_INC(ar, c) (ar->debug.dfs_stats.c++)
 
@@ -139,17 +134,6 @@ static inline void ath10k_debug_tpc_stats_process(struct ath10k *ar,
 static inline void ath10k_debug_dbglog_add(struct ath10k *ar, u8 *buffer,
 					   int len)
 {
-}
-
-static inline struct ath10k_fw_crash_data *
-ath10k_debug_get_new_fw_crash_data(struct ath10k *ar)
-{
-	return NULL;
-}
-
-static inline int ath10k_debug_fw_devcoredump(struct ath10k *ar)
-{
-	return 0;
 }
 
 #define ATH10K_DFS_STAT_INC(ar, c) do { } while (0)
