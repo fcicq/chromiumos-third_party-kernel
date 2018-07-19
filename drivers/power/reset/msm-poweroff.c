4bbaec87abdda5cc516bac1a74146907fafbbbd8
Rajendra Nayak <rnayak@codeaurora.org>
2018-07-19 10:48:49 +0530

    BACKPORT: FROMLIST: power: reset: msm: Add support for download-mode control
    commit '8c1b7dc9b: firmware: qcom: scm: Expose download-mode control'
    added support for download-mode control using the scm firmware
    driver for platforms which require a secure call to write the magic
    cookie into the tcsr location.
    
    For platforms which *do not* need an scm call and where the kernel can
    write the cookie by a direct read/write, add similar support in the
    msm-poweroff driver.
    Similar to the scm driver, the msm-poweroff driver clears the cookie
    during a clean reboot.
    
    Download mode using msm-poweroff driver can be enabled by including
    msm-poweroff.download_mode=1 on the command line.
    
    Signed-off-by: Rajendra Nayak <rnayak@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/10533579/)
    
    Conflicts:
       drivers/power/reset/Kconfig
    
    BUG=b:110849289
    TEST=Pat your head and rub your tummy
    
    Change-Id: Ia1875d0086bf30d9bb028d16df104932ba58972d
    Signed-off-by: Douglas Anderson <dianders@chromium.org>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -18,11 +18,20 @@
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/reboot.h>
+#include <linux/regmap.h>
 #include <linux/pm.h>
 
+static bool download_mode = IS_ENABLED(CONFIG_POWER_RESET_MSM_DOWNLOAD_MODE);
+module_param(download_mode, bool, 0);
+
+#define QCOM_SET_DLOAD_MODE 0x10
 static void __iomem *msm_ps_hold;
+static struct regmap *tcsr_regmap;
+static unsigned int dload_mode_offset;
+
 static int deassert_pshold(struct notifier_block *nb, unsigned long action,
 			   void *data)
 {
@@ -44,8 +53,29 @@ static void do_msm_poweroff(void)
 
 static int msm_restart_probe(struct platform_device *pdev)
 {
+	int ret;
+	struct of_phandle_args args;
 	struct device *dev = &pdev->dev;
 	struct resource *mem;
+
+	if (download_mode) {
+		ret = of_parse_phandle_with_fixed_args(dev->of_node,
+						       "qcom,dload-mode", 1, 0,
+						       &args);
+		if (ret < 0)
+			return ret;
+
+		tcsr_regmap = syscon_node_to_regmap(args.np);
+		of_node_put(args.np);
+		if (IS_ERR(tcsr_regmap))
+			return PTR_ERR(tcsr_regmap);
+
+		dload_mode_offset = args.args[0];
+
+		/* Enable download mode by writing the cookie */
+		regmap_write(tcsr_regmap, dload_mode_offset,
+			     QCOM_SET_DLOAD_MODE);
+	}
 
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	msm_ps_hold = devm_ioremap_resource(dev, mem);
@@ -57,6 +87,13 @@ static int msm_restart_probe(struct platform_device *pdev)
 	pm_power_off = do_msm_poweroff;
 
 	return 0;
+}
+
+static void msm_restart_shutdown(struct platform_device *pdev)
+{
+	/* Clean shutdown, disable download mode to allow normal restart */
+	if (download_mode)
+		regmap_write(tcsr_regmap, dload_mode_offset, 0x0);
 }
 
 static const struct of_device_id of_msm_restart_match[] = {
@@ -71,6 +108,7 @@ static struct platform_driver msm_restart_driver = {
 		.name = "msm-restart",
 		.of_match_table = of_match_ptr(of_msm_restart_match),
 	},
+	.shutdown = msm_restart_shutdown,
 };
 
 static int __init msm_restart_init(void)
