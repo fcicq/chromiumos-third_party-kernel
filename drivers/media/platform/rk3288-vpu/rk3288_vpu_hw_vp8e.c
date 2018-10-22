3bf9028648260f6d163a9a6180253505a3915ea1
Hirokazu Honda <hiroh@chromium.org>
2018-10-22 11:17:01 +0900

    CHROMIUM: media: rk3288-vpu: Honor format number of planes in encoder
    The encoder always assumes 3 planes, but we also advertise support for 1- and
    2-plane formats, so honor them.
    
    This fixes a null pointer dereference which happened when the driver
    tries to get a DMA address of an out of bounds plane.
    
    BUG=chromium:893530, chromium:894381
    TEST=Run VEA unit test with NV12 input on veyron_minnie
    
    Change-Id: I7141b4349c48df7dffd5f068e3c3c1a6daa9822c
    Signed-off-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1293111
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/platform/rk3288-vpu/rk3288_vpu_hw_vp8e.c
+++ drivers/media/platform/rk3288-vpu/rk3288_vpu_hw_vp8e.c
@@ -183,14 +183,18 @@ static inline u32 enc_in_img_ctrl(struct rk3288_vpu_ctx *ctx)
 static void rk3288_vpu_vp8e_set_buffers(struct rk3288_vpu_dev *vpu,
 					struct rk3288_vpu_ctx *ctx)
 {
+	const u32 src_addr_regs[] = { VEPU_REG_ADDR_IN_LUMA,
+				      VEPU_REG_ADDR_IN_CB,
+				      VEPU_REG_ADDR_IN_CR };
 	const struct rk3288_vp8e_reg_params *params = ctx->run.vp8e.reg_params;
+	struct v4l2_pix_format_mplane *src_fmt = &ctx->src_fmt;
 	dma_addr_t ref_buf_dma, rec_buf_dma;
 	dma_addr_t stream_dma;
 	size_t rounded_size;
 	dma_addr_t dst_dma;
 	u32 start_offset;
 	size_t dst_size;
-
+	int i;
 	rounded_size = ref_luma_size(ctx->src_fmt.width,
 						ctx->src_fmt.height);
 
@@ -279,15 +283,10 @@ static void rk3288_vpu_vp8e_set_buffers(struct rk3288_vpu_dev *vpu,
 		vepu_write_relaxed(vpu, vpu->dummy_encode_src[PLANE_CR].dma,
 					VEPU_REG_ADDR_IN_CR);
 	} else {
-		vepu_write_relaxed(vpu, vb2_dma_contig_plane_dma_addr(
-					&ctx->run.src->b, PLANE_Y),
-					VEPU_REG_ADDR_IN_LUMA);
-		vepu_write_relaxed(vpu, vb2_dma_contig_plane_dma_addr(
-					&ctx->run.src->b, PLANE_CB),
-					VEPU_REG_ADDR_IN_CB);
-		vepu_write_relaxed(vpu, vb2_dma_contig_plane_dma_addr(
-					&ctx->run.src->b, PLANE_CR),
-					VEPU_REG_ADDR_IN_CR);
+		for (i = 0; i < src_fmt->num_planes; ++i)
+			vepu_write_relaxed(vpu, vb2_dma_contig_plane_dma_addr(
+						&ctx->run.src->b, i),
+					src_addr_regs[i]);
 	}
 
 	/* Source parameters. */
