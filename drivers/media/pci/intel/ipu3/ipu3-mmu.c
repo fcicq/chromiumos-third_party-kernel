7c52030e83190cc319aa11c63c6206c92aa2992c
Yong Zhi <yong.zhi@intel.com>
2018-11-08 10:57:56 -0600

    CHROMIUM: media: ipu3-imgu: Use pm helper to check power status
    Use pm_runtime_get_if_in_use() to check the PM status
    and usage counter.
    
    Link: https://patchwork.kernel.org/patch/10660313/
    
    BUG=b:72725454
    TEST=Sanity checked camera functions
    
    Change-Id: I981a879d744739ec086f842c463850807157cd0a
    Signed-off-by: Yong Zhi <yong.zhi@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1327185
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/pci/intel/ipu3/ipu3-mmu.c
+++ drivers/media/pci/intel/ipu3/ipu3-mmu.c
@@ -86,9 +86,10 @@ static void ipu3_mmu_tlb_invalidate(struct ipu3_mmu *mmu)
 static void call_if_ipu3_is_powered(struct ipu3_mmu *mmu,
 				    void (*func)(struct ipu3_mmu *mmu))
 {
-	pm_runtime_get_noresume(mmu->dev);
-	if (pm_runtime_active(mmu->dev))
-		func(mmu);
+	if (!pm_runtime_get_if_in_use(mmu->dev))
+		return;
+
+	func(mmu);
 	pm_runtime_put(mmu->dev);
 }
 
