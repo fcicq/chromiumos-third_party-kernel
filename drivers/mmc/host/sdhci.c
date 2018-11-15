4d037e2e23f5e240759ef4d123e85f8bdcded459
Adrian Hunter <adrian.hunter@intel.com>
2018-11-15 15:53:41 +0200

    BACKPORT: mmc: sdhci: Fix data command CRC error handling
    Existing data command CRC error handling is non-standard and does not work
    with some Intel host controllers. Specifically, the assumption that the
    host controller will continue operating normally after the error interrupt,
    is not valid. Change the driver to handle the error in the same manner
    as a data CRC error, taking care to ensure that the data line reset is
    done for single or multi-block transfers, and it is done before
    unmapping DMA.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 4bf780996669280171c9cd58196512849b93434e)
    Signed-off-by: Kaiyen Chang <kaiyen.chang@intel.corp-partner.google.com>
    
    BUG=b:117176477, b:123303213
    TEST=To sample the responses of cmd in the failing window to see if the
         kernel can detect the interrupt of ACMD23 CRC error.
    CQ-DEPEND=CL:1405963
    
    Conflicts:
    	drivers/mmc/host/sdhci.c
    
    Change-Id: I204395c5f7e37c325504443b5383fca65759543c
    Reviewed-on: https://chromium-review.googlesource.com/1405964
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Nelson Ye <nelson_ye@compal.corp-partner.google.com>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    (cherry picked from commit 06605dac4b7f552854dbd9b6eb7768dc4b3a7706)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1541843
    Reviewed-by: Gwendal Grignou <gwendal@google.com>
    Commit-Queue: Gwendal Grignou <gwendal@google.com>
    Tested-by: Gwendal Grignou <gwendal@google.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -889,6 +889,7 @@ static void sdhci_set_transfer_mode(struct sdhci_host *host,
 
 static void sdhci_finish_data(struct sdhci_host *host)
 {
+	struct mmc_command *data_cmd = host->data_cmd;
 	struct mmc_data *data;
 
 	BUG_ON(!host->data);
@@ -896,6 +897,16 @@ static void sdhci_finish_data(struct sdhci_host *host)
 	data = host->data;
 	host->data = NULL;
 	host->data_cmd = NULL;
+
+	/*
+	 * The controller needs a reset of internal state machines upon error
+	 * conditions.
+	 */
+	if (data->error) {
+		if (!host->cmd || host->cmd == data_cmd)
+			sdhci_do_reset(host, SDHCI_RESET_CMD);
+		sdhci_do_reset(host, SDHCI_RESET_DATA);
+	}
 
 	if ((host->flags & (SDHCI_REQ_USE_DMA | SDHCI_USE_ADMA)) ==
 	    (SDHCI_REQ_USE_DMA | SDHCI_USE_ADMA))
@@ -920,19 +931,9 @@ static void sdhci_finish_data(struct sdhci_host *host)
 	 */
 	if (data->stop &&
 	    (data->error ||
-	     !host->mrq->sbc)) {
-
-		/*
-		 * The controller needs a reset of internal state machines
-		 * upon error conditions.
-		 */
-		if (data->error) {
-			sdhci_do_reset(host, SDHCI_RESET_CMD);
-			sdhci_do_reset(host, SDHCI_RESET_DATA);
-		}
-
+	     !host->mrq->sbc))
 		sdhci_send_command(host, data->stop);
-	} else
+	else
 		tasklet_schedule(&host->finish_tasklet);
 }
 
@@ -2221,8 +2222,7 @@ static void sdhci_tasklet_finish(unsigned long param)
 	if (!(host->flags & SDHCI_DEVICE_DEAD) &&
 	    ((mrq->cmd && mrq->cmd->error) ||
 	     (mrq->sbc && mrq->sbc->error) ||
-	     (mrq->data && ((mrq->data->error && !mrq->data->stop) ||
-			    (mrq->data->stop && mrq->data->stop->error))) ||
+	     (mrq->data && mrq->data->stop && mrq->data->stop->error) ||
 	     (host->quirks & SDHCI_QUIRK_RESET_AFTER_REQUEST))) {
 
 		/* Some controllers need this kick or reset won't work here */
@@ -2307,20 +2307,12 @@ static void sdhci_cmd_irq(struct sdhci_host *host, u32 intmask, u32 *mask)
 		else
 			host->cmd->error = -EILSEQ;
 
-		/*
-		 * If this command initiates a data phase and a response
-		 * CRC error is signalled, the card can start transferring
-		 * data - the card may have received the command without
-		 * error.  We must not terminate the mmc_request early.
-		 *
-		 * If the card did not receive the command or returned an
-		 * error which prevented it sending data, the data phase
-		 * will time out.
-		 */
+		/* Treat data command CRC error the same as data CRC error */
 		if (host->cmd->data &&
 		    (intmask & (SDHCI_INT_CRC | SDHCI_INT_TIMEOUT)) ==
 		     SDHCI_INT_CRC) {
 			host->cmd = NULL;
+			*mask |= SDHCI_INT_DATA_CRC;
 			return;
 		}
 
