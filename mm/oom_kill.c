0aaad65218808bc6eed93cfb40cde488316ebdff
Luigi Semenzato <semenzato@chromium.org>
2019-02-28 16:25:54 -0800

    CHROMIUM: add oom kill count to /proc/vmstat
    This is a massively reduced backport of oom-kill counters in
    4.14.  This merely counts oom kill events globally and reports
    them in /proc/vmstat.
    
    The original patch is 8e675f7af50747e1e9e96538e8706767e4f80e2c.
    The functionality that we need (oom_kill field in /proc/vmstat)
    was added in 4.14.
    
    Link: http://lkml.kernel.org/r/149570810989.203600.9492483715840752937.stgit@buzz
    
    BUG=chromium:934937
    TEST=verified /proc/vmstat content after OOM kill
    Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    
    Change-Id: Id921eb54b0d0dc367ceb5cb3348fd1429385a294
    Reviewed-on: https://chromium-review.googlesource.com/1495762
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -640,6 +640,7 @@ void oom_kill_process(struct oom_control *oc, struct task_struct *p,
 	 */
 	do_send_sig_info(SIGKILL, SEND_SIG_FORCED, victim, true);
 	mark_oom_victim(victim);
+	count_vm_event(OOM_KILL);
 	pr_err("Killed process %d (%s) total-vm:%lukB, anon-rss:%lukB, file-rss:%lukB\n",
 		task_pid_nr(victim), victim->comm, K(victim->mm->total_vm),
 		K(get_mm_counter(victim->mm, MM_ANONPAGES)),
