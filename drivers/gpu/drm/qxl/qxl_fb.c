0415b83d0c64f9130d2d24e37c955251d1bcf185
Guenter Roeck <groeck@chromium.org>
2019-03-04 11:00:28 -0800

    FIXUP: BACKPORT: drm: Protect fb_defio in drivers with CONFIG_KMS_FBDEV_EMULATION
    0day reports:
    
    drivers/gpu/drm/qxl/qxl_fb.c:168:13: error:
    	'qxl_deferred_io' defined but not used
    
    Unlike the upstream code, which calls a generic function,
    the code in chromeos-4.4 calls a locally defined function.
    This function is no longer called if CONFIG_DRM_FBDEV_EMULATION
    is not enabled, resulting in the build error.
    
    BUG=b:117899525
    TEST=trybot
    
    Change-Id: Id298055abdefa54282ab229407be4f900b45c5b2
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1500113
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>

--- drivers/gpu/drm/qxl/qxl_fb.c
+++ drivers/gpu/drm/qxl/qxl_fb.c
@@ -165,6 +165,8 @@ static void qxl_dirty_update(struct qxl_fbdev *qfbdev,
 	schedule_work(&qdev->fb_work);
 }
 
+#ifdef CONFIG_DRM_FBDEV_EMULATION
+
 static void qxl_deferred_io(struct fb_info *info,
 			    struct list_head *pagelist)
 {
@@ -189,7 +191,6 @@ static void qxl_deferred_io(struct fb_info *info,
 	}
 };
 
-#ifdef CONFIG_DRM_FBDEV_EMULATION
 static struct fb_deferred_io qxl_defio = {
 	.delay		= QXL_DIRTY_DELAY,
 	.deferred_io	= qxl_deferred_io,
