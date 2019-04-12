a47ce1c207d775db543f298c1723dbb47f54eb7b
Evan Green <evgreen@chromium.org>
2019-04-11 20:25:37 -0700

    CHROMIUM: ALSA: hda: Add Cometlake PCI ID
    Add the Comet Lake PCI ID for spectaular audio experience.
    
    BUG=b:130385930
    TEST=Boot hatch
    
    Change-Id: I46fac1580cc604006b4bd8f06cd99dd9de31c0d3
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1564404
    Reviewed-by: Philip Chen <philipchen@chromium.org>
    Commit-Queue: Philip Chen <philipchen@chromium.org>
    Tested-by: Philip Chen <philipchen@chromium.org>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2464,6 +2464,9 @@ static const struct pci_device_id azx_ids[] = {
 	/* Icelake */
 	{ PCI_DEVICE(0x8086, 0x34c8),
 	  .driver_data = AZX_DRIVER_SKL | AZX_DCAPS_INTEL_SKYLAKE},
+	/* Comet Lake */
+	{ PCI_DEVICE(0x8086, 0x02c8),
+	  .driver_data = AZX_DRIVER_SKL | AZX_DCAPS_INTEL_SKYLAKE},
 	/* Broxton-P(Apollolake) */
 	{ PCI_DEVICE(0x8086, 0x5a98),
 	  .driver_data = AZX_DRIVER_SKL | AZX_DCAPS_INTEL_BROXTON },
