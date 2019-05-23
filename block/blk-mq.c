0228b4375ba5dac9657311491ff79d867ac25bcf
Mattias Nissler <mnissler@chromium.org>
2019-05-23 23:46:25 +0200

    CHROMIUM: blk-mq: Freeze queues independently on hotplug
    The code had an optimization to try and freeze all queues in parallel
    to minimize the wall time spent on queue reinitialization.
    Unfortunately, this can lead to deadlocks when one queue requires
    requests on another queue to be processed before it can run dry and
    the queues get frozen in the wrong order. This can happen with loop
    devices that depend on requests to the underlying device to be
    serviced before the loop requests can complete.
    
    BUG=b:132722933
    TEST=See bug.
    
    Signed-off-by: Mattias Nissler <mnissler@chromium.org>
    
    Change-Id: Ifa843e63da1987a8f696b50347d5fd3ba58c52f8
    Reviewed-on: https://chromium-review.googlesource.com/1626868
    Commit-Ready: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1646536
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2166,30 +2166,18 @@ static int blk_mq_queue_reinit_notify(struct notifier_block *nb,
 
 	mutex_lock(&all_q_mutex);
 
-	/*
-	 * We need to freeze and reinit all existing queues.  Freezing
-	 * involves synchronous wait for an RCU grace period and doing it
-	 * one by one may take a long time.  Start freezing all queues in
-	 * one swoop and then wait for the completions so that freezing can
-	 * take place in parallel.
-	 */
-	list_for_each_entry(q, &all_q_list, all_q_node)
-		blk_mq_freeze_queue_start(q);
 	list_for_each_entry(q, &all_q_list, all_q_node) {
-		blk_mq_freeze_queue_wait(q);
+		blk_mq_freeze_queue(q);
 
 		/*
 		 * timeout handler can't touch hw queue during the
 		 * reinitialization
 		 */
 		del_timer_sync(&q->timeout);
-	}
 
-	list_for_each_entry(q, &all_q_list, all_q_node)
 		blk_mq_queue_reinit(q, &online_new);
-
-	list_for_each_entry(q, &all_q_list, all_q_node)
 		blk_mq_unfreeze_queue(q);
+	}
 
 	mutex_unlock(&all_q_mutex);
 	return NOTIFY_OK;
