c8683da8e221c5f7bb3c5017c394ef760f1c4ca3
Chiou, Cooper <cooper.chiou@intel.com>
2019-08-16 11:40:19 +0800

    [Test only. Not for merge] CHROMIUM: revert "drm: Reject getfb for multi-plane framebuffers"
    This reverts commit b24791fe00f8b089d5b10cb7bcc4e1ae88b4831b
    
    Reason for revert:
    factory test script drm_utils executed _GetDRMLibrary().drmModeGetFB to
    get framebuffer but failed in null point exception due to drm_mode_getfb
    not support multi-planar framebuffer when chromium UI sends multi-layer.
    In previous version, chromium UI only set single layer in factory UI.
    Moreover, kernel didn't land ioctl getfb2 solution in libdrm and kernel
    to handle multi-plane framebuffer case.
    So we revert this change to pass factory test script "USB3 external
    display", and no impact chromium.
    
    BUG=b:139266105
    TEST=boot hatch, run google play, run camera, run factory_script,
    pass external display test, no graphics corruption
    
    Change-Id: I0e7e205f733ba08c729157f72eb1813210f1f18e
    Signed-off-by: Chiou, Cooper <cooper.chiou@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1758558
    Reviewed-by: Philip Chen <philipchen@chromium.org>
    Commit-Queue: Philip Chen <philipchen@chromium.org>
    Tested-by: Philip Chen <philipchen@chromium.org>

--- drivers/gpu/drm/drm_framebuffer.c
+++ drivers/gpu/drm/drm_framebuffer.c
@@ -503,12 +503,6 @@ int drm_mode_getfb(struct drm_device *dev,
 	if (!fb)
 		return -ENOENT;
 
-	/* Multi-planar framebuffers need getfb2. */
-	if (fb->format->num_planes > 1) {
-		ret = -EINVAL;
-		goto out;
-	}
-
 	if (!fb->funcs->create_handle) {
 		ret = -ENODEV;
 		goto out;
