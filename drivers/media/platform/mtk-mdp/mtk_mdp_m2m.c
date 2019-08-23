2f1449ab6467f6d29685a891361fd46d74b402db
Hirokazu Honda <hiroh@chromium.org>
2019-08-23 05:05:33 +0000

    Revert "CHROMIUM: media: mtk-mdp: Add V4L2_PIX_FMT_NV12 support"
    This reverts commit a7bb9f3933cca31db251cc7664c328310eaab851.
    
    Reason for revert: Break many CTS (b/139714614)
    
    Original change's description:
    > CHROMIUM: media: mtk-mdp: Add V4L2_PIX_FMT_NV12 support
    >
    > V4L2_PIX_FMT_NV12 is supported by mtk-mdp as well as V4L2_PIX_FMT_NV12M.
    >
    > BUG=chromium:987185
    > TEST=video_decode_accelerator_tests
    > TEST=Play video with Chrome
    > Cq-Depend:chromium:1716864
    > Change-Id: I1a204a88c3a0d74ff1ac8865a86679e329cf66b4
    > Signed-off-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/1716844
    > Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    > Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    > Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    
    BUG=chromium:987185
    BUG=b:139714614
    Cq-Depend: chromium:1767450
    
    Change-Id: Ia1f25bbfce2243fa58202067121efd307fdd056e
    Reviewed-on: https://chromium-review.googlesource.com/1767680
    Tested-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Ready: Hirokazu Honda <hiroh@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/platform/mtk-mdp/mtk_mdp_m2m.c
+++ drivers/media/platform/mtk-mdp/mtk_mdp_m2m.c
@@ -77,15 +77,6 @@ static const struct mtk_mdp_fmt mtk_mdp_formats[] = {
 		.flags		= MTK_MDP_FMT_FLAG_OUTPUT |
 				  MTK_MDP_FMT_FLAG_CAPTURE,
 	}, {
-		.name		= "YUV420 contig, YCbCr",
-		.pixelformat	= V4L2_PIX_FMT_NV12,
-		.depth		= { 12 },
-		.row_depth	= { 8 },
-		.num_planes	= 1,
-		.num_comp	= 2,
-		.flags		= MTK_MDP_FMT_FLAG_OUTPUT |
-				  MTK_MDP_FMT_FLAG_CAPTURE,
-	}, {
 		.name		= "YUV420 non-contig. 3p, Y/Cb/Cr",
 		.pixelformat	= V4L2_PIX_FMT_YUV420M,
 		.depth		= { 8, 2, 2 },
@@ -475,8 +466,6 @@ static void mtk_mdp_prepare_addr(struct mtk_mdp_ctx *ctx,
 			addr->addr[1] = (dma_addr_t)(addr->addr[0] + pix_size);
 			addr->addr[2] = (dma_addr_t)(addr->addr[1] +
 					(pix_size >> 2));
-		} else if (frame->fmt->pixelformat == V4L2_PIX_FMT_NV12) {
-			addr->addr[1] = (dma_addr_t) (addr->addr[0] + pix_size);
 		} else {
 			dev_err(&ctx->mdp_dev->pdev->dev,
 				"Invalid pixelformat:0x%x\n",
