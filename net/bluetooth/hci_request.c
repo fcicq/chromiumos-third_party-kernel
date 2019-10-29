51350d39b8783f4fc43f3c783cdf7e3046904962
Alain Michaud <alainmichaud@google.com>
2019-10-29 09:49:56 -0400

    CHROMIUM: bluetooth: Enable filter duplicated adv offload.
    This change will reduce by an order of magnitude the number
    of advertisements the system will need to process when an LE
    device that distributed an IRK is paired but isn't connected.
    
    This isn't a complete solution since the filter duplicate is
    a best effort solution at best from the controller, so in very
    noisy environements, it is possible that the controller will
    be unable to filter duplicates and will send them all up to the
    host.
    
    BUG=b:143696105
    TEST=Verify that advertisements duplicates are filtered after
    pairing a bluetooth LE mouse and disconnecting it.
    
    Change-Id: If1f038d3f6419ed23dfca5dbdcf783d3b9bb6b08
    Signed-off-by: Alain Michaud <alainmichaud@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1897687
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Michael Sun <michaelfsun@google.com>

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -836,7 +836,8 @@ void hci_req_add_le_passive_scan(struct hci_request *req)
 
 	memset(&enable_cp, 0, sizeof(enable_cp));
 	enable_cp.enable = LE_SCAN_ENABLE;
-	enable_cp.filter_dup = LE_SCAN_FILTER_DUP_ENABLE;
+	enable_cp.filter_dup = (hdev->le_scan_type == LE_SCAN_PASSIVE) ?
+			LE_SCAN_FILTER_DUP_ENABLE : LE_SCAN_FILTER_DUP_DISABLE;
 	hci_req_add(req, HCI_OP_LE_SET_SCAN_ENABLE, sizeof(enable_cp),
 		    &enable_cp);
 }
@@ -1932,7 +1933,6 @@ static int le_scan_restart(struct hci_request *req, unsigned long opt)
 	struct hci_dev *hdev = req->hdev;
 	struct hci_cp_le_set_scan_enable cp;
 
-
 	/* If controller is not scanning or about to scan we are done. */
 	if (!hci_dev_test_flag(hdev, HCI_LE_SCAN) && !hci_dev_test_flag(hdev, HCI_LE_SCAN_CHANGE_IN_PROGRESS))
 		return 0;
@@ -1942,7 +1942,8 @@ static int le_scan_restart(struct hci_request *req, unsigned long opt)
 
 	memset(&cp, 0, sizeof(cp));
 	cp.enable = LE_SCAN_ENABLE;
-	cp.filter_dup = LE_SCAN_FILTER_DUP_DISABLE;
+	cp.filter_dup = (hdev->le_scan_type == LE_SCAN_PASSIVE) ?
+			LE_SCAN_FILTER_DUP_ENABLE : LE_SCAN_FILTER_DUP_DISABLE;
 	hci_req_add(req, HCI_OP_LE_SET_SCAN_ENABLE, sizeof(cp), &cp);
 
 	return 0;
@@ -2035,7 +2036,8 @@ static int active_scan(struct hci_request *req, unsigned long opt)
 
 	memset(&enable_cp, 0, sizeof(enable_cp));
 	enable_cp.enable = LE_SCAN_ENABLE;
-	enable_cp.filter_dup = LE_SCAN_FILTER_DUP_DISABLE;
+	enable_cp.filter_dup = (hdev->le_scan_type == LE_SCAN_PASSIVE) ?
+			LE_SCAN_FILTER_DUP_ENABLE : LE_SCAN_FILTER_DUP_DISABLE;
 
 	BT_DBG("BT_DBG_DG: set scan enable tx: active_scan (ena=%d)\n", enable_cp.enable);
 	hci_req_add(req, HCI_OP_LE_SET_SCAN_ENABLE, sizeof(enable_cp),
